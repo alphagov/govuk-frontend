266f2f9017e25fd70833e8a09224e7010da0e5e0
Jani Kraner <jani.kraner@gmail.com>
2017-05-19 09:54:55 +0100

    adding paths.json configuration, styles linting task and sasslint dev dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
+const paths = require('./config/paths.json')
 const gulp = require('gulp')
+const sasslint = require('gulp-sass-lint')
 
 // Default task --------------------------
 // Lists out available tasks.
 // ---------------------------------------
 gulp.task('default', () => {
   // place code for your default task here
+})
+
+// Sass lint task
+gulp.task('lint:styles', () => {
+  return gulp.src(paths.assetsScss + '**/*.scss')
+    .pipe(sasslint({
+      configFile: paths.config + '.sass-lint.yml'
+    }))
+    .pipe(sasslint.format())
+    .pipe(sasslint.failOnError())
 })
