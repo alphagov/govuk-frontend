19c853f04d2fe8ca6abcff2778dc75ac23123ac2
Gemma Leigh <hello@gemmaleigh.com>
2017-05-23 09:42:20 +0100

    Add a dev to compile and watch styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
+'use strict'
+
 const paths = require('./config/paths.json')
 const gulp = require('gulp')
 const sasslint = require('gulp-sass-lint')
 const sass = require('gulp-sass')
 const runsequence = require('run-sequence')
 
-// Default task --------------------------
-// Lists out available tasks.
+// Styles build task ---------------------
+// Compiles CSS from Sass
+// Output both a minified and non-minified version into /public/stylesheets/
 // ---------------------------------------
-gulp.task('default', () => {
-  // place code for your default task here
+gulp.task('styles', cb => {
+  runsequence('scss:lint', 'scss:compile', cb)
 })
 
-// Sass lint task
-gulp.task('lint:styles', () => {
+gulp.task('scss:lint', () => {
   return gulp.src(paths.src + '**/*.scss')
     .pipe(sasslint({
       configFile: paths.config + '.sass-lint.yml'
@@ -21,18 +23,23 @@ gulp.task('lint:styles', () => {
     .pipe(sasslint.failOnError())
 })
 
-// Sass build task
-gulp.task('scss:build', () => {
+gulp.task('scss:compile', () => {
   return gulp.src(paths.globalScss + '**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(paths.distCss))
 })
 
-gulp.task('build:styles', cb => {
-  runsequence('lint:styles', 'scss:build', cb)
+// Watch task ----------------------------
+// When a file is changed, re-run the build task.
+// ---------------------------------------
+gulp.task('watch', () => {
+  gulp.watch([paths.src + '**/**/*.scss'], ['styles'])
 })
 
-// Sass watch task
-gulp.task('watch', () => {
-  gulp.watch([paths.src +'**/**/*.scss'], ['build:styles'])
+// Dev task --------------------------
+// Compiles assets and sets up watches.
+// ---------------------------------------
+gulp.task('dev', cb => {
+  runsequence('styles',
+              'watch', cb)
 })
