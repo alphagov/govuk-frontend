4b6a9aa8893bd2e17967377a7e5924000ab1ae65
Jani Kraner <jani.kraner@gmail.com>
2017-05-24 10:19:05 +0100

    Rudamentary server and concatenation of html
    Fixed a silly typo in gulpfile
    
    Injecting html from all components into a preview
    
    Utilising gulp-inject to grab html from all components and
    injecting into a index.html preview file that is served with
    gulp-live-server. Any change to component html will rebuild the
    preview file. Added to `gulp dev` task.
    
    Replaced function () with () =>
    
    Run compile when a new file is added or removed

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const gutil = require('gulp-util')
 const sasslint = require('gulp-sass-lint')
 const sass = require('gulp-sass')
 const runsequence = require('run-sequence')
+const gls = require('gulp-live-server')
+const inject = require('gulp-inject')
 
 // Styles build task ---------------------
 // Compiles CSS from Sass
@@ -35,6 +37,13 @@ gulp.task('scss:compile', () => {
 // ---------------------------------------
 gulp.task('watch', () => {
   gulp.watch([paths.src + '**/**/*.scss'], ['styles'])
+  const watcher = gulp.watch([paths.src + 'components/**/*.html'], ['combine:html'])
+  watcher.on('change', event => {
+    if (event.type === 'deleted') {
+      gulp.watch([paths.src + 'components/**/*.html'], ['combine:html'])
+    }
+    gulp.watch([paths.src + 'components/**/*.html'], ['combine:html'])
+  })
 })
 
 // Dev task --------------------------
@@ -42,7 +51,34 @@ gulp.task('watch', () => {
 // ---------------------------------------
 gulp.task('dev', cb => {
   runsequence('styles',
-              'watch', cb)
+              'watch',
+              'combine:html',
+              'serve', cb)
+})
+
+// Serve task --------------------------
+// Creates a server to preview components
+// ---------------------------------------
+gulp.task('serve', () => {
+  const server = gls.static(paths.dist, 8888)
+  server.start()
+  gulp.watch([paths.src + 'components/**/*.html'], ['combine:html'], file => {
+    server.notify.apply(server, [file])
+  })
+})
+
+// Combine html task --------------------------
+// Combines all html files in components into a single  file
+// ---------------------------------------
+gulp.task('combine:html', () => {
+  gulp.src(paths.dist + 'index.html')
+  .pipe(inject(gulp.src([paths.src + 'components/**/*.html']), {
+    starttag: '<!-- inject:html -->',
+    transform: function (filePath, file) {
+      return file.contents.toString('utf8')
+    }
+  }))
+  .pipe(gulp.dest(paths.dist))
 })
 
 // Default task --------------------------
