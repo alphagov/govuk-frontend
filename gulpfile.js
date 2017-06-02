55aee276a1d9bebd4c428ef4218aeb2f8a5c2528
Gemma Leigh <hello@gemmaleigh.com>
2017-06-02 12:31:16 +0100

    Add a task to check HTML using Tenon
    This will check the html of each component and output the result to the
    console.
    
    Use filter to set Tenon IDs to ignore.

--- gulpfile.js
+++ gulpfile.js
@@ -103,6 +103,22 @@ gulp.task('preview:components', () => {
   gulp.start('copy:images')
 })
 
+// Check HTML using Tenon ----------------
+// ---------------------------------------
+gulp.task('html:tenon', function () {
+  gulp.src('src/components/**/*.html', {read: false})
+  .pipe(gtenon({
+    key: 'fc7b85e07ea9b862c6422e412e999f3b',
+    snippet: true, // include errorSnippet in the console output
+    filter: [
+      3,  // Ignore: language of page is not set
+      31, // Ignore: link uses an invalid hypertext reference
+      64, // Ignore: page has no title
+      97  // Ignore: page has no headings
+    ]
+  }))
+})
+
 // Copy images --------------------------
 // Copy images to dist for component preview
 // ---------------------------------------
