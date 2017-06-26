8ad27ce791185ec0e85921e2d644b411a11d068e
Gemma Leigh <hello@gemmaleigh.com>
2017-06-26 18:21:35 +0100

    Set Heroku config vars for apps

--- review.js
+++ review.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+// Get config vars for Heroku apps
+var herokuAppReview = process.env.HEROKU_REVIEW
+var herokuAppDemo = process.env.HEROKU_DEMO
+
 // Define the port to run on
 app.set('port', (process.env.PORT || 3000))
 
-app.use('/', express.static(path.join(__dirname, 'preview')))
+// If this is the Heroku review app
+if (herokuAppReview === 'HEROKU_REVIEW') {
+  app.use('/', express.static(path.join(__dirname, 'preview')))
+}
+
+// If this is the Heroku demo app
+if (herokuAppDemo === 'HEROKU_DEMO') {
+  app.use('/', express.static(path.join(__dirname, 'demo')))
+}
+
 app.get('/', (req, res) => {
   res.render('index.html')
 })
