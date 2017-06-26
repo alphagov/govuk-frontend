5cdab5e96e389b831a71da0a77d18174a8bb1738
Gemma Leigh <hello@gemmaleigh.com>
2017-06-26 17:51:20 +0100

    Add a script for demo deployment

---
+++ demo.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+// Define the port to run on
+app.set('port', (process.env.PORT || 3000))
+
+app.use('/', express.static(path.join(__dirname, 'demo')))
+app.get('/', (req, res) => {
+  res.render('index.html')
+})
+
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'))
+})
