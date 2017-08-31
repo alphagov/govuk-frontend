67be6202e48704a7d3365071fffce9125c344160
Gemma Leigh <hello@gemmaleigh.com>
2017-08-31 13:53:39 +0100

    Use fs.readfile to get file contents
    Use this to save the component njk and component html files
    and pass these along with the component path to the view.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const nunjucks = require('nunjucks')
+const fs = require('fs')
 const path = require('path')
 const paths = require('./config/paths.json')
 const views = ['src/views', 'src/components', 'src/examples']
@@ -11,15 +12,10 @@ nunjucks.configure(views, {
   noCache: true,
   watch: true
 })
-app.set('view engine', 'njk')
-app.set('port', (process.env.PORT || 3000))
 
-// looks for html in views folder relative to current working directory
-nunjucks.configure(views, {
-  autoescape: true,
-  cache: false,
-  express: app
-})
+app.set('view engine', 'njk')
+
+app.set('port', (process.env.PORT || 3000))
 
 app.get('/', function (req, res) {
   res.render('default')
@@ -29,11 +25,27 @@ app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'))
 })
 
+// Components
 app.get('/components*', function (req, res) {
   var path = (req.params[0]).replace(/\//g, '')
-  res.render(path, function (err, html) {
+
+  // Get component path, component njk and component html files
+  try {
+
+    var componentNjk = fs.readFileSync('src/components/' + path + '/' + path + '.njk', 'utf8')
+    var componentHtml = fs.readFileSync('src/components/' + path + '/' + path + '.html', 'utf8')
+
+    res.locals.componentPath = path
+    res.locals.componentNunjucksFile = componentNjk
+    res.locals.componentHtmlFile = componentHtml
+
+  } catch (e) {
+    console.log('Error:', e.stack)
+  }
+
+  res.render(path, { componentPath: res.locals.componentPath, componentNunjucksFile: res.locals.componentNunjucksFile, componentHtmlFile: res.locals.componentHtmlFile }, function (err, html) {
     if (err) {
-      res.render(path + '/' + path, function (err2, html) {
+      res.render(path + '/' + 'index', function (err2, html) {
         if (err2) {
           res.status(404).send(err + '<br>' + err2)
         } else {
