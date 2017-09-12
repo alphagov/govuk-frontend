e21eaf1edb4287032b47e9f0b252c28aede69129
Jani Kraner <jani.kraner@gmail.com>
2017-09-12 08:52:03 +0100

    Compile:components task if condition change
    Change destination structure based on condition: if packages than packages/ else folder/components/

--- tasks/gulp/compile-components.js
+++ tasks/gulp/compile-components.js
@@ -5,10 +5,14 @@ const gulp = require('gulp')
 const nunjucks = require('gulp-nunjucks')
 const rename = require('gulp-rename')
 const taskArguments = require('./task-arguments')
+const gulpif = require('gulp-if')
+const flatten = require('gulp-flatten')
 
 // Compile Nunjucks ----------------------
 // Compile Nunjucks to HTML
 // ---------------------------------------
+
+const isPackages = (taskArguments.destination === 'packages') || false
 gulp.task('compile:components', () => {
   return gulp.src(
     [
@@ -19,5 +23,5 @@ gulp.task('compile:components', () => {
     ])
     .pipe(nunjucks.compile({ lstripBlocks: true, trimBlocks: true }))
     .pipe(rename({ extname: '.html' }))
-    .pipe(gulp.dest(taskArguments.destination + '/components/'))
+    .pipe(gulp.dest(gulpif(isPackages, taskArguments.destination, taskArguments.destination + '/components/')))
 })
