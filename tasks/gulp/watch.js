0e18ec70fb0929cb4fab802ee96b8f3a31e9df00
Jani Kraner <jani.kraner@gmail.com>
2017-09-14 08:36:13 +0100

    Update paths to configPaths for consistency

--- tasks/gulp/watch.js
+++ tasks/gulp/watch.js
@@ -1,12 +1,12 @@
 'use strict'
 const gulp = require('gulp')
-const paths = require('../../config/paths.json')
+const configPaths = require('../../config/paths.json')
 
 // Watch task ----------------------------
 // When a file is changed, re-run the build task.
 // ---------------------------------------
 gulp.task('watch', () => {
-  gulp.watch([paths.src + '**/**/*.scss'], ['styles'])
-  gulp.watch([paths.src + '**/**/*.js'], ['scripts'])
-  gulp.watch([paths.src + '**/**/*.njk'], ['generate:readme', 'compile:components'])
+  gulp.watch([configPaths.src + '**/**/*.scss'], ['styles'])
+  gulp.watch([configPaths.src + '**/**/*.js'], ['scripts'])
+  gulp.watch([configPaths.src + '**/**/*.njk'], ['generate:readme', 'compile:components'])
 })
