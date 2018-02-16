ec9acabaf750da6b50579d03790426087e1f1cfe
igloosi <jani.kraner@gmail.com>
2018-02-16 12:10:11 +0000

    default to /public as task argument

--- tasks/gulp/task-arguments.js
+++ tasks/gulp/task-arguments.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const argv = require('yargs').argv
-const destination = argv.destination ? argv.destination : 'tmp'
+const destination = argv.destination ? argv.destination : 'public'
 const isProduction = argv.production || false
 
 exports.destination = destination
