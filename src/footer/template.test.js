db423c8cc31111878f3d1e36a1e751914b3efc86
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-03-01 17:13:32 +0000

    Add meta feature to footer
    As seen on the GOV.UK Footer.

--- src/footer/template.test.js
+++ src/footer/template.test.js
@@ -13,4 +13,33 @@ describe('footer', () => {
     const results = await axe($.html())
     expect(results).toHaveNoViolations()
   })
+
+  describe('meta', () => {
+    it('passes accessibility tests', async () => {
+      const $ = render('footer', examples['with-meta'])
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+
+    it('renders heading', () => {
+      const $ = render('footer', examples['with-meta'])
+
+      const $component = $('.govuk-c-footer')
+      const $heading = $component.find('h2.govuk-h-visually-hidden')
+      expect($heading.text()).toEqual('Support links')
+    })
+
+    it('renders links', () => {
+      const $ = render('footer', examples['with-meta'])
+
+      const $component = $('.govuk-c-footer')
+      const $list = $component.find('ul.govuk-c-footer__inline-list')
+      const $items = $list.find('li.govuk-c-footer__inline-list-item')
+      const $firstItem = $items.find('a.govuk-c-footer__link:first-child')
+      expect($items.length).toEqual(3)
+      expect($firstItem.attr('href')).toEqual('#1')
+      expect($firstItem.text()).toContain('Item 1')
+    })
+  })
 })
