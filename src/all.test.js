85689cd307277f16ce395b2d2b6c08e25c7dff9a
Jani Kraner <jani.kraner@gmail.com>
2018-04-27 13:48:06 +0100

    Move all folder into root

--- src/all.test.js
+++ src/all.test.js
@@ -2,7 +2,7 @@
 
 const util = require('util')
 
-const configPaths = require('../../config/paths.json')
+const configPaths = require('../config/paths.json')
 
 const sass = require('node-sass')
 const sassRender = util.promisify(sass.render)
@@ -15,7 +15,7 @@ describe('GOV.UK Frontend', () => {
   describe('global styles', async() => {
     it('are disabled by default', async () => {
       const sass = `
-        @import "all/all";
+        @import "all";
       `
       const results = await sassRender({ data: sass, ...sassConfig })
       expect(results.css.toString()).not.toContain(', a {')
@@ -24,7 +24,7 @@ describe('GOV.UK Frontend', () => {
     it('are enabled if $global-styles variable is set to true', async () => {
       const sass = `
         $govuk-global-styles: true;
-        @import "all/all";
+        @import "all";
       `
       const results = await sassRender({ data: sass, ...sassConfig })
       expect(results.css.toString()).toContain(', a {')
