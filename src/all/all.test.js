563d373e925c043de0808348e2b62de5ff14e19b
Jani Kraner <jani.kraner@gmail.com>
2018-05-02 12:13:43 +0100

    Add tests to check output

---
+++ src/all/all.test.js
@@ -0,0 +1,34 @@
+/* eslint-env jest */
+
+const util = require('util')
+
+const configPaths = require('../../config/paths.json')
+
+const sass = require('node-sass')
+const sassRender = util.promisify(sass.render)
+
+const sassConfig = {
+  includePaths: [ configPaths.src ]
+}
+
+describe('GOV.UK Frontend', () => {
+  describe('global styles', async() => {
+    it('are disabled by default', async () => {
+      const sass = `
+        @import "all/all";
+      `
+      const results = await sassRender({ data: sass, ...sassConfig })
+      expect(results.css.toString()).not.toContain(', a {')
+      expect(results.css.toString()).not.toContain(', p {')
+    })
+    it('are enabled if $global-styles variable is set to true', async () => {
+      const sass = `
+        $govuk-global-styles: true;
+        @import "all/all";
+      `
+      const results = await sassRender({ data: sass, ...sassConfig })
+      expect(results.css.toString()).toContain(', a {')
+      expect(results.css.toString()).toContain(', p {')
+    })
+  })
+})
