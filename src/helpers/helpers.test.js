8a5960577e60ec1290a6df92c42f1a2593e57203
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2018-05-11 16:52:55 +0100

    Add test to ensure helpers layer doesn't output CSS

---
+++ src/helpers/helpers.test.js
@@ -0,0 +1,18 @@
+/* eslint-env jest */
+
+const path = require('path')
+const util = require('util')
+
+const sass = require('node-sass')
+const sassRender = util.promisify(sass.render)
+
+const configPaths = require('../../config/paths.json')
+
+describe('The helpers layer', () => {
+  it('should not output any CSS', async () => {
+    const helpers = path.join(configPaths.src, 'helpers', '_all.scss')
+
+    const output = await sassRender({ file: helpers })
+    expect(output.css.toString()).toEqual('')
+  })
+})
