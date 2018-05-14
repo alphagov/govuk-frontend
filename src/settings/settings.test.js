850996eff24c2264b4cd3bdbc0befe9f07e7cae8
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2018-05-14 12:00:55 +0100

    Add test to ensure settings layer doesn't output CSS

---
+++ src/settings/settings.test.js
@@ -0,0 +1,18 @@
+/* eslint-env jest */
+
+const path = require('path')
+const util = require('util')
+
+const sass = require('node-sass')
+const sassRender = util.promisify(sass.render)
+
+const configPaths = require('../../config/paths.json')
+
+describe('The settings layer', () => {
+  it('should not output any CSS', async () => {
+    const settings = path.join(configPaths.src, 'settings', '_all.scss')
+
+    const output = await sassRender({ file: settings })
+    expect(output.css.toString()).toEqual('')
+  })
+})
