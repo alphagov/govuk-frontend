307c86a244a755b3811e0259ae796060552ce705
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2018-06-01 16:32:13 +0100

    Group settings by sub-category

--- tasks/gulp/sassdoc.js
+++ tasks/gulp/sassdoc.js
@@ -1,15 +1,21 @@
 const sassdoc = require('sassdoc')
 const gulp = require('gulp')
 const paths = require('../../config/paths.json')
-const debug = require('gulp-debug')
 
 gulp.task('sassdoc', function () {
   return gulp.src([paths.src + '**/**/*.scss', `!${paths.src}/vendor/*`])
-    .pipe(debug())
     .pipe(sassdoc({
       dest: paths.sassdoc,
       groups: {
-        settings: 'Settings',
+        'settings/assets': 'Settings / Assets',
+        'settings/colours': 'Settings / Colours',
+        'settings/compatibility': 'Settings / Compatibility',
+        'settings/global-styles': 'Settings / Global Styles',
+        'settings/ie8': 'Settings / IE8',
+        'settings/measurements': 'Settings / Measurements',
+        'settings/media-queries': 'Settings / Media Queries',
+        'settings/spacing': 'Settings / Spacing',
+        'settings/typography': 'Settings / Typography',
         tools: 'Tools',
         helpers: 'Helpers',
         overrides: 'Overrides'
