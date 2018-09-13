e3bf0d0703851e934cee454f02cd9f4d4a268b9b
Hanna Laakso <hannalaakso123@yahoo.co.uk>
2018-09-13 16:18:23 +0100

    Transform component `params` into JSON and copy to `package`
    Call files `macro-options.json` as only that data is copied over to package.

--- tasks/gulp/copy-to-destination.js
+++ tasks/gulp/copy-to-destination.js
@@ -6,8 +6,14 @@ const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 const taskArguments = require('./task-arguments')
 const filter = require('gulp-filter')
+const fs = require('fs')
+const yamlToJson = require('js-yaml')
+const path = require('path')
+const map = require('map-stream')
+const rename = require('gulp-rename')
 
 let scssFiles = filter([configPaths.src + '**/*.scss'], {restore: true})
+let yamlFiles = filter([configPaths.components + '**/*.yaml'], {restore: true})
 
 gulp.task('copy-files', () => {
   return gulp.src([
@@ -16,7 +22,6 @@ gulp.task('copy-files', () => {
     '!**/*.test.js',
     '!' + configPaths.src + 'README.md', // Don't override the existing README in /package
     '!' + configPaths.components + '**/README.njk',
-    '!' + configPaths.components + '**/*.{yml,yaml}',
     '!' + configPaths.components + '**/__snapshots__/**',
     '!' + configPaths.components + '**/__snapshots__/'
   ])
@@ -25,5 +30,36 @@ gulp.task('copy-files', () => {
       autoprefixer
     ], {syntax: require('postcss-scss')}))
     .pipe(scssFiles.restore)
+    .pipe(yamlFiles)
+    .pipe(map(function (file, done) {
+      let componentName = path.dirname(file.path).split(path.sep).slice(-1).toString()
+      let componentPath = path.join(configPaths.components, componentName, `${componentName}.yaml`)
+      let yaml
+      let json
+      let paramsJson
+
+      try {
+        yaml = fs.readFileSync(componentPath, {encoding: 'utf8', json: true})
+      } catch (e) {
+        console.error('ENOENT: no such file or directory: ', componentPath)
+      }
+
+      if (yaml) {
+        json = yamlToJson.safeLoad(yaml)
+        paramsJson = json.params // We only want the 'params' data from component yaml
+
+        if (paramsJson) {
+          file.contents = Buffer.from(JSON.stringify(paramsJson, null, 4))
+        } else {
+          console.error(componentPath + ' is missing "params"')
+        }
+      }
+      done(null, file)
+    }))
+    .pipe(rename(path => {
+      path.basename = 'macro-options'
+      path.extname = '.json'
+    }))
+    .pipe(yamlFiles.restore)
     .pipe(gulp.dest(taskArguments.destination + '/'))
 })
