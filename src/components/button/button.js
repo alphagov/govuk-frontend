bf22fba7871ace6239b14d8a43d5338c6047f7de
Chris Hill-Scott <me@quis.cc>
2018-10-02 16:12:03 +0100

    Prevent doubling clicking form submissions
    Have seen users complaining that they got an invitation email twice.
    This is probably because they clicked the ‘send invite’ button twice
    even though they think they only clicked it once.
    
    Double form submission is a common issue on web pages, and there are a
    number of different ways to prevent it. I’ve chosen to do it this way
    because:
    - temporarily, not permanently disabling the button means that this
      addresses the double clicking issue without breaking things if the
      user did, really want to click the button again deliberately (for
      whatever reason)
    - doing it with a `data` attribute, rather than the `disabled` attribute
      means that the interaction behaviour of the button doesn’t change (
      `disabled` buttons can’t be focused, for example)
    
    Adapted from https://github.com/alphagov/notifications-admin/blob/93e7f9213533790ffb52a8f3afadbace9145eea2/app/assets/javascripts/preventDuplicateFormSubmissions.js

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -11,6 +11,8 @@
 import '../../vendor/polyfills/Event' // addEventListener and event.target normaliziation
 
 var KEY_SPACE = 32
+var DEBOUNCE_TIMEOUT_IN_SECONDS = 1
+var debounceFormSubmitTimer = null
 
 function Button ($module) {
   this.$module = $module
@@ -34,11 +36,46 @@ Button.prototype.handleKeyDown = function (event) {
 }
 
 /**
+* Add event handler for click
+* If the click quickly succeeds a previous click then nothing will happen.
+* This stops people accidentally causing multiple form submissions by
+* double clicking buttons.
+*/
+Button.prototype.debounce = function (event) {
+  // Only handle events on <input> and <button> elements
+  var tagName = event.target.tagName
+  if (tagName !== 'INPUT' && tagName !== 'BUTTON') {
+    return
+  }
+
+  var $button = event.target
+
+  // We only want to to handle submit buttons that are used in forms
+  var isSubmitButton = $button.type === 'submit'
+  var isInsideAForm = $button.form
+
+  if (!isSubmitButton || !isInsideAForm) {
+    return
+  }
+
+  // If the timer is still running then we want to prevent the click from submitting the form
+  if (debounceFormSubmitTimer) {
+    event.preventDefault()
+    return false
+  }
+
+  debounceFormSubmitTimer = setTimeout(function () {
+    debounceFormSubmitTimer = null
+  }, DEBOUNCE_TIMEOUT_IN_SECONDS * 1000)
+}
+
+/**
 * Initialise an event listener for keydown at document level
 * this will help listening for later inserted elements with a role="button"
 */
 Button.prototype.init = function () {
   this.$module.addEventListener('keydown', this.handleKeyDown)
+  this.$module.addEventListener('click', this.debounce)
 }
 
 export default Button
