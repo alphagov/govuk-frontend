b5060993d416f23eff313650b898f65b39bb4c2f
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-11-15 13:48:04 +0000

    Remove implicit dependency on Element for classList

--- src/vendor/polyfills/Element/prototype/classList.js
+++ src/vendor/polyfills/Element/prototype/classList.js
@@ -1,5 +1,6 @@
 import '../../Object/defineProperty'
 import '../../DOMTokenList'
+import '../../Element'
 
 (function(undefined) {
 
@@ -14,7 +15,7 @@ import '../../DOMTokenList'
 
     if (detect) return
 
-    // Polyfill from https://raw.githubusercontent.com/Financial-Times/polyfill-service/8717a9e04ac7aff99b4980fbedead98036b0929a/packages/polyfill-library/polyfills/Element/prototype/classList/polyfill.js
+    // Polyfill from https://cdn.polyfill.io/v2/polyfill.js?features=Element.prototype.classList&flags=always
     (function (global) {
       var dpSupport = true;
       var defineGetter = function (object, name, fn, configurable) {
