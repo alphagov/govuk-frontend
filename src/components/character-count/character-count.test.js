755a205eb9cd994a9ec73f5f6fa763c80ae50ac2
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2018-12-21 13:45:56 +0000

    Define component URL in one place

--- src/components/character-count/character-count.test.js
+++ src/components/character-count/character-count.test.js
@@ -5,10 +5,10 @@
 
 const configPaths = require('../../../config/paths.json')
 const PORT = configPaths.ports.test
+const componentUrl = `http://localhost:${PORT}/components/character-count/preview`
 
 let browser
 let page
-let baseUrl = 'http://localhost:' + PORT
 
 beforeAll(async () => {
   browser = global.__BROWSER__
@@ -21,7 +21,6 @@ afterAll(async () => {
 
 describe('Character count', () => {
   describe('when JavaScript is unavailable or fails', () => {
-
     beforeAll(async () => {
       await page.setJavaScriptEnabled(false)
     })
@@ -31,7 +30,7 @@ describe('Character count', () => {
     })
 
     it('shows the static message', async () => {
-      await page.goto(baseUrl + '/components/character-count/preview', { waitUntil: 'load' })
+      await page.goto(componentUrl, { waitUntil: 'load' })
       const message = await page.$eval('.govuk-character-count__message', el => el.innerHTML.trim())
 
       expect(message).toEqual('You can enter up to 10 characters')
@@ -40,7 +39,7 @@ describe('Character count', () => {
 
   describe('when JavaScript is available', () => {
     it('shows the dynamic message', async () => {
-      await page.goto(baseUrl + '/components/character-count/preview', { waitUntil: 'load' })
+      await page.goto(componentUrl, { waitUntil: 'load' })
 
       const message = await page.$eval('.govuk-character-count__message', el => el.innerHTML.trim())
 
@@ -49,7 +48,7 @@ describe('Character count', () => {
 
     describe('when within the character limit', () => {
       it('counts down to the character limit', async () => {
-        await page.goto(baseUrl + '/components/character-count/preview', { waitUntil: 'load' })
+        await page.goto(componentUrl, { waitUntil: 'load' })
         await page.type('.js-character-count', 'A')
 
         const message = await page.$eval('.govuk-character-count__message', el => el.innerHTML.trim())
@@ -61,25 +60,22 @@ describe('Character count', () => {
     describe('when the character limit is exceeded', () => {
       beforeAll(async () => {
         // Type 11 characters into the character count
-        await page.goto(baseUrl + '/components/character-count/preview', { waitUntil: 'load' })
+        await page.goto(componentUrl, { waitUntil: 'load' })
         await page.type('.js-character-count', 'A'.repeat(11))
       })
 
       it('shows the number of characters over the limit', async () => {
         const message = await page.$eval('.govuk-character-count__message', el => el.innerHTML.trim())
-
         expect(message).toEqual('You have 1 character too many')
       })
 
       it('adds error styles to the textarea', async () => {
         const textareaClasses = await page.$eval('.js-character-count', el => el.className)
-
         expect(textareaClasses).toContain('govuk-textarea--error')
       })
 
       it('adds error styles to the count message', async () => {
         const messageClasses = await page.$eval('.govuk-character-count__message', el => el.className)
-
         expect(messageClasses).toContain('govuk-error-message')
       })
     })
