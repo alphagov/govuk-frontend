5855de113f7cbac0e0f12af52ac8112df41c52b3
Ernest Walzel <ewalzel@gmail.com>
2019-01-14 23:31:02 +0100

    Add ekosystem.staging to known hosts

--- .github/deploy/entrypoint.sh
+++ .github/deploy/entrypoint.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-mkdir ~/.ssh && ssh-keyscan -v ekosystem.staging.slovensko.digital >> ~/.ssh/known_hosts
+mkdir ~/.ssh
+echo ekosystem.staging.slovensko.digital ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMtqTn27l7YyPbzNAry/6eAAo+1n4dNL3eTJv8+PJja3BtnT94HaNhGSriecn2Euu1uQxIT9OQ8nxfpq1yQn8fhbUl+1SYDaMH2P/Lavh5++R5mkeH3G+GmrJOY0aP6651cPPz2SpICw38gooaKu6SqV/GyDBokVidQv27iUWUixNi0kfBwAgjW4EGNjPv+sdYN+lPVrvmVXR3IEU107JCSGf//OrRykeELUZm5vlZjwENvjt8HGa6t3KPL+2yzQXzwI+ww/1axHJHLDRF2rA2g109MJiuL5+KRHJud5d6lsPk82nrQF3EI3uzME7IxlVUWQDN2wGt6V47/NCxLYAb >> ~/.ssh/known_hosts
 gpg -q --batch --yes --decrypt --passphrase="$SSH_KEY_DECRYPT_PASSPHRASE" -o ~/.ssh/id_rsa /id_rsa.gpg
 
 git --git-dir=/navody-frontend/.git --work-tree=/navody-frontend $*
