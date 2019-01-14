65f17c8e8362d875f9f1e5c0594852d8db57bd57
Ernest Walzel <ewalzel@gmail.com>
2019-01-14 23:56:58 +0100

    Make deployment to staging work

--- .github/deploy/entrypoint.sh
+++ .github/deploy/entrypoint.sh
@@ -2,9 +2,11 @@
 
 set -eu
 
-mkdir ~/.ssh
-echo ekosystem.staging.slovensko.digital ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMtqTn27l7YyPbzNAry/6eAAo+1n4dNL3eTJv8+PJja3BtnT94HaNhGSriecn2Euu1uQxIT9OQ8nxfpq1yQn8fhbUl+1SYDaMH2P/Lavh5++R5mkeH3G+GmrJOY0aP6651cPPz2SpICw38gooaKu6SqV/GyDBokVidQv27iUWUixNi0kfBwAgjW4EGNjPv+sdYN+lPVrvmVXR3IEU107JCSGf//OrRykeELUZm5vlZjwENvjt8HGa6t3KPL+2yzQXzwI+ww/1axHJHLDRF2rA2g109MJiuL5+KRHJud5d6lsPk82nrQF3EI3uzME7IxlVUWQDN2wGt6V47/NCxLYAb >> ~/.ssh/known_hosts
-cat ~/.ssh/known_hosts
-gpg -q --batch --yes --decrypt --passphrase="$SSH_KEY_DECRYPT_PASSPHRASE" -o ~/.ssh/id_rsa /id_rsa.gpg
+mv /ssh /root/.ssh
+
+gpg -q --batch --yes --decrypt --passphrase="$SSH_KEY_DECRYPT_PASSPHRASE" -o /root/.ssh/id_rsa /root/.ssh/id_rsa.gpg
+
+chmod 600 /root/.ssh/*
+chmod 700 /root/.ssh
 
 git --git-dir=/navody-frontend/.git --work-tree=/navody-frontend $*
--- .github/main.workflow
+++ .github/main.workflow
@@ -11,6 +11,6 @@ action "Master branch" {
 action "Deploy to Staging" {
   needs = "Master branch"
   uses = "./.github/deploy"
-  args = "push -v staging"
+  args = "push staging"
   secrets = ["SSH_KEY_DECRYPT_PASSPHRASE"]
 }
