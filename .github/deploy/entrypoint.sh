f92dcaedbb347a39fe1351b36c42310d3c5e79f9
Ernest Walzel <ewalzel@gmail.com>
2019-01-14 23:18:06 +0100

    fix entrypoint

--- .github/deploy/entrypoint.sh
+++ .github/deploy/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-RUN mkdir ~/.ssh && ssh-keyscan ekosystem.staging.slovensko.digital >> ~/.ssh/known_hosts
+mkdir ~/.ssh && ssh-keyscan ekosystem.staging.slovensko.digital >> ~/.ssh/known_hosts
 gpg -q --batch --yes --decrypt --passphrase="$SSH_KEY_DECRYPT_PASSPHRASE" -o ~/.ssh/id_rsa /id_rsa.gpg
 
 git --git-dir=/navody-frontend/.git --work-tree=/navody-frontend $*
