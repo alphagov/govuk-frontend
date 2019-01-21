c4c403c1200ab987e9c01b40b0a593367cf188a1
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2019-01-21 17:11:44 +0000

    Fix Checkboxes and Radios targeting selectors outside it's scope
    We only want conditional reveals that are within the module markup to be impacted by changes to inputs.
    
    This scopes the querySelector only to this module.

--- src/components/checkboxes/checkboxes.js
+++ src/components/checkboxes/checkboxes.js
@@ -40,8 +40,10 @@ Checkboxes.prototype.setAttributes = function ($input) {
   var inputIsChecked = $input.checked
   $input.setAttribute('aria-expanded', inputIsChecked)
 
-  var $content = document.querySelector('#' + $input.getAttribute('aria-controls'))
-  $content.classList.toggle('govuk-checkboxes__conditional--hidden', !inputIsChecked)
+  var $content = this.$module.querySelector('#' + $input.getAttribute('aria-controls'))
+  if ($content) {
+    $content.classList.toggle('govuk-checkboxes__conditional--hidden', !inputIsChecked)
+  }
 }
 
 Checkboxes.prototype.handleClick = function (event) {
