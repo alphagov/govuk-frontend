839b66805202c0e671dbddb873da94fca4cdd0fa
Nick Colley <nickcolley7@gmail.com>
2019-02-18 13:42:20 +0000

    Refactor full page examples, split from main app
    Makes it easier to refactor these pages later, without
    changing the structure too much.
    
    Potentially it might be nice to have a /pages directory,
    rather than views similar to how our components are structured.

---
+++ app/full-page-examples.js
@@ -0,0 +1,20 @@
+module.exports = (app) => {
+  require('./views/full-page-examples/feedback-page')(app)
+  require('./views/full-page-examples/how-do-you-want-to-sign-in')(app)
+  require('./views/full-page-examples/news-and-communications')(app)
+  require('./views/full-page-examples/passport-details')(app)
+  require('./views/full-page-examples/upload-your-photo')(app)
+  require('./views/full-page-examples/what-is-your-nationality')(app)
+  require('./views/full-page-examples/what-is-your-postcode')(app)
+
+  // Display full page examples index by default if not handled already
+  app.get('/full-page-examples/:example', function (req, res, next) {
+    res.render(`${req.params.example}/index`, function (error, html) {
+      if (error) {
+        next(error)
+      } else {
+        res.send(html)
+      }
+    })
+  })
+}
