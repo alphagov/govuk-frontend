8edfb5a51dbd862aa533476e7efe5425a8ae557a
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2019-02-21 11:23:34 +0000

    Shuffle results order based on filters
    Add small form inputs to view.
    
    Remove error validation from filters as we cover this with
    another example.
    
    Co-authored-by: Nick Colley <nick.colley@digital.cabinet-office.gov.uk>

--- app/views/full-page-examples/news-and-communications/index.js
+++ app/views/full-page-examples/news-and-communications/index.js
@@ -1,38 +1,32 @@
+const shuffleSeed = require('shuffle-seed')
+
+const { documents } = require('./data.json')
+
 module.exports = (app) => {
   app.get(
     '/full-page-examples/news-and-communications',
     (request, response) => {
-      const { order, brexit } = request.query
-      let source = 'most-viewed'
-      let errors = {}
-      if (
-        order === 'most-viewed' ||
-        order === 'updated-oldest' ||
-        order === 'updated-newest'
-      ) {
-        source = order
+      let { order, brexit, organisation } = request.query
+      if (!order) {
+        order = 'most-viewed'
       }
-      if (order === 'default' && !brexit) {
-        errors = {
-          'order': {
-            href: '#order',
-            value: 'default',
-            text: 'Select an option'
-          }
-        }
-      }
-      // If brexit true on forget about the source order, which is OK for a demonstration page.
-      if (brexit) {
-        source = 'related-to-brexit'
-      }
-      const { total, documents } = require(`./data/${source}.json`)
+
+      // Shuffle the documents based on the query string, to simulate different responses.
+      const seed = order + brexit + organisation
+      const shuffledDocuments = shuffleSeed.shuffle(documents, seed)
+
+      const total = '128124'
+      // Shuffle the total based on the query string
+      const randomizedTotal = shuffleSeed.shuffle(total.split(''), seed).join('')
+      // Make the total more readable
+      const formattedTotal = randomizedTotal.substring(0, 3) + ',' + randomizedTotal.substring(3)
+
       response.render('./full-page-examples/news-and-communications/index', {
-        total,
-        documents,
+        total: formattedTotal,
+        documents: shuffledDocuments,
         order,
         brexit,
-        errors,
-        errorSummary: Object.values(errors)
+        values: request.query // In production this should sanitized.
       })
     }
   )
