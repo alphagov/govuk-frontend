98e87040013cc15483be26ffbb399eef2679822d
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2019-02-21 12:02:03 +0000

    Remove all other examples from review app
    We only want to to test News and Comms on this branch

--- app/full-page-examples.js
+++ app/full-page-examples.js
@@ -1,15 +1,5 @@
 module.exports = (app) => {
-  require('./views/full-page-examples/have-you-changed-your-name')(app)
-  require('./views/full-page-examples/feedback')(app)
-  require('./views/full-page-examples/how-do-you-want-to-sign-in')(app)
   require('./views/full-page-examples/news-and-communications')(app)
-  require('./views/full-page-examples/passport-details')(app)
-  require('./views/full-page-examples/update-your-account-details')(app)
-  require('./views/full-page-examples/upload-your-photo')(app)
-  require('./views/full-page-examples/what-is-your-address')(app)
-  require('./views/full-page-examples/what-is-your-nationality')(app)
-  require('./views/full-page-examples/what-is-your-postcode')(app)
-  require('./views/full-page-examples/what-was-the-last-country-you-visited')(app)
 
   // Display full page examples index by default if not handled already
   app.get('/full-page-examples/:example', function (req, res, next) {
