8b4c7c402cbebd61c42bf1e314339a895660354f
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2019-03-05 10:50:55 +0000

    Test that core can be imported individually

---
+++ src/core/core.test.js
@@ -0,0 +1,15 @@
+/* eslint-env jest */
+
+const util = require('util')
+const glob = require('glob')
+
+const configPaths = require('../../config/paths.json')
+
+const sass = require('node-sass')
+const sassRender = util.promisify(sass.render)
+
+const sassFiles = glob.sync(`${configPaths.src}/core/**/*.scss`)
+
+it.each(sassFiles)('%s renders to CSS without errors', (file) => {
+  return sassRender({ file: file, includePaths: [ configPaths.src ] })
+})
