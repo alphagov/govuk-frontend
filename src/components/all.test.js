501e25dbd219524aafe78bac8e8e00bfd806c763
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2019-03-05 11:37:49 +0000

    Simplify Sass render calls

--- src/components/all.test.js
+++ src/components/all.test.js
@@ -1,21 +1,13 @@
 /* eslint-env jest */
 
-const util = require('util')
-
 const { allComponents } = require('../../lib/file-helper')
+const { renderSass } = require('../../lib/jest-helpers')
 
 const configPaths = require('../../config/paths.json')
 
 // We can't use the render function from jest-helpers, because we need control
 // over the nunjucks environment.
 const nunjucks = require('nunjucks')
-
-const sass = require('node-sass')
-const sassRender = util.promisify(sass.render)
-
-const sassConfig = {
-  includePaths: [ configPaths.src ]
-}
 
 describe('When nunjucks is configured with a different base path', () => {
   beforeAll(() => {
@@ -32,8 +24,7 @@ describe('When nunjucks is configured with a different base path', () => {
 })
 
 it.each(allComponents)('%s.scss renders to CSS without errors', (component) => {
-  return sassRender({
-    file: `${configPaths.src}/components/${component}/_${component}.scss`,
-    ...sassConfig
+  return renderSass({
+    file: `${configPaths.src}/components/${component}/_${component}.scss`
   })
 })
--- src/objects/objects.test.js
+++ src/objects/objects.test.js
@@ -1,15 +1,11 @@
 /* eslint-env jest */
 
-const util = require('util')
 const glob = require('glob')
-
+const { renderSass } = require('../../lib/jest-helpers')
 const configPaths = require('../../config/paths.json')
-
-const sass = require('node-sass')
-const sassRender = util.promisify(sass.render)
 
 const sassFiles = glob.sync(`${configPaths.src}/objects/**/*.scss`)
 
 it.each(sassFiles)('%s renders to CSS without errors', (file) => {
-  return sassRender({ file: file, includePaths: [ configPaths.src ] })
+  return renderSass({ file: file })
 })
