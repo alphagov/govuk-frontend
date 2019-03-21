9c0099f71fca90ec9d29941d009da51d4e8c2b6a
pom <>
2019-03-21 17:54:57 +0100

    Add appear-link and dynamic feedbackbar

--- src/utilities/appear-link/appear-link.js
+++ src/utilities/appear-link/appear-link.js
@@ -18,32 +18,20 @@ SndAppearLink.prototype.init = function() {
 
   this.$appear = document.getElementById(appearId);
   this.$disappear = document.getElementById(disappearId);
-
-  console.log($module, appearId, disappearId, this.$appear, this.$disappear);
 };
 
 SndAppearLink.prototype.handleClick = function(event) {
   event.preventDefault();
 
   if (this.$appear) {
-    this.$appear.classList.remove("appear-link-hide");
-    this.$appear.classList.add("appear-link-show");
+    this.$appear.classList.remove("sdn-appear-link-hide");
   }
   if (this.$disappear) {
-    this.$disappear.classList.remove("appear-link-show");
-    this.$disappear.classList.add("appear-link-hide");
+    this.$disappear.classList.add("sdn-appear-link-hide");
   }
-  console.log(this.$appear, this.$disappear);
 
-  // this.parentNode.classList.toggle("sdn-timeline__step--dropdown-active");
+  console.log(this.$appear, this.$disappear)
+
 };
-
-// SndAppearLink.prototype.handleBlur = function (event) {
-//   event.preventDefault()
-
-//   setTimeout(function () {
-//     this.parentNode.classList.remove('sdn-timeline__step--dropdown-active')
-//   }.bind(this), 100)
-// }
 
 export default SndAppearLink;
