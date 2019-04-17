fbce8e2f4ff38bc8de7eaffa805d3b49c70c1b2b
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-04-17 15:53:35 +0100

    Add a legacy mode to review app
    Legacy mode is a means to test legacy css (GOV.UK Elements, GOV.UK template
    etc.) along side govuk-frontend.  The review app will by default only use
    govuk-frontend. However if you add the query string of `?legacy=1` to
    the address the application should load govuk-elements, govuk-template and
    govuk-frontend. It also also remember the state are you navigate through
    the review app.

--- app/app.js
+++ app/app.js
@@ -71,6 +71,17 @@ module.exports = (options) => {
   // Handle the banner component serverside.
   require('./banner.js')(app)
 
+  // Define middleware for all routes
+  app.use('*', function (request, response, next) {
+    response.locals.legacy = request.query['legacy'] === '1'
+    if (response.locals.legacy) {
+      response.locals.legacy_query = '?legacy=1'
+    } else {
+      response.locals.legacy_query = ''
+    }
+    next()
+  })
+
   // Define routes
 
   // Index page - render the component list template
