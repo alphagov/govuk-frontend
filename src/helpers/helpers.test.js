513d29a75b18a09c0a0d9acad8a261b995bd57a4
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2019-05-28 11:37:42 +0100

    Add tests to ensure all layers can be compiled individually

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -2,8 +2,12 @@
 
 const path = require('path')
 
+const glob = require('glob')
+
 const { renderSass } = require('../../lib/jest-helpers')
 const configPaths = require('../../config/paths.json')
+
+const sassFiles = glob.sync(`${configPaths.src}/helpers/**/*.scss`)
 
 describe('The helpers layer', () => {
   it('should not output any CSS', async () => {
@@ -11,5 +15,9 @@ describe('The helpers layer', () => {
 
     const output = await renderSass({ file: helpers })
     expect(output.css.toString()).toEqual('')
+  })
+
+  it.each(sassFiles)('%s renders to CSS without errors', (file) => {
+    return renderSass({ file: file })
   })
 })
--- src/tools/tools.test.js
+++ src/tools/tools.test.js
@@ -2,8 +2,12 @@
 
 const path = require('path')
 
+const glob = require('glob')
+
 const { renderSass } = require('../../lib/jest-helpers')
 const configPaths = require('../../config/paths.json')
+
+const sassFiles = glob.sync(`${configPaths.src}/tools/**/*.scss`)
 
 describe('The tools layer', () => {
   it('should not output any CSS', async () => {
@@ -11,5 +15,9 @@ describe('The tools layer', () => {
 
     const output = await renderSass({ file: tools })
     expect(output.css.toString()).toEqual('')
+  })
+
+  it.each(sassFiles)('%s renders to CSS without errors', (file) => {
+    return renderSass({ file: file })
   })
 })
