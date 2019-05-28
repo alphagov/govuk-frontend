b9a79a2ebde4e1b231bd3b886be2fa6416d2c97f
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2019-05-28 11:37:42 +0100

    Add tests to ensure all layers can be compiled individually

--- src/settings/settings.test.js
+++ src/settings/settings.test.js
@@ -2,8 +2,12 @@
 
 const path = require('path')
 
+const glob = require('glob')
+
 const { renderSass } = require('../../lib/jest-helpers')
 const configPaths = require('../../config/paths.json')
+
+const sassFiles = glob.sync(`${configPaths.src}/settings/**/*.scss`)
 
 describe('The settings layer', () => {
   it('should not output any CSS', async () => {
@@ -11,5 +15,9 @@ describe('The settings layer', () => {
 
     const output = await renderSass({ file: settings })
     expect(output.css.toString()).toEqual('')
+  })
+
+  it.each(sassFiles)('%s renders to CSS without errors', (file) => {
+    return renderSass({ file: file })
   })
 })
