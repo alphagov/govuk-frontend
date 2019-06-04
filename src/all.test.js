eff7b5d5680fe644550278440046f91a14f45ccb
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2019-06-04 17:21:32 +0100

    Make checkboxes easier to link to from error summary
    If the user has not specified an id for the first item, it will by default render without a suffix number which will make it easier to link to from the error summary as it'll match the `name` attribute.

--- src/all.test.js
+++ src/all.test.js
@@ -77,20 +77,20 @@ describe('GOV.UK Frontend', () => {
       // that we can interact with to check if they're interactive.
 
       // Check that the conditional reveal component has a conditional section that would open if enhanced.
-      await page.waitForSelector('#conditional-not-scoped-1', { hidden: true })
+      await page.waitForSelector('#conditional-not-scoped', { hidden: true })
 
-      await page.click('[for="not-scoped-1"]')
+      await page.click('[for="not-scoped"]')
 
       // Check that when it is clicked that nothing opens, which shows that it has not been enhanced.
-      await page.waitForSelector('#conditional-not-scoped-1', { hidden: true })
+      await page.waitForSelector('#conditional-not-scoped', { hidden: true })
 
       // Check the other conditional reveal which has been enhanced based on it's scope.
-      await page.waitForSelector('#conditional-scoped-1', { hidden: true })
+      await page.waitForSelector('#conditional-scoped', { hidden: true })
 
-      await page.click('[for="scoped-1"]')
+      await page.click('[for="scoped"]')
 
       // Check that it has opened as expected.
-      await page.waitForSelector('#conditional-scoped-1', { hidden: false })
+      await page.waitForSelector('#conditional-scoped', { hidden: false })
     })
   })
   describe('global styles', () => {
