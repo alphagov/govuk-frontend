e17c0a89bc48234a2d9839e18422ba3f97bf00aa
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-08-19 16:30:18 +0100

    Details

--- src/govuk/components/details/template.test.js
+++ src/govuk/components/details/template.test.js
@@ -24,6 +24,27 @@ describe('Details', () => {
     expect($component.get(0).tagName).toEqual('details')
   })
 
+  it('renders with a custom id', () => {
+    const $ = render('details', examples.id)
+
+    const $component = $('.govuk-details')
+    expect($component.attr('id')).toEqual('my-details-element')
+  })
+
+  it('is collapsed by default', () => {
+    const $ = render('details', examples.default)
+
+    const $component = $('.govuk-details')
+    expect($component.attr('open')).toBeFalsy()
+  })
+
+  it('can be opened by default', () => {
+    const $ = render('details', examples.expanded)
+
+    const $component = $('.govuk-details')
+    expect($component.attr('open')).toBeTruthy()
+  })
+
   it('includes a nested summary', () => {
     const $ = render('details', examples.default)
 
@@ -33,57 +54,42 @@ describe('Details', () => {
   })
 
   it('allows text to be passed whilst escaping HTML entities', () => {
-    const $ = render('details', {
-      text: 'More about the greater than symbol (>)'
-    })
+    const $ = render('details', examples['html as text'])
 
     const detailsText = $('.govuk-details__text').html().trim()
     expect(detailsText).toEqual('More about the greater than symbol (&gt;)')
   })
 
   it('allows HTML to be passed un-escaped', () => {
-    const $ = render('details', {
-      html: 'More about <b>bold text</b>'
-    })
+    const $ = render('details', examples.html)
 
     const detailsText = $('.govuk-details__text').html().trim()
     expect(detailsText).toEqual('More about <b>bold text</b>')
   })
 
   it('allows summary text to be passed whilst escaping HTML entities', () => {
-    const $ = render('details', {
-      summaryText: 'The greater than symbol (>) is the best'
-    })
+    const $ = render('details', examples['summary html as text'])
 
     const detailsText = $('.govuk-details__summary-text').html().trim()
     expect(detailsText).toEqual('The greater than symbol (&gt;) is the best')
   })
 
   it('allows summary HTML to be passed un-escaped', () => {
-    const $ = render('details', {
-      summaryHtml: 'Use <b>bold text</b> sparingly'
-    })
+    const $ = render('details', examples['summary html'])
 
     const detailsText = $('.govuk-details__summary-text').html().trim()
     expect(detailsText).toEqual('Use <b>bold text</b> sparingly')
   })
 
   it('allows additional classes to be added to the details element', () => {
-    const $ = render('details', {
-      classes: 'some-additional-class'
-    })
+    const $ = render('details', examples.classes)
 
     const $component = $('.govuk-details')
     expect($component.hasClass('some-additional-class')).toBeTruthy()
   })
 
   it('allows additional attributes to be added to the details element', () => {
-    const $ = render('details', {
-      attributes: {
-        'data-some-data-attribute': 'i-love-data',
-        'another-attribute': 'true'
-      }
-    })
+    const $ = render('details', examples.attributes)
 
     const $component = $('.govuk-details')
     expect($component.attr('data-some-data-attribute')).toEqual('i-love-data')
