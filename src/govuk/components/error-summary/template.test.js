5b6fd7b6ab066319d9bb0662716f78243166f553
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-08-19 15:50:11 +0100

    Error summary

--- src/govuk/components/error-summary/template.test.js
+++ src/govuk/components/error-summary/template.test.js
@@ -46,66 +46,49 @@ describe('Error-summary', () => {
   })
 
   it('allows title text to be passed whilst escaping HTML entities', () => {
-    const $ = render('error-summary', {
-      titleText: 'Alert, <em>alert</em>'
-    })
+    const $ = render('error-summary', examples['html as titleText'])
 
     const summaryTitle = $('.govuk-error-summary__title').html().trim()
     expect(summaryTitle).toEqual('Alert, &lt;em&gt;alert&lt;/em&gt;')
   })
 
   it('allows title HTML to be passed un-escaped', () => {
-    const $ = render('error-summary', {
-      titleHtml: 'Alert, <em>alert</em>'
-    })
+    const $ = render('error-summary', examples['title html'])
 
     const summaryTitle = $('.govuk-error-summary__title').html().trim()
     expect(summaryTitle).toEqual('Alert, <em>alert</em>')
   })
 
   it('renders description text', () => {
-    const $ = render('error-summary', {
-      descriptionText: 'Lorem ipsum'
-    })
+    const $ = render('error-summary', examples.description)
     const summaryDescription = $('.govuk-error-summary__body p').text().trim()
 
     expect(summaryDescription).toEqual('Lorem ipsum')
   })
 
   it('allows description text to be passed whilst escaping HTML entities', () => {
-    const $ = render('error-summary', {
-      descriptionText: 'See errors below (▼)'
-    })
+    const $ = render('error-summary', examples['html as descriptionText'])
 
     const summaryDescription = $('.govuk-error-summary__body p').html().trim()
     expect(summaryDescription).toEqual('See errors below (&#x25BC;)')
   })
 
   it('allows description HTML to be passed un-escaped', () => {
-    const $ = render('error-summary', {
-      descriptionHtml: 'See <span>errors</span> below'
-    })
+    const $ = render('error-summary', examples['description html'])
 
     const summaryDescription = $('.govuk-error-summary__body p').html().trim()
     expect(summaryDescription).toEqual('See <span>errors</span> below')
   })
 
   it('allows additional classes to be added to the error-summary component', () => {
-    const $ = render('error-summary', {
-      classes: 'extra-class one-more-class'
-    })
+    const $ = render('error-summary', examples.classes)
 
     const $component = $('.govuk-error-summary')
     expect($component.hasClass('extra-class one-more-class')).toBeTruthy()
   })
 
   it('allows additional attributes to be added to the error-summary component', () => {
-    const $ = render('error-summary', {
-      attributes: {
-        'first-attribute': 'true',
-        'second-attribute': 'false'
-      }
-    })
+    const $ = render('error-summary', examples.attributes)
 
     const $component = $('.govuk-error-summary')
     expect($component.attr('first-attribute')).toEqual('true')
@@ -134,18 +117,7 @@ describe('Error-summary', () => {
   })
 
   it('renders anchor tag with attributes', () => {
-    const $ = render('error-summary', {
-      errorList: [
-        {
-          text: 'Error-1',
-          href: '#item',
-          attributes: {
-            'data-attribute': 'my-attribute',
-            'data-attribute-2': 'my-attribute-2'
-          }
-        }
-      ]
-    })
+    const $ = render('error-summary', examples['error list with attributes'])
 
     const $component = $('.govuk-error-summary__list a')
     expect($component.attr('data-attribute')).toEqual('my-attribute')
@@ -153,20 +125,14 @@ describe('Error-summary', () => {
   })
 
   it('renders error item text', () => {
-    const $ = render('error-summary', examples.default)
+    const $ = render('error-summary', examples['without links'])
     const errorItemText = $('.govuk-error-summary .govuk-error-summary__list li:first-child').text().trim()
 
-    expect(errorItemText).toEqual('The date your passport was issued must be in the past')
+    expect(errorItemText).toEqual('Invalid username or password')
   })
 
   it('allows error item HTML to be passed un-escaped', () => {
-    const $ = render('error-summary', {
-      errorList: [
-        {
-          html: 'The date your passport was issued <b>must</b> be in the past'
-        }
-      ]
-    })
+    const $ = render('error-summary', examples['error list with html'])
 
     const errorItemText = $('.govuk-error-summary .govuk-error-summary__list li').html().trim()
 
@@ -174,13 +140,7 @@ describe('Error-summary', () => {
   })
 
   it('allows error item text to be passed whilst escaping HTML entities', () => {
-    const $ = render('error-summary', {
-      errorList: [
-        {
-          text: 'Descriptive link to the <b>question</b> with an error'
-        }
-      ]
-    })
+    const $ = render('error-summary', examples['error list with html as text'])
 
     const errorItemText = $('.govuk-error-summary .govuk-error-summary__list li').html().trim()
 
@@ -188,14 +148,7 @@ describe('Error-summary', () => {
   })
 
   it('allows error item HTML inside "a" tag to be passed un-escaped', () => {
-    const $ = render('error-summary', {
-      errorList: [
-        {
-          html: 'Descriptive link to the <b>question</b> with an error',
-          href: '#error-1'
-        }
-      ]
-    })
+    const $ = render('error-summary', examples['error list with html link'])
 
     const errorItemText = $('.govuk-error-summary .govuk-error-summary__list li a').html().trim()
 
@@ -203,14 +156,7 @@ describe('Error-summary', () => {
   })
 
   it('allows error item text inside "a" tag to be passed whilst escaping HTML entities', () => {
-    const $ = render('error-summary', {
-      errorList: [
-        {
-          text: 'Descriptive link to the <b>question</b> with an error',
-          href: '#error-1'
-        }
-      ]
-    })
+    const $ = render('error-summary', examples['error list with html as text link'])
 
     const errorItemText = $('.govuk-error-summary .govuk-error-summary__list li a').html().trim()
 
