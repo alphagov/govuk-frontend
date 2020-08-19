c953d40c267f1b952f8ed39867fadde7a337688e
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-08-19 11:01:55 +0100

    Input

--- src/govuk/components/input/template.test.js
+++ src/govuk/components/input/template.test.js
@@ -22,92 +22,77 @@ describe('Input', () => {
     })
 
     it('renders with classes', () => {
-      const $ = render('input', {
-        classes: 'app-input--custom-modifier'
-      })
+      const $ = render('input', examples.classes)
 
       const $component = $('.govuk-input')
       expect($component.hasClass('app-input--custom-modifier')).toBeTruthy()
     })
 
     it('renders with id', () => {
-      const $ = render('input', {
-        id: 'my-input'
-      })
+      const $ = render('input', examples.default)
 
       const $component = $('.govuk-input')
-      expect($component.attr('id')).toEqual('my-input')
+      expect($component.attr('id')).toEqual('input-example')
     })
 
     it('renders with name', () => {
-      const $ = render('input', {
-        name: 'my-input-name'
-      })
+      const $ = render('input', examples.default)
 
       const $component = $('.govuk-input')
-      expect($component.attr('name')).toEqual('my-input-name')
+      expect($component.attr('name')).toEqual('test-name')
     })
 
     it('renders with type="text" by default', () => {
-      const $ = render('input', {})
+      const $ = render('input', examples.default)
 
       const $component = $('.govuk-input')
       expect($component.attr('type')).toEqual('text')
     })
 
     it('allows you to override the type', () => {
-      const $ = render('input', {
-        type: 'number'
-      })
+      const $ = render('input', examples['custom type'])
 
       const $component = $('.govuk-input')
       expect($component.attr('type')).toEqual('number')
     })
 
+    it('renders with pattern attribute', () => {
+      const $ = render('input', examples['with pattern attribute'])
+
+      const $component = $('.govuk-input')
+      expect($component.attr('pattern')).toEqual('[0-9]*')
+    })
+
     it('renders with value', () => {
-      const $ = render('input', {
-        value: 'QQ 12 34 56 C'
-      })
+      const $ = render('input', examples.value)
 
       const $component = $('.govuk-input')
       expect($component.val()).toEqual('QQ 12 34 56 C')
     })
 
     it('renders with aria-describedby', () => {
-      const describedById = 'some-id'
-
-      const $ = render('input', {
-        describedBy: describedById
-      })
+      const $ = render('input', examples['with describedBy'])
 
       const $component = $('.govuk-input')
-      expect($component.attr('aria-describedby')).toMatch(describedById)
+      expect($component.attr('aria-describedby')).toMatch('some-id')
     })
 
     it('renders with attributes', () => {
-      const $ = render('input', {
-        attributes: {
-          'data-attribute': 'my data value'
-        }
-      })
+      const $ = render('input', examples.attributes)
 
       const $component = $('.govuk-input')
       expect($component.attr('data-attribute')).toEqual('my data value')
     })
 
     it('renders with a form group wrapper', () => {
-      const $ = render('input', {})
+      const $ = render('input', examples.default)
 
       const $formGroup = $('.govuk-form-group')
       expect($formGroup.length).toBeTruthy()
     })
 
     it('renders with a form group wrapper that has extra classes', () => {
-      const $ = render('input', {
-        formGroup: {
-          classes: 'extra-class'
-        }
-      })
+      const $ = render('input', examples['with optional form-group classes'])
 
       const $formGroup = $('.govuk-form-group')
       expect($formGroup.hasClass('extra-class')).toBeTruthy()
@@ -116,23 +101,13 @@ describe('Input', () => {
 
   describe('when it includes a hint', () => {
     it('renders the hint', () => {
-      const $ = render('input', {
-        id: 'input-with-hint',
-        hint: {
-          text: 'It’s on your National Insurance card, benefit letter, payslip or P60. For example, ‘QQ 12 34 56 C’.'
-        }
-      })
+      const $ = render('input', examples['with hint text'])
 
       expect(htmlWithClassName($, '.govuk-hint')).toMatchSnapshot()
     })
 
     it('associates the input as "described by" the hint', () => {
-      const $ = render('input', {
-        id: 'input-with-hint',
-        hint: {
-          text: 'It’s on your National Insurance card, benefit letter, payslip or P60. For example, ‘QQ 12 34 56 C’.'
-        }
-      })
+      const $ = render('input', examples['with hint text'])
 
       const $input = $('.govuk-input')
       const $hint = $('.govuk-hint')
@@ -146,21 +121,13 @@ describe('Input', () => {
     })
 
     it('associates the input as "described by" the hint and parent fieldset', () => {
-      const describedById = 'some-id'
-
-      const $ = render('input', {
-        id: 'input-with-hint',
-        describedBy: describedById,
-        hint: {
-          text: 'It’s on your National Insurance card, benefit letter, payslip or P60. For example, ‘QQ 12 34 56 C’.'
-        }
-      })
+      const $ = render('input', examples['hint with describedBy'])
 
       const $input = $('.govuk-input')
       const $hint = $('.govuk-hint')
 
       const hintId = new RegExp(
-        WORD_BOUNDARY + describedById + WHITESPACE + $hint.attr('id') + WORD_BOUNDARY
+        WORD_BOUNDARY + 'some-id' + WHITESPACE + $hint.attr('id') + WORD_BOUNDARY
       )
 
       expect($input.attr('aria-describedby'))
@@ -170,23 +137,13 @@ describe('Input', () => {
 
   describe('when it includes an error message', () => {
     it('renders the error message', () => {
-      const $ = render('input', {
-        id: 'input-with-error',
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('input', examples['with error message'])
 
       expect(htmlWithClassName($, '.govuk-error-message')).toMatchSnapshot()
     })
 
     it('associates the input as "described by" the error message', () => {
-      const $ = render('input', {
-        id: 'input-with-error',
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('input', examples['with error message'])
 
       const $input = $('.govuk-input')
       const $errorMessage = $('.govuk-error-message')
@@ -200,21 +157,13 @@ describe('Input', () => {
     })
 
     it('associates the input as "described by" the error message and parent fieldset', () => {
-      const describedById = 'some-id'
-
-      const $ = render('input', {
-        id: 'input-with-error',
-        describedBy: describedById,
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('input', examples['error with describedBy'])
 
       const $input = $('.govuk-input')
       const $errorMessage = $('.govuk-error-message')
 
       const errorMessageId = new RegExp(
-        WORD_BOUNDARY + describedById + WHITESPACE + $errorMessage.attr('id') + WORD_BOUNDARY
+        WORD_BOUNDARY + 'some-id' + WHITESPACE + $errorMessage.attr('id') + WORD_BOUNDARY
       )
 
       expect($input.attr('aria-describedby'))
@@ -222,22 +171,14 @@ describe('Input', () => {
     })
 
     it('includes the error class on the input', () => {
-      const $ = render('input', {
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('input', examples['with error message'])
 
       const $component = $('.govuk-input')
       expect($component.hasClass('govuk-input--error')).toBeTruthy()
     })
 
     it('renders with a form group wrapper that has an error state', () => {
-      const $ = render('input', {
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('input', examples['with error message'])
 
       const $formGroup = $('.govuk-form-group')
       expect($formGroup.hasClass('govuk-form-group--error')).toBeTruthy()
@@ -246,28 +187,21 @@ describe('Input', () => {
 
   describe('when it has the spellcheck attribute', () => {
     it('renders with spellcheck attribute set to true', () => {
-      const $ = render('input', {
-        spellcheck: true
-      })
+      const $ = render('input', examples['with spellcheck enabled'])
 
       const $component = $('.govuk-input')
       expect($component.attr('spellcheck')).toEqual('true')
     })
 
     it('renders with spellcheck attribute set to false', () => {
-      const $ = render('input', {
-        name: 'my-input-name',
-        spellcheck: false
-      })
+      const $ = render('input', examples['with spellcheck disabled'])
 
       const $component = $('.govuk-input')
       expect($component.attr('spellcheck')).toEqual('false')
     })
 
     it('renders without spellcheck attribute by default', () => {
-      const $ = render('input', {
-        name: 'my-input-name'
-      })
+      const $ = render('input', examples.default)
 
       const $component = $('.govuk-input')
       expect($component.attr('spellcheck')).toBeUndefined()
@@ -276,14 +210,7 @@ describe('Input', () => {
 
   describe('when it includes both a hint and an error message', () => {
     it('associates the input as described by both the hint and the error message', () => {
-      const $ = render('input', {
-        errorMessage: {
-          text: 'Error message'
-        },
-        hint: {
-          text: 'Hint'
-        }
-      })
+      const $ = render('input', examples['with error and hint'])
 
       const $component = $('.govuk-input')
       const errorMessageId = $('.govuk-error-message').attr('id')
@@ -298,24 +225,14 @@ describe('Input', () => {
     })
 
     it('associates the input as described by the hint, error message and parent fieldset', () => {
-      const describedById = 'some-id'
-
-      const $ = render('input', {
-        describedBy: describedById,
-        errorMessage: {
-          text: 'Error message'
-        },
-        hint: {
-          text: 'Hint'
-        }
-      })
+      const $ = render('input', examples['with error, hint and describedBy'])
 
       const $component = $('.govuk-input')
       const errorMessageId = $('.govuk-error-message').attr('id')
       const hintId = $('.govuk-hint').attr('id')
 
       const combinedIds = new RegExp(
-        WORD_BOUNDARY + describedById + WHITESPACE + hintId + WHITESPACE + errorMessageId + WORD_BOUNDARY
+        WORD_BOUNDARY + 'some-id' + WHITESPACE + hintId + WHITESPACE + errorMessageId + WORD_BOUNDARY
       )
 
       expect($component.attr('aria-describedby'))
@@ -325,47 +242,29 @@ describe('Input', () => {
 
   describe('with dependant components', () => {
     it('have correct nesting order', () => {
-      const $ = render('input', {
-        id: 'my-input',
-        label: {
-          text: 'National Insurance number'
-        }
-      })
+      const $ = render('input', examples.default)
 
       const $component = $('.govuk-form-group > .govuk-input')
       expect($component.length).toBeTruthy()
     })
 
     it('renders with label', () => {
-      const $ = render('input', {
-        id: 'my-input',
-        label: {
-          text: 'National Insurance number'
-        }
-      })
+      const $ = render('input', examples.default)
 
       expect(htmlWithClassName($, '.govuk-label')).toMatchSnapshot()
     })
 
     it('renders label with "for" attribute reffering the input "id"', () => {
-      const $ = render('input', {
-        id: 'my-input',
-        label: {
-          text: 'National Insurance number'
-        }
-      })
+      const $ = render('input', examples.default)
 
       const $label = $('.govuk-label')
-      expect($label.attr('for')).toEqual('my-input')
+      expect($label.attr('for')).toEqual('input-example')
     })
   })
 
   describe('when it includes an autocomplete attribute', () => {
     it('renders the autocomplete attribute', () => {
-      const $ = render('input', {
-        id: 'input-with-autocomplete',
-        autocomplete: 'postal-code'
-      })
+      const $ = render('input', examples['with autocomplete attribute'])
 
       const $component = $('.govuk-input')
       expect($component.attr('autocomplete')).toEqual('postal-code')
@@ -374,9 +273,7 @@ describe('Input', () => {
 
   describe('when it includes an inputmode', () => {
     it('renders with an inputmode attached to the input', () => {
-      const $ = render('input', {
-        inputmode: 'decimal'
-      })
+      const $ = render('input', examples.inputmode)
 
       const $component = $('.govuk-form-group > .govuk-input')
       expect($component.attr('inputmode')).toEqual('decimal')
