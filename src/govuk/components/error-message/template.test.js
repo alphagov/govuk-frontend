bcc2ce3a51568a856d8ab7968b46f287951ffc01
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-08-19 16:07:55 +0100

    Error message

--- src/govuk/components/error-message/template.test.js
+++ src/govuk/components/error-message/template.test.js
@@ -17,40 +17,36 @@ describe('Error message', () => {
     expect(results).toHaveNoViolations()
   })
 
+  it('renders with a custom id', () => {
+    const $ = render('error-message', examples.id)
+
+    const $component = $('.govuk-error-message')
+    expect($component.attr('id')).toEqual('my-error-message-id')
+  })
+
   it('allows additional classes to specified', () => {
-    const $ = render('error-message', {
-      classes: 'custom-class'
-    })
+    const $ = render('error-message', examples.classes)
 
     const $component = $('.govuk-error-message')
     expect($component.hasClass('custom-class')).toBeTruthy()
   })
 
   it('allows text to be passed whilst escaping HTML entities', () => {
-    const $ = render('error-message', {
-      text: 'Unexpected > in body'
-    })
+    const $ = render('error-message', examples['html as text'])
 
     const content = $('.govuk-error-message').html().trim()
     expect(content).toContain('Unexpected &gt; in body')
   })
 
   it('allows summary HTML to be passed un-escaped', () => {
-    const $ = render('error-message', {
-      html: 'Unexpected <b>bold text</b> in body copy'
-    })
+    const $ = render('error-message', examples.html)
 
     const content = $('.govuk-error-message').html().trim()
     expect(content).toContain('Unexpected <b>bold text</b> in body copy')
   })
 
   it('allows additional attributes to be specified', () => {
-    const $ = render('error-message', {
-      attributes: {
-        'data-test': 'attribute',
-        id: 'my-error-message'
-      }
-    })
+    const $ = render('error-message', examples.attributes)
 
     const $component = $('.govuk-error-message')
     expect($component.attr('data-test')).toEqual('attribute')
@@ -58,29 +54,21 @@ describe('Error message', () => {
   })
 
   it('includes a visually hidden "Error" prefix by default', () => {
-    const $ = render('error-message', {
-      text: 'Enter your full name'
-    })
+    const $ = render('error-message', examples.default)
 
     const $component = $('.govuk-error-message')
-    expect($component.text().trim()).toEqual('Error: Enter your full name')
+    expect($component.text().trim()).toEqual('Error: Error message about full name goes here')
   })
 
   it('allows the visually hidden prefix to be customised', () => {
-    const $ = render('error-message', {
-      text: 'Rhowch eich enw llawn',
-      visuallyHiddenText: 'Gwall'
-    })
+    const $ = render('error-message', examples['with visually hidden text'])
 
     const $component = $('.govuk-error-message')
     expect($component.text().trim()).toEqual('Gwall: Rhowch eich enw llawn')
   })
 
   it('allows the visually hidden prefix to be removed', () => {
-    const $ = render('error-message', {
-      text: 'There is an error on line 42',
-      visuallyHiddenText: false
-    })
+    const $ = render('error-message', examples['visually hidden text removed'])
 
     const $component = $('.govuk-error-message')
     expect($component.text().trim()).toEqual('There is an error on line 42')
