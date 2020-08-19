80480a59face2b26dba1cdca57dc950dc39807d4
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-08-19 11:29:33 +0100

    Hint

--- src/govuk/components/hint/template.test.js
+++ src/govuk/components/hint/template.test.js
@@ -18,48 +18,43 @@ describe('Hint', () => {
       expect(results).toHaveNoViolations()
     })
 
+    it('renders with text', () => {
+      const $ = render('hint', examples.default)
+
+      const content = $('.govuk-hint').text()
+      expect(content).toEqual('\n  It\'s on your National Insurance card, benefit letter, payslip or P60.\nFor example, \'QQ 12 34 56 C\'.\n\n')
+    })
+
     it('renders with classes', () => {
-      const $ = render('hint', {
-        classes: 'app-hint--custom-modifier'
-      })
+      const $ = render('hint', examples.classes)
 
       const $component = $('.govuk-hint')
       expect($component.hasClass('app-hint--custom-modifier')).toBeTruthy()
     })
 
     it('renders with id', () => {
-      const $ = render('hint', {
-        id: 'my-hint'
-      })
+      const $ = render('hint', examples.id)
 
       const $component = $('.govuk-hint')
       expect($component.attr('id')).toEqual('my-hint')
     })
 
     it('allows text to be passed whilst escaping HTML entities', () => {
-      const $ = render('hint', {
-        text: 'Unexpected <strong>bold text</strong> in body'
-      })
+      const $ = render('hint', examples['html as text'])
 
       const content = $('.govuk-hint').html().trim()
       expect(content).toEqual('Unexpected &lt;strong&gt;bold text&lt;/strong&gt; in body')
     })
 
     it('allows HTML to be passed un-escaped', () => {
-      const $ = render('hint', {
-        html: 'Unexpected <b>bold text</b> in body copy'
-      })
+      const $ = render('hint', examples['with html'])
 
       const content = $('.govuk-hint').html().trim()
-      expect(content).toEqual('Unexpected <b>bold text</b> in body copy')
+      expect(content).toEqual('It&apos;s on your National Insurance card, benefit letter, payslip or <a class="govuk-link" href="#">P60</a>.\nFor example, &apos;QQ 12 34 56 C&apos;.')
     })
 
     it('renders with attributes', () => {
-      const $ = render('hint', {
-        attributes: {
-          'data-attribute': 'my data value'
-        }
-      })
+      const $ = render('hint', examples.attributes)
 
       const $component = $('.govuk-hint')
       expect($component.attr('data-attribute')).toEqual('my data value')
