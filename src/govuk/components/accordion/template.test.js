2ae642d6e47b829c7b31f2f5c59f4fe1996c70d0
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-08-20 17:51:51 +0100

    Accordion

--- src/govuk/components/accordion/template.test.js
+++ src/govuk/components/accordion/template.test.js
@@ -19,87 +19,35 @@ describe('Accordion', () => {
     })
 
     it('renders with specified heading level', () => {
-      const $ = render('accordion', {
-        headingLevel: '3',
-        items: [
-          {
-            heading: {
-              text: 'Section A'
-            },
-            content: {
-              text: 'Some content'
-            }
-          }
-        ]
-      })
+      const $ = render('accordion', examples['custom heading level'])
       const $componentHeading = $('.govuk-accordion__section-heading')
 
       expect($componentHeading.get(0).tagName).toEqual('h3')
     })
 
     it('renders with heading button text', () => {
-      const $ = render('accordion', {
-        headingLevel: '3',
-        items: [
-          {
-            heading: {
-              html: '<span class="myClass">Section A</span>'
-            },
-            content: {
-              text: 'Some content'
-            }
-          }
-        ]
-      })
+      const $ = render('accordion', examples.default)
+      const $componentHeadingButton = $('.govuk-accordion__section-button')
+
+      expect($componentHeadingButton.html().trim()).toEqual('Section A')
+    })
+
+    it('renders with heading button html', () => {
+      const $ = render('accordion', examples['heading html'])
       const $componentHeadingButton = $('.govuk-accordion__section-button')
 
       expect($componentHeadingButton.html().trim()).toEqual('<span class="myClass">Section A</span>')
     })
 
     it('renders with content', () => {
-      const $ = render('accordion', {
-        headingLevel: '3',
-        items: [
-          {
-            heading: {
-              text: 'Section A'
-            },
-            content: {
-              text: 'Some content'
-            }
-          }
-        ]
-      })
-      const $componentContent = $('.govuk-accordion__section-content')
+      const $ = render('accordion', examples.default)
+      const $componentContent = $('.govuk-accordion__section-content').first()
 
-      expect($componentContent.text().trim()).toEqual('Some content')
+      expect($componentContent.text().trim()).toEqual('Example item 1')
     })
 
     it('renders list without falsely values', () => {
-      const $ = render('accordion', {
-        headingLevel: '3',
-        items: [
-          {
-            heading: {
-              text: 'Section A'
-            },
-            content: {
-              text: 'Some content'
-            }
-          },
-          false,
-          undefined,
-          null,
-          {
-            heading: {
-              text: 'Section B'
-            },
-            content: {
-              text: 'Some content'
-            }
-          }
-        ]
-      })
+      const $ = render('accordion', examples['with falsey values'])
       const $component = $('.govuk-accordion')
       const $items = $component.find('.govuk-accordion__section')
 
@@ -107,74 +55,37 @@ describe('Accordion', () => {
     })
 
     it('renders with classes', () => {
-      const $ = render('accordion', {
-        classes: 'app-accordion--custom-modifier'
-      })
+      const $ = render('accordion', examples.default)
 
       const $component = $('.govuk-accordion')
-      expect($component.hasClass('app-accordion--custom-modifier')).toBeTruthy()
+      expect($component.hasClass('myClass')).toBeTruthy()
     })
 
     it('renders with id', () => {
-      const $ = render('accordion', {
-        id: 'my-accordion'
-      })
+      const $ = render('accordion', examples.default)
 
       const $component = $('.govuk-accordion')
-      expect($component.attr('id')).toEqual('my-accordion')
+      expect($component.attr('id')).toEqual('default-example')
     })
 
     it('renders with attributes', () => {
-      const $ = render('accordion', {
-        attributes: {
-          'data-attribute': 'my data value'
-        }
-      })
+      const $ = render('accordion', examples.default)
       const $component = $('.govuk-accordion')
-      expect($component.attr('data-attribute')).toEqual('my data value')
+      expect($component.attr('data-attribute')).toEqual('value')
     })
 
     it('renders with section expanded class', () => {
-      const $ = render('accordion', {
-        items: [
-          {
-            expanded: true,
-            heading: {
-              text: 'Section A'
-            },
-            content: {
-              text: 'Some content'
-            }
-          }
-        ]
-      })
-      const $componentSection = $('.govuk-accordion__section')
+      const $ = render('accordion', examples['with one section open'])
+      const $componentSection = $('.govuk-accordion__section').first()
 
       expect($componentSection.hasClass('govuk-accordion__section--expanded')).toBeTruthy()
     })
 
-    describe('when it includes a summary', () => {
-      it('renders with summary', () => {
-        const $ = render('accordion', {
-          headingLevel: '3',
-          items: [
-            {
-              heading: {
-                text: 'Section A'
-              },
-              summary: {
-                text: 'Summary of content'
-              },
-              content: {
-                text: 'Some content'
-              }
-            }
-          ]
-        })
-        const $componentSummary = $('.govuk-accordion__section-summary')
+    it('renders with summary', () => {
+      const $ = render('accordion', examples['with additional descriptions'])
+      const $componentSummary = $('.govuk-accordion__section-summary').first()
 
-        expect($componentSummary.text().trim()).toEqual('Summary of content')
-      })
+      expect($componentSummary.text().trim()).toEqual('Additional description')
     })
   })
 })
