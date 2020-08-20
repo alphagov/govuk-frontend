34e56fd66455661f661563ef58c50c0ef5db0d54
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-08-20 16:50:14 +0100

    Back link

--- src/govuk/components/back-link/template.test.js
+++ src/govuk/components/back-link/template.test.js
@@ -26,64 +26,42 @@ describe('back-link component', () => {
   })
 
   it('renders classes correctly', () => {
-    const $ = render('back-link', {
-      classes: 'app-back-link--custom-class',
-      href: '#',
-      html: '<b>Back</b>'
-    })
+    const $ = render('back-link', examples.classes)
 
     const $component = $('.govuk-back-link')
     expect($component.hasClass('app-back-link--custom-class')).toBeTruthy()
   })
 
   it('renders custom text correctly', () => {
-    const $ = render('back-link', {
-      href: '#',
-      text: 'Home'
-    })
+    const $ = render('back-link', examples['with custom text'])
 
     const $component = $('.govuk-back-link')
-    expect($component.html()).toEqual('Home')
+    expect($component.html()).toEqual('Back to home')
   })
 
   it('renders escaped html when passed to text', () => {
-    const $ = render('back-link', {
-      href: '#',
-      text: '<b>Home</b>'
-    })
+    const $ = render('back-link', examples['html as text'])
 
     const $component = $('.govuk-back-link')
     expect($component.html()).toEqual('&lt;b&gt;Home&lt;/b&gt;')
   })
 
   it('renders html correctly', () => {
-    const $ = render('back-link', {
-      href: '#',
-      html: '<b>Back</b>'
-    })
+    const $ = render('back-link', examples.html)
 
     const $component = $('.govuk-back-link')
     expect($component.html()).toEqual('<b>Back</b>')
   })
 
   it('renders default text correctly', () => {
-    const $ = render('back-link', {
-      href: '#'
-    })
+    const $ = render('back-link', examples.default)
 
     const $component = $('.govuk-back-link')
     expect($component.html()).toEqual('Back')
   })
 
   it('renders attributes correctly', () => {
-    const $ = render('back-link', {
-      attributes: {
-        'data-test': 'attribute',
-        'aria-label': 'Back to home'
-      },
-      href: '#',
-      html: 'Back'
-    })
+    const $ = render('back-link', examples.attributes)
 
     const $component = $('.govuk-back-link')
     expect($component.attr('data-test')).toEqual('attribute')
