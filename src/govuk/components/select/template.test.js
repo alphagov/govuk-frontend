d10b00347cafa11f42971b80c1ba24a178c82ddc
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-08-25 14:51:17 +0100

    Select

--- src/govuk/components/select/template.test.js
+++ src/govuk/components/select/template.test.js
@@ -21,200 +21,100 @@ describe('Select', () => {
       expect(results).toHaveNoViolations()
     })
 
-    it('renders with classes', () => {
-      const $ = render('select', {
-        classes: 'app-select--custom-modifier'
-      })
-
-      const $component = $('.govuk-select')
-      expect($component.hasClass('app-select--custom-modifier')).toBeTruthy()
-    })
-
     it('renders with id', () => {
-      const $ = render('select', {
-        id: 'my-select'
-      })
+      const $ = render('select', examples.default)
 
       const $component = $('.govuk-select')
-      expect($component.attr('id')).toEqual('my-select')
+      expect($component.attr('id')).toEqual('select-1')
     })
 
     it('renders with name', () => {
-      const $ = render('select', {
-        name: 'my-select-name'
-      })
+      const $ = render('select', examples.default)
 
       const $component = $('.govuk-select')
-      expect($component.attr('name')).toEqual('my-select-name')
+      expect($component.attr('name')).toEqual('select-1')
     })
 
     it('renders with items', () => {
-      const $ = render('select', {
-        items: [
-          {
-            text: 'Option 1'
-          },
-          {
-            text: 'Options 2'
-          }
-        ]
-      })
+      const $ = render('select', examples.default)
       const $items = $('.govuk-select option')
-      expect($items.length).toEqual(2)
-    })
-
-    it('renders without falsely items', () => {
-      const $ = render('select', {
-        items: [
-          {
-            text: 'Option 1'
-          },
-          undefined,
-          null,
-          false,
-          {
-            text: 'Options 2'
-          }
-        ]
-      })
-      const $items = $('.govuk-select option')
-      expect($items.length).toEqual(2)
+      expect($items.length).toEqual(3)
     })
 
     it('renders item with value', () => {
-      const $ = render('select', {
-        value: '2',
-        items: [
-          {
-            value: '1',
-            text: 'Option 1'
-          },
-          {
-            value: '2',
-            text: 'Options 2'
-          }
-        ]
-      })
+      const $ = render('select', examples.default)
 
       const $firstItem = $('.govuk-select option:first-child')
       expect($firstItem.attr('value')).toEqual('1')
     })
 
     it('renders item with text', () => {
-      const $ = render('select', {
-        value: '2',
-        items: [
-          {
-            text: 'Option 1'
-          },
-          {
-            text: 'Options 2'
-          }
-        ]
-      })
+      const $ = render('select', examples.default)
 
       const $firstItem = $('.govuk-select option:first-child')
-      expect($firstItem.text()).toEqual('Option 1')
+      expect($firstItem.text()).toEqual('GOV.UK frontend option 1')
     })
 
     it('renders item with selected', () => {
-      const $ = render('select', {
-        value: '2',
-        items: [
-          {
-            text: 'Option 1',
-            selected: true
-          },
-          {
-            text: 'Options 2'
-          }
-        ]
-      })
+      const $ = render('select', examples.default)
 
-      const $firstItem = $('.govuk-select option:first-child')
-      expect($firstItem.attr('selected')).toBeTruthy()
+      const $selectedItem = $('.govuk-select option:nth-child(2)')
+      expect($selectedItem.attr('selected')).toBeTruthy()
     })
 
     it('renders item with disabled', () => {
-      const $ = render('select', {
-        value: '2',
-        items: [
-          {
-            text: 'Option 1',
-            disabled: true
-          },
-          {
-            text: 'Options 2'
-          }
-        ]
-      })
+      const $ = render('select', examples.default)
 
-      const $firstItem = $('.govuk-select option:first-child')
-      expect($firstItem.attr('disabled')).toBeTruthy()
-    })
-
-    it('renders with aria-describedby', () => {
-      const describedById = 'some-id'
-
-      const $ = render('select', {
-        describedBy: describedById
-      })
-
-      const $component = $('.govuk-select')
-      expect($component.attr('aria-describedby')).toMatch(describedById)
-    })
-
-    it('renders with attributes', () => {
-      const $ = render('select', {
-        attributes: {
-          'data-attribute': 'my data value'
-        }
-      })
-
-      const $component = $('.govuk-select')
-      expect($component.attr('data-attribute')).toEqual('my data value')
+      const $disabledItem = $('.govuk-select option:last-child')
+      expect($disabledItem.attr('disabled')).toBeTruthy()
     })
 
     it('renders with a form group wrapper', () => {
-      const $ = render('select', {})
+      const $ = render('select', examples.default)
 
       const $formGroup = $('.govuk-form-group')
       expect($formGroup.length).toBeTruthy()
     })
 
     it('renders with a form group wrapper that has extra classes', () => {
-      const $ = render('select', {
-        formGroup: {
-          classes: 'extra-class'
-        }
-      })
+      const $ = render('select', examples['with optional form-group classes'])
 
       const $formGroup = $('.govuk-form-group')
       expect($formGroup.hasClass('extra-class')).toBeTruthy()
     })
+
+    it('renders without falsely items', () => {
+      const $ = render('select', examples['with falsey values'])
+
+      const $items = $('.govuk-select option')
+      expect($items.length).toEqual(2)
+    })
   })
 
-  describe('when they include option attributes', () => {
-    it('renders the option attributes', () => {
-      const $ = render('select', {
-        value: '2',
-        items: [
-          {
-            text: 'Option 1',
-            attributes: {
-              'data-attribute': 'ABC',
-              'data-second-attribute': 'DEF'
-            }
-          },
-          {
-            text: 'Options 2',
-            attributes: {
-              'data-attribute': 'GHI',
-              'data-second-attribute': 'JKL'
-            }
-          }
-        ]
-      })
+  describe('custom options', () => {
+    it('renders with classes', () => {
+      const $ = render('select', examples['with full width override'])
+
+      const $component = $('.govuk-select')
+      expect($component.hasClass('govuk-!-width-full')).toBeTruthy()
+    })
+
+    it('renders with aria-describedby', () => {
+      const $ = render('select', examples['with describedBy'])
+
+      const $component = $('.govuk-select')
+      expect($component.attr('aria-describedby')).toMatch('some-id')
+    })
+
+    it('renders with attributes', () => {
+      const $ = render('select', examples.attributes)
+
+      const $component = $('.govuk-select')
+      expect($component.attr('data-attribute')).toEqual('my data value')
+    })
+
+    it('renders with attributes on items', () => {
+      const $ = render('select', examples['attributes on items'])
 
       const $component = $('.govuk-select')
 
@@ -230,23 +130,13 @@ describe('Select', () => {
 
   describe('when it includes a hint', () => {
     it('renders the hint', () => {
-      const $ = render('select', {
-        id: 'select-with-hint',
-        hint: {
-          text: 'Hint text goes here'
-        }
-      })
+      const $ = render('select', examples.hint)
 
       expect(htmlWithClassName($, '.govuk-hint')).toMatchSnapshot()
     })
 
     it('associates the select as "described by" the hint', () => {
-      const $ = render('select', {
-        id: 'select-with-hint',
-        hint: {
-          text: 'Hint text goes here'
-        }
-      })
+      const $ = render('select', examples.hint)
 
       const $select = $('.govuk-select')
       const $hint = $('.govuk-hint')
@@ -260,47 +150,24 @@ describe('Select', () => {
     })
 
     it('associates the select as "described by" the hint and parent fieldset', () => {
-      const describedById = 'some-id'
-
-      const $ = render('select', {
-        id: 'select-with-hint',
-        describedBy: describedById,
-        hint: {
-          text: 'Hint text goes here'
-        }
-      })
+      const $ = render('select', examples['hint and describedBy'])
 
       const $select = $('.govuk-select')
-      const $hint = $('.govuk-hint')
-
-      const hintId = new RegExp(
-        WORD_BOUNDARY + describedById + WHITESPACE + $hint.attr('id') + WORD_BOUNDARY
-      )
 
       expect($select.attr('aria-describedby'))
-        .toMatch(hintId)
+        .toMatch('some-id')
     })
   })
 
   describe('when it includes an error message', () => {
     it('renders with error message', () => {
-      const $ = render('select', {
-        id: 'select-with-error',
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('select', examples.error)
 
       expect(htmlWithClassName($, '.govuk-error-message')).toMatchSnapshot()
     })
 
     it('associates the select as "described by" the error message', () => {
-      const $ = render('select', {
-        id: 'select-with-error',
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('select', examples.error)
 
       const $input = $('.govuk-select')
       const $errorMessage = $('.govuk-error-message')
@@ -314,44 +181,23 @@ describe('Select', () => {
     })
 
     it('associates the select as "described by" the error message and parent fieldset', () => {
-      const describedById = 'some-id'
-
-      const $ = render('select', {
-        id: 'select-with-error',
-        describedBy: describedById,
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('select', examples['error and describedBy'])
 
       const $input = $('.govuk-select')
-      const $errorMessage = $('.govuk-error-message')
-
-      const errorMessageId = new RegExp(
-        WORD_BOUNDARY + describedById + WHITESPACE + $errorMessage.attr('id') + WORD_BOUNDARY
-      )
 
       expect($input.attr('aria-describedby'))
-        .toMatch(errorMessageId)
+        .toMatch('some-id')
     })
 
     it('adds the error class to the select', () => {
-      const $ = render('select', {
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('select', examples.error)
 
       const $component = $('.govuk-select')
       expect($component.hasClass('govuk-select--error')).toBeTruthy()
     })
 
     it('renders with a form group wrapper that has an error state', () => {
-      const $ = render('select', {
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('select', examples.error)
 
       const $formGroup = $('.govuk-form-group')
       expect($formGroup.hasClass('govuk-form-group--error')).toBeTruthy()
@@ -360,14 +206,7 @@ describe('Select', () => {
 
   describe('when it includes both a hint and an error message', () => {
     it('associates the select as described by both the hint and the error message', () => {
-      const $ = render('select', {
-        errorMessage: {
-          text: 'Error message'
-        },
-        hint: {
-          text: 'Hint'
-        }
-      })
+      const $ = render('select', examples['with hint text and error message'])
 
       const $component = $('.govuk-select')
       const errorMessageId = $('.govuk-error-message').attr('id')
@@ -382,68 +221,34 @@ describe('Select', () => {
     })
 
     it('associates the select as described by the hint, error message and parent fieldset', () => {
-      const describedById = 'some-id'
-
-      const $ = render('select', {
-        describedBy: describedById,
-        errorMessage: {
-          text: 'Error message'
-        },
-        hint: {
-          text: 'Hint'
-        }
-      })
+      const $ = render('select', examples['with hint text and error message'])
 
       const $component = $('.govuk-select')
-      const errorMessageId = $('.govuk-error-message').attr('id')
-      const hintId = $('.govuk-hint').attr('id')
-
-      const combinedIds = new RegExp(
-        WORD_BOUNDARY + describedById + WHITESPACE + hintId + WHITESPACE + errorMessageId + WORD_BOUNDARY
-      )
 
       expect($component.attr('aria-describedby'))
-        .toMatch(combinedIds)
+        .toMatch('select-2-hint select-2-error')
     })
   })
 
   describe('with dependant components', () => {
     it('have correct nesting order', () => {
-      const $ = render('select', {
-        id: 'nesting-order',
-        label: {
-          text: 'National Insurance number'
-        },
-        errorMessage: {
-          text: 'Error message'
-        }
-      })
+      const $ = render('select', examples['with hint text and error message'])
 
       const $component = $('.govuk-form-group > .govuk-select')
       expect($component.length).toBeTruthy()
     })
 
     it('renders with label', () => {
-      const $ = render('select', {
-        id: 'my-select',
-        label: {
-          text: 'National Insurance number'
-        }
-      })
+      const $ = render('select', examples.default)
 
       expect(htmlWithClassName($, '.govuk-label')).toMatchSnapshot()
     })
 
     it('renders label with "for" attribute reffering the select "id"', () => {
-      const $ = render('select', {
-        id: 'my-select',
-        label: {
-          text: 'Label text'
-        }
-      })
+      const $ = render('select', examples.default)
 
       const $label = $('.govuk-label')
-      expect($label.attr('for')).toEqual('my-select')
+      expect($label.attr('for')).toEqual('select-1')
     })
   })
 })
