2e5741887a8df025c19a7e9b11a985ced57688e6
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2021-01-27 16:30:29 +0000

    Add an example of a server-side cookie banner
    The cookie banner components needs to be flexible enough to work for both server-side and client-side implementations.
    
    Add an example of a typical server-side cookie banner, where a page navigation occurs when the user chooses to accept or reject the cookies, and the choice to render either the original ‘accept/reject’ banner or the confirmation banner is made on the server.
    
    We do include a small amount of client-side JavaScript to handle the ‘Hide’ button in the example, as I think this would typically be handled client-side as nothing needs to be recorded.

---
+++ app/views/full-page-examples/cookie-banner-server-side/index.js
@@ -0,0 +1,7 @@
+module.exports = (app) => {
+  app.post('/full-page-examples/cookie-banner-server-side', (request, response) => {
+    response.render('./full-page-examples/cookie-banner-server-side/index', {
+      cookies: request.body.cookies
+    })
+  })
+}
