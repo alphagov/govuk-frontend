2ffad88dbf7f39269b439cf7b59e1836eced59f4
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2021-03-09 09:54:17 +0000

    Hide the cookie banner with JS in server side example
    If the user chooses hides the confirmation banner, progressively enhance
    the cookie banner to hide with JavaScript. If JavaScript is not enabled,
    the page reloads.

--- app/views/full-page-examples/cookie-banner-server-side/index.js
+++ app/views/full-page-examples/cookie-banner-server-side/index.js
@@ -1,7 +1,8 @@
 module.exports = (app) => {
   app.post('/full-page-examples/cookie-banner-server-side', (request, response) => {
     response.render('./full-page-examples/cookie-banner-server-side/index', {
-      cookies: request.body.cookies
+      cookies: request.body.cookies,
+      currentUrl: request.url
     })
   })
 }
