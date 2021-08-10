c50fe90e66e8e9ed575357ec0f6a6083390e0837
Chris Yoong <chris.yoong@digital.cabinet-office.gov.uk>
2021-08-10 19:53:39 +0100

    Update testing
    Update testing for changes and new Design
    Add additional tests:
    
    - Test toggle text is changed on click (eg "Show/Hide[ this section]")
    - Test updated "all" toggle text is present and changes (eg "Hide all sections]")
    - Test comma for screen readers is present
    - Test new icons are present on JS loads
    - Check that summary div is changed to span once JS loads
    - Check the summary is added inside the button in the expected order once JS loads.

--- src/govuk/components/accordion/accordion.test.js
+++ src/govuk/components/accordion/accordion.test.js
@@ -29,10 +29,10 @@ describe('/components/accordion', () => {
         }
       })
 
-      it('does not display "+/-" in the section headings', async () => {
+      it('does not display "↓/↑" in the section headings', async () => {
         await page.goto(baseUrl + '/components/accordion/preview', { waitUntil: 'load' })
 
-        const numberOfIcons = await page.evaluate(() => document.body.querySelectorAll('.govuk-accordion .govuk-accordion__section .govuk-accordion__icon').length)
+        const numberOfIcons = await page.evaluate(() => document.body.querySelectorAll('.govuk-accordion .govuk-accordion__section .govuk-accordion-nav__chevron').length)
         expect(numberOfIcons).toEqual(0)
       })
     })
@@ -57,22 +57,22 @@ describe('/components/accordion', () => {
         }
       })
 
-      it('should change the Open all button to Close all when both sections are opened', async () => {
+      it('should change the Show all sections button to Hide all sections when both sections are opened', async () => {
         await page.goto(baseUrl + '/components/accordion/preview', { waitUntil: 'load' })
 
         await page.click('.govuk-accordion .govuk-accordion__section:nth-of-type(2) .govuk-accordion__section-header')
-
         await page.click('.govuk-accordion .govuk-accordion__section:nth-of-type(3) .govuk-accordion__section-header')
 
-        var openOrCloseAllButtonText = await page.evaluate(() => document.body.querySelector('.govuk-accordion__open-all').textContent)
+        const openOrCloseAllButtonText = await page.evaluate(() => document.body.querySelector('.govuk-accordion__show-all').textContent)
+        await page.click('.govuk-accordion__show-all')
 
-        expect(openOrCloseAllButtonText).toEqual('Close all sections')
+        expect(openOrCloseAllButtonText).toEqual('Hide all sections')
       })
 
-      it('should open both sections when the Open all button is clicked', async () => {
+      it('should open both sections when the Show all sections button is clicked', async () => {
         await page.goto(baseUrl + '/components/accordion/preview', { waitUntil: 'load' })
 
-        await page.click('.govuk-accordion__open-all')
+        await page.click('.govuk-accordion__show-all')
 
         const firstSectionHeaderButtonExpanded = await page.evaluate(() => document.body.querySelectorAll('.govuk-accordion__section').item(0).querySelector('.govuk-accordion__section-button').getAttribute('aria-expanded'))
 
@@ -96,16 +96,15 @@ describe('/components/accordion', () => {
           expect(sectionHeaderButtonExpanded).toEqual('true')
         }
 
-        const openOrCloseAllButtonText = await page.evaluate(() => document.body.querySelector('.govuk-accordion__open-all').textContent)
+        const openOrCloseAllButtonText = await page.evaluate(() => document.body.querySelector('.govuk-accordion__show-all').textContent)
 
-        expect(openOrCloseAllButtonText).toEqual('Close all sections')
+        expect(openOrCloseAllButtonText).toEqual('Hide all sections')
       })
 
       it('should maintain the expanded state after a page refresh', async () => {
         const sectionHeaderButton = '.govuk-accordion .govuk-accordion__section:nth-of-type(2) .govuk-accordion__section-button'
 
         await page.goto(baseUrl + '/components/accordion/preview', { waitUntil: 'load' })
-
         await page.click(sectionHeaderButton)
 
         const expandedState = await page.evaluate((sectionHeaderButton) => {
@@ -123,14 +122,70 @@ describe('/components/accordion', () => {
         expect(expandedState).toEqual(expandedStateAfterRefresh)
       })
 
-      describe('"+/-" icon', () => {
-        it('should display "+/-" in the section headings', async () => {
+      describe('"↓/↑" icon', () => {
+        it('should display "↓/↑" in the section headings', async () => {
           await page.goto(baseUrl + '/components/accordion/preview', { waitUntil: 'load' })
 
           const numberOfExampleSections = 2
+          const numberOfIcons = await page.evaluate(() => document.body.querySelectorAll('.govuk-accordion .govuk-accordion__section .govuk-accordion-nav__chevron').length)
 
-          const numberOfIcons = await page.evaluate(() => document.body.querySelectorAll('.govuk-accordion .govuk-accordion__section .govuk-accordion__icon').length)
           expect(numberOfIcons).toEqual(numberOfExampleSections)
+        })
+      })
+
+      describe('hidden comma', () => {
+        it('should contain hidden comma " ," for assistive technology', async () => {
+          await page.goto(baseUrl + '/components/accordion/preview', { waitUntil: 'load' })
+
+          const numberOfExampleSections = 2
+          const numberOfCommas = await page.evaluate(() => document.body.querySelectorAll('.govuk-accordion .govuk-accordion__section .govuk-visually-hidden:first-child').length)
+          const firstComma = await page.evaluate(() => document.body.querySelectorAll('.govuk-accordion .govuk-accordion__section .govuk-visually-hidden')[0].innerHTML)
+          const secondComma = await page.evaluate(() => document.body.querySelectorAll('.govuk-accordion .govuk-accordion__section .govuk-visually-hidden')[2].innerHTML)
+
+          expect(numberOfCommas).toEqual(numberOfExampleSections)
+          expect(firstComma).toMatch(/, /)
+          expect(secondComma).toMatch(/, /)
+        })
+      })
+
+      describe('location of summary', () => {
+        it('should move the additional description to the button text in the correct order', async () => {
+          await page.goto(baseUrl + '/components/accordion/with-additional-descriptions/preview', { waitUntil: 'load' })
+
+          const summaryClass = 'govuk-accordion__section-summary govuk-body'
+          const firstSummaryElement = await page.evaluate(() => document.body.querySelectorAll('.govuk-accordion__section-button > span')[2].className)
+          expect(firstSummaryElement).toMatch(summaryClass)
+        })
+      })
+
+      describe('div to span', () => {
+        it('should have converted the div to a span tag', async () => {
+          await page.goto(baseUrl + '/components/accordion/with-additional-descriptions/preview', { waitUntil: 'load' })
+
+          const firstSummaryElement = await page.evaluate(() => document.body.querySelector('.govuk-accordion .govuk-accordion__section .govuk-accordion__section-summary').outerHTML)
+
+          expect(firstSummaryElement).toMatch(/<span[^>]*>/)
+        })
+      })
+
+      it('should change the (combined) Show[ this section] text to Hide[ this section ] when sections are opened', async () => {
+        await page.goto(baseUrl + '/components/accordion/preview', { waitUntil: 'load' })
+        await page.click('.govuk-accordion .govuk-accordion__section:nth-of-type(2) .govuk-accordion__section-header')
+        await page.click('.govuk-accordion .govuk-accordion__section:nth-of-type(3) .govuk-accordion__section-header')
+
+        const ShowOrHideButtonText = await page.evaluate(() => document.body.querySelector('.govuk-accordion__section-toggle-text').textContent)
+
+        expect(ShowOrHideButtonText).toEqual('Hide this section')
+      })
+
+      describe('hidden text', () => {
+        it('should contain hidden text " this section" for assistive technology', async () => {
+          await page.goto(baseUrl + '/components/accordion/preview', { waitUntil: 'load' })
+
+          const numberOfExampleSections = 2
+          const hiddenText = await page.evaluate(() => document.body.querySelectorAll('.govuk-accordion .govuk-accordion__section .govuk-accordion__section-toggle-text .govuk-visually-hidden').length)
+
+          expect(hiddenText).toEqual(numberOfExampleSections)
         })
       })
     })
