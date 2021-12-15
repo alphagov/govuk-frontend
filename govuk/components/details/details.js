7130ab9d03f32310365fe271a11b22b9afd5100e
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2021-12-15 14:38:51 +0000

    Release v4.0.0

--- govuk/components/details/details.js
+++ govuk/components/details/details.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define('GOVUKFrontend', factory) :
-	(global.GOVUKFrontend = factory());
+	typeof define === 'function' && define.amd ? define('GOVUKFrontend.Details', factory) :
+	(global.GOVUKFrontend = global.GOVUKFrontend || {}, global.GOVUKFrontend.Details = factory());
 }(this, (function () { 'use strict';
 
 (function(undefined) {
