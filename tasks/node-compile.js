06c58b062da30250586271489188272cdf8145d8
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2021-12-22 14:38:20 +0000

    FIXUP compile dist JS

--- tasks/node-compile.js
+++ tasks/node-compile.js
@@ -7,6 +7,9 @@ const cssnano = require('cssnano')
 const fs = require('fs')
 const sass = require('node-sass')
 const postcss = require('postcss')
+const rollup = require('rollup')
+const eol = require('eol')
+var uglify = require("uglify-js");
 const oldie = require('oldie')({
   rgba: { filter: true },
   rem: { disable: true },
@@ -26,7 +29,7 @@ function compilePublic() {
 
 }
 
-function compileDist() {
+async function compileDist() {
   // Fetch package.json version to update VERSION.txt and append to filenames
   const packageJson = require('../' + configPaths.package + 'package.json')
   fs.writeFileSync(taskArguments.destination + '/VERSION.txt', packageJson.version + '\r\n')
@@ -53,7 +56,7 @@ function compileDist() {
     }
   })
 
-  // Compile old IE
+  // Compile old IE styles
   const IeStylesheet = configPaths.src + 'all-ie8.scss'
 
   const IeCss = sass.renderSync({
@@ -74,6 +77,35 @@ function compileDist() {
       })
     }
   })
+
+  // Compile JS
+  const js = configPaths.src + 'all.js'
+
+  const bundle = await rollup.rollup({
+    input: js,
+  });
+
+  const generatedBundle = await bundle.generate({
+    // Used to set the `window` global and UMD/AMD export name
+    // Component JavaScript is given a unique name to aid individual imports, e.g GOVUKFrontend.Accordion
+    name: "GOVUKFrontend",
+    // Legacy mode is required for IE8 support
+    legacy: true,
+    // UMD allows the published bundle to work in CommonJS and in the browser.
+    format: 'umd'
+  })
+
+  const minifiedJs = uglify.minify(generatedBundle.code);
+
+  const completedJs = eol.auto(minifiedJs.code)
+
+  fs.writeFile(taskArguments.destination + '/govuk-frontend-' + packageJson.version + '.min.js', completedJs, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+  })
+
 }
 
 function compilePackage() {
