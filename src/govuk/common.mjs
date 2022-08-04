4533908a205ef158089a2f4f0aa151f58f2e87f2
Brett Kyle <brett.kyle@digital.cabinet-office.gov.uk>
2022-08-04 22:27:51 +0100

    Add getOptions function
    To create an options object with the following precedence:
    
    1. Options passed in programmatically during initialisation
    2. Options derived from data attributes
    3. Default options

--- src/govuk/common.mjs
+++ src/govuk/common.mjs
@@ -26,3 +26,41 @@ export function generateUniqueID () {
     return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
   })
 }
+
+/*
+ * Used to get the options of a component in the following order of precedence:
+ * 1. Config passed into a component programmatically at construction
+ * 2. Config derived by the data attributes of a component's element
+ * 3. The component's default options
+ *
+ * I've gone back and forth on precedence. In cases where the options are
+ * initialised using initAll(), it might make sense to be able to override
+ * instances using the Data API/macros. The other argument for data attributes
+ * taking precedence is keeping the need for JavaScript to a minimum, and
+ * lowering the barrier to entry on using the Design System in a service.
+ *
+ * However, my assumption is that users mostly use our macros or HTML, and thus
+ * we should treat any use of a programmatic API as the more specific need - it
+ * could indicate a user needing to do something non-standard.
+ *
+ * Where this gets fuzzy is when components are programmatically initialised via
+ * initAll(), but then data attributes/properties are used in individual macro
+ * calls or HTML. I guess it's possible to keep track of where things are
+ * initialised and introduce a level of precedence for initAll(), but that seems
+ * like a nightmare to document for users and ensure consistency. 
+ *
+ * Since this would be common functionality across ALL components, it might make
+ * sense to eventually put this in some kind of base component class which all
+ * components inherit from. That would also mean fewer args to pass in.
+ *
+ * @TODO: Rewrite this in an ES5-friendly way
+ * @TODO: Filter the dataset in case of clashes. We may want to investigate
+ * using a namespaced prefix like data-govuk for our data attributes
+ */
+export function getOptions (passedOptions, moduleElement, defaultOptions) {
+  return {
+    ...defaultOptions,
+    ...moduleElement.dataset,
+    ...passedOptions
+  }
+}
