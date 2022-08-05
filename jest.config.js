33a29a8ed6a8c1d625c24650574690066b172169
Vanita Barrett-Smith <vanita.barrett@digital.cabinet-office.gov.uk>
2022-08-05 15:27:47 +0100

    Setup Jest config file for unit tests
    - Moves existing Jest config from package.json to jest.config.js
    file for easier readability
    - Split config into 'projects' depending on type of test, with own config
    - Add 'unit testing' project to run anything called **.unit.test.js or
    **.unit.test.mjs as a unit test with it's own config

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const config = {
+  projects: [
+    {
+      displayName: 'Snapshot tests',
+      testMatch: ['./**/template.test.js'],
+      snapshotSerializers: [
+        'jest-serializer-html'
+      ],
+      setupFilesAfterEnv: [
+        './config/jest-setup.js'
+      ]
+    },
+    {
+      displayName: 'JavaScript behaviour tests',
+      testMatch: ['./**/*.test.js', '!./**/template.test.js'],
+      preset: 'jest-puppeteer'
+    },
+    {
+      displayName: 'JavaScript unit tests',
+      testMatch: ['./**/*.unit.test.js, ./**/*.unit.test.mjs']
+    }
+  ]
+}
+
+module.exports = config
