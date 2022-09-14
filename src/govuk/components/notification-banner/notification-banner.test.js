2510e0c9f32b3b3a9095849fc6db82ee76b6fa81
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2022-09-14 11:44:32 +0100

    Tidy up test structure
    Co-authored-by: Romaric Pascal <hello@romaricpascal.is>

--- src/govuk/components/notification-banner/notification-banner.test.js
+++ src/govuk/components/notification-banner/notification-banner.test.js
@@ -8,7 +8,7 @@ const PORT = configPaths.ports.test
 
 const baseUrl = 'http://localhost:' + PORT
 
-describe('/components/notification-banner/with-type-as-success', () => {
+describe('Notification banner, when type is set to "success"', () => {
   it('has the correct tabindex attribute to be focused with JavaScript', async () => {
     await page.goto(baseUrl + '/components/notification-banner/with-type-as-success/preview', { waitUntil: 'load' })
 
@@ -33,131 +33,129 @@ describe('/components/notification-banner/with-type-as-success', () => {
     const tabindex = await page.$eval('.govuk-notification-banner', el => el.getAttribute('tabindex'))
     expect(tabindex).toBeNull()
   })
-})
 
-describe('when auto-focus is disabled using data attributes', () => {
-  beforeAll(async () => {
-    await page.goto(`${baseUrl}/components/notification-banner/auto-focus-disabled,-with-type-as-success/preview`, { waitUntil: 'load' })
-  })
+  describe('and auto-focus is disabled using data attributes', () => {
+    beforeAll(async () => {
+      await page.goto(`${baseUrl}/components/notification-banner/auto-focus-disabled,-with-type-as-success/preview`, { waitUntil: 'load' })
+    })
 
-  it('does not have a tabindex attribute', async () => {
-    const tabindex = await page.$eval('.govuk-notification-banner', el => el.getAttribute('tabindex'))
+    it('does not have a tabindex attribute', async () => {
+      const tabindex = await page.$eval('.govuk-notification-banner', el => el.getAttribute('tabindex'))
 
-    expect(tabindex).toBeNull()
-  })
+      expect(tabindex).toBeNull()
+    })
 
-  it('does not focus the notification banner', async () => {
-    const activeElement = await page.evaluate(() => document.activeElement.dataset.module)
+    it('does not focus the notification banner', async () => {
+      const activeElement = await page.evaluate(() => document.activeElement.dataset.module)
 
-    expect(activeElement).not.toBe('govuk-notification-banner')
-  })
-})
-
-describe('when auto-focus is disabled using JavaScript configuration', () => {
-  beforeAll(async () => {
-    await page.goto(`${baseUrl}/tests/boilerplate`, { waitUntil: 'load' })
-
-    // Render the notification banner Nunjucks template
-    const html = renderHtml('notification-banner', examples['with type as success'])
-
-    // Inject rendered HTML into the slot
-    await page.$eval('#slot', (slot, htmlForSlot) => {
-      slot.innerHTML = htmlForSlot
-    }, html)
-
-    // Run a script to init the JavaScript component
-    await page.evaluate(() => {
-      var $notificationBanner = document.querySelector('[data-module="govuk-notification-banner"]')
-      new window.GOVUKFrontend.NotificationBanner($notificationBanner, {
-        disableAutoFocus: true
-      }).init()
+      expect(activeElement).not.toBe('govuk-notification-banner')
     })
   })
 
-  it('does not have a tabindex attribute', async () => {
-    const tabindex = await page.$eval('.govuk-notification-banner', el => el.getAttribute('tabindex'))
+  describe('and auto-focus is disabled using JavaScript configuration', () => {
+    beforeAll(async () => {
+      await page.goto(`${baseUrl}/tests/boilerplate`, { waitUntil: 'load' })
 
-    expect(tabindex).toBeNull()
-  })
+      // Render the notification banner Nunjucks template
+      const html = renderHtml('notification-banner', examples['with type as success'])
 
-  it('does not focus the notification banner', async () => {
-    const activeElement = await page.evaluate(() => document.activeElement.dataset.module)
+      // Inject rendered HTML into the slot
+      await page.$eval('#slot', (slot, htmlForSlot) => {
+        slot.innerHTML = htmlForSlot
+      }, html)
 
-    expect(activeElement).not.toBe('govuk-notification-banner')
-  })
-})
-
-describe('when auto-focus is disabled using options passed to initAll', () => {
-  beforeAll(async () => {
-    await page.goto(`${baseUrl}/tests/boilerplate`, { waitUntil: 'load' })
-
-    // Render the notification banner Nunjucks template
-    const html = renderHtml('notification-banner', examples['with type as success'])
-
-    // Inject rendered HTML into the slot
-    await page.$eval('#slot', (slot, htmlForSlot) => {
-      slot.innerHTML = htmlForSlot
-    }, html)
-
-    // Run a script to init the JavaScript component
-    await page.evaluate(() => {
-      window.GOVUKFrontend.initAll({
-        notificationBanner: {
+      // Run a script to init the JavaScript component
+      await page.evaluate(() => {
+        var $notificationBanner = document.querySelector('[data-module="govuk-notification-banner"]')
+        new window.GOVUKFrontend.NotificationBanner($notificationBanner, {
           disableAutoFocus: true
-        }
+        }).init()
       })
     })
-  })
 
-  it('does not have a tabindex attribute', async () => {
-    const tabindex = await page.$eval('.govuk-notification-banner', el => el.getAttribute('tabindex'))
+    it('does not have a tabindex attribute', async () => {
+      const tabindex = await page.$eval('.govuk-notification-banner', el => el.getAttribute('tabindex'))
 
-    expect(tabindex).toBeNull()
-  })
+      expect(tabindex).toBeNull()
+    })
 
-  it('does not focus the notification banner', async () => {
-    const activeElement = await page.evaluate(() => document.activeElement.dataset.module)
+    it('does not focus the notification banner', async () => {
+      const activeElement = await page.evaluate(() => document.activeElement.dataset.module)
 
-    expect(activeElement).not.toBe('govuk-notification-banner')
-  })
-})
-
-describe('when autofocus is disabled in JS but enabled in data attributes, attributes win', () => {
-  beforeAll(async () => {
-    await page.goto(`${baseUrl}/tests/boilerplate`, { waitUntil: 'load' })
-
-    // Render the notification banner Nunjucks template
-    const html = renderHtml('notification-banner', examples['auto-focus explicitly enabled, with type as success'])
-
-    // Inject rendered HTML into the slot
-    await page.$eval('#slot', (slot, htmlForSlot) => {
-      slot.innerHTML = htmlForSlot
-    }, html)
-
-    // Run a script to init the JavaScript component
-    await page.evaluate(() => {
-      var $notificationBanner = document.querySelector('[data-module="govuk-notification-banner"]')
-      new window.GOVUKFrontend.NotificationBanner($notificationBanner, {
-        disableAutoFocus: true
-      }).init()
+      expect(activeElement).not.toBe('govuk-notification-banner')
     })
   })
 
-  it('has the correct tabindex attribute to be focused with JavaScript', async () => {
-    const tabindex = await page.$eval('.govuk-notification-banner', el => el.getAttribute('tabindex'))
+  describe('and auto-focus is disabled using options passed to initAll', () => {
+    beforeAll(async () => {
+      await page.goto(`${baseUrl}/tests/boilerplate`, { waitUntil: 'load' })
 
-    expect(tabindex).toEqual('-1')
+      // Render the notification banner Nunjucks template
+      const html = renderHtml('notification-banner', examples['with type as success'])
+
+      // Inject rendered HTML into the slot
+      await page.$eval('#slot', (slot, htmlForSlot) => {
+        slot.innerHTML = htmlForSlot
+      }, html)
+
+      // Run a script to init the JavaScript component
+      await page.evaluate(() => {
+        window.GOVUKFrontend.initAll({
+          notificationBanner: {
+            disableAutoFocus: true
+          }
+        })
+      })
+    })
+
+    it('does not have a tabindex attribute', async () => {
+      const tabindex = await page.$eval('.govuk-notification-banner', el => el.getAttribute('tabindex'))
+
+      expect(tabindex).toBeNull()
+    })
+
+    it('does not focus the notification banner', async () => {
+      const activeElement = await page.evaluate(() => document.activeElement.dataset.module)
+
+      expect(activeElement).not.toBe('govuk-notification-banner')
+    })
   })
 
-  it('is automatically focused when the page loads', async () => {
-    const activeElement = await page.evaluate(() => document.activeElement.dataset.module)
+  describe('and autofocus is disabled in JS but enabled in data attributes, attributes win', () => {
+    beforeAll(async () => {
+      await page.goto(`${baseUrl}/tests/boilerplate`, { waitUntil: 'load' })
 
-    expect(activeElement).toBe('govuk-notification-banner')
+      // Render the notification banner Nunjucks template
+      const html = renderHtml('notification-banner', examples['auto-focus explicitly enabled, with type as success'])
+
+      // Inject rendered HTML into the slot
+      await page.$eval('#slot', (slot, htmlForSlot) => {
+        slot.innerHTML = htmlForSlot
+      }, html)
+
+      // Run a script to init the JavaScript component
+      await page.evaluate(() => {
+        var $notificationBanner = document.querySelector('[data-module="govuk-notification-banner"]')
+        new window.GOVUKFrontend.NotificationBanner($notificationBanner, {
+          disableAutoFocus: true
+        }).init()
+      })
+    })
+
+    it('has the correct tabindex attribute to be focused with JavaScript', async () => {
+      const tabindex = await page.$eval('.govuk-notification-banner', el => el.getAttribute('tabindex'))
+
+      expect(tabindex).toEqual('-1')
+    })
+
+    it('is automatically focused when the page loads', async () => {
+      const activeElement = await page.evaluate(() => document.activeElement.dataset.module)
+
+      expect(activeElement).toBe('govuk-notification-banner')
+    })
   })
-})
 
-describe('components/notification-banner/role=alert-overridden-to-role=region,-with-type-as-success', () => {
-  describe('when role is not alert', () => {
+  describe('and role is overridden to "region"', () => {
     it('does not have a tabindex attribute', async () => {
       await page.goto(`${baseUrl}/components/notification-banner/role=alert-overridden-to-role=region,-with-type-as-success/preview`, { waitUntil: 'load' })
 
@@ -174,11 +172,9 @@ describe('components/notification-banner/role=alert-overridden-to-role=region,-w
       expect(activeElement).not.toBe('govuk-notification-banner')
     })
   })
-})
 
-describe('/components/notification-banner/custom-tabindex', () => {
-  describe('when custom tabindex set', () => {
-    it('it does not remove the tabindex attribute on blur', async () => {
+  describe('and a custom tabindex is set', () => {
+    it('does not remove the tabindex attribute on blur', async () => {
       await page.goto(baseUrl + '/components/notification-banner/custom-tabindex/preview', { waitUntil: 'load' })
 
       await page.$eval('.govuk-notification-banner', el => el.blur())
