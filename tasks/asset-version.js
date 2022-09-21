45752333732e6793e1e2f2a6883222aa99d17e0f
Colin Rotherham <work@colinr.com>
2022-09-21 11:31:15 +0100

    Ensure all gulp tasks run to completion

--- tasks/asset-version.js
+++ tasks/asset-version.js
@@ -1,39 +1,35 @@
 const configPaths = require('../config/paths.js')
-const fs = require('fs')
-const path = require('path')
+const { rename, writeFile } = require('fs/promises')
+const { basename, resolve } = require('path')
 
 // Update assets' version numbers
 // Uses the version number from `package/package.json` and writes it to various places
-function updateDistAssetsVersion (cb) {
+async function updateDistAssetsVersion () {
   const distFolder = 'dist'
-  const pkg = require('../' + configPaths.package + 'package.json')
 
-  // Write VERSION.txt file
-  fs.writeFileSync(distFolder + '/VERSION.txt', pkg.version + '\r\n')
+  const pkg = require('../' + configPaths.package + 'package.json')
 
   // Files to process
   const assetFiles = [
-    distFolder + '/govuk-frontend.min.css',
-    distFolder + '/govuk-frontend-ie8.min.css',
-    distFolder + '/govuk-frontend.min.js'
+    resolve(`${distFolder}/govuk-frontend.min.css`),
+    resolve(`${distFolder}/govuk-frontend-ie8.min.css`),
+    resolve(`${distFolder}/govuk-frontend.min.js`)
   ]
 
-  // Loop through files and move them to their new locations
-  assetFiles.forEach(srcFilename => {
+  // Loop through files
+  const assetTasks = assetFiles.map(srcFilename => {
     const destFilename = srcFilename.replace(/(govuk.*)(?=\.min)/g, '$1-' + pkg.version)
-    fs.rename(
-      path.resolve(srcFilename),
-      path.resolve(destFilename),
-      error => {
-        if (error) throw error
-        console.log(`Moved ${srcFilename} to ${destFilename}.`)
-      }
-    )
+
+    // Move to new location
+    return rename(srcFilename, destFilename)
+      .then(() => console.log(`Moved ${basename(srcFilename)} to ${basename(destFilename)}.`))
   })
 
-  // Required for Gulp task to complete successfully.
-  // Can be removed once this no longer runs via Gulp.
-  cb()
+  // Write VERSION.txt file
+  assetTasks.push(writeFile(resolve(`${distFolder}/VERSION.txt`), pkg.version + '\r\n'))
+
+  // Resolve on completion
+  return Promise.all(assetTasks)
 }
 
 module.exports = updateDistAssetsVersion
