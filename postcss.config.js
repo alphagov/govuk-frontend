0f89c56ab2162b14904b29d0bb5bfcf245d4902c
Colin Rotherham <work@colinr.com>
2022-10-11 11:04:45 +0100

    Add `postcss.config.js` config

---
+++ postcss.config.js
@@ -0,0 +1,59 @@
+const { parse } = require('path')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const oldie = require('oldie')
+const pseudoclasses = require('postcss-pseudo-classes')
+
+/**
+ * PostCSS config
+ *
+ * @param {object} context - PostCSS context
+ * @param {string} context.env - Browserslist environment
+ * @param {import('vinyl')} context.file - File object
+ */
+module.exports = ({ env, file }) => {
+  const { dir, name } = parse(file.path)
+
+  const plugins = [
+    autoprefixer({ env })
+  ]
+
+  // Minify CSS
+  if (name.endsWith('.min')) {
+    plugins.push(cssnano())
+  }
+
+  // Add review app auto-generated 'companion' classes for each pseudo-class
+  // For example ':hover' and ':focus' classes to simulate form label states
+  if (dir.endsWith('app/assets/scss') && (name === 'app' || name.startsWith('app-'))) {
+    plugins.push(pseudoclasses({
+      // Work around a bug in pseudo classes plugin that badly transforms
+      // :not(:whatever) pseudo selectors
+      blacklist: [
+        ':not(',
+        ':disabled)',
+        ':first-child)',
+        ':last-child)',
+        ':focus)',
+        ':active)',
+        ':hover)'
+      ]
+    }))
+  }
+
+  // Transpile CSS for Internet Explorer
+  if (name.endsWith('-ie8') || name.endsWith('-ie8.min')) {
+    plugins.push(
+      oldie({
+        rgba: { filter: true },
+        rem: { disable: true },
+        unmq: { disable: true },
+        pseudo: { disable: true }
+      })
+    )
+  }
+
+  return {
+    plugins
+  }
+}
