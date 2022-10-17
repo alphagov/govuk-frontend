fc2de739dc63b211bb071257dd493f79001bc4be
Kimberly Grey <kimberly.grey@digital.cabinet-office.gov.uk>
2022-10-17 14:59:50 +0100

    Rebase Hide this Page component

---
+++ src/govuk/components/hide-this-page/hide-this-page.test.js
@@ -0,0 +1,57 @@
+/* eslint-env jest */
+
+const configPaths = require('../../../../config/paths.json')
+const PORT = configPaths.ports.test
+
+const baseUrl = 'http://localhost:' + PORT
+const buttonClass = '.govuk-js-hide-this-page-button'
+
+describe('/components/hide-this-page', () => {
+  beforeEach(async () => {
+    await page.goto(baseUrl + '/components/hide-this-page/preview', { waitUntil: 'load' })
+  })
+
+  it('navigates to the href of the button', async () => {
+    const href = await page.evaluate((buttonClass) => document.querySelector(buttonClass).href, buttonClass)
+
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click(buttonClass)
+    ])
+
+    const url = await page.url()
+    expect(url).toBe(href)
+  })
+
+  it('opens a page in a new tab when the button is clicked', async () => {
+    const expectedNewTabUrl = await page.evaluate((buttonClass) => document.querySelector(buttonClass).getAttribute('data-new-tab-url'), buttonClass)
+    const pageTarget = page.target()
+
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click(buttonClass)
+    ])
+
+    // Because we're opening a new tab, we need to wait for that tab to open and load before we can test for it
+    // We do do this by waiting for a new target to appear in the browser object which was opened by our button and getting it's page object
+    const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget)
+    const newPage = await newTarget.page()
+    const newTabUrl = await newPage.url()
+
+    expect(newTabUrl).toBe(expectedNewTabUrl)
+  })
+
+  it('activates the button functionality when the escape key is pressed 3 times', async () => {
+    const href = await page.evaluate((buttonClass) => document.querySelector(buttonClass).href, buttonClass)
+
+    await Promise.all([
+      await page.keyboard.press('Escape'),
+      await page.keyboard.press('Escape'),
+      await page.keyboard.press('Escape'),
+      await page.waitForNavigation()
+    ])
+
+    const url = await page.url()
+    expect(url).toBe(href)
+  })
+})
---
+++ src/govuk/components/hide-this-page/template.test.js
@@ -0,0 +1,74 @@
+/**
+ * @jest-environment jsdom
+ */
+/* eslint-env jest */
+
+const axe = require('../../../../lib/axe-helper')
+
+const { render, getExamples } = require('../../../../lib/jest-helpers')
+
+const examples = getExamples('hide-this-page')
+
+describe('Hide this page', () => {
+  it('default example passes accessibility tests', async () => {
+    const $ = render('hide-this-page', examples.default)
+
+    const results = await axe($.html())
+    expect(results).toHaveNoViolations()
+  })
+
+  it('renders the default example', () => {
+    const $ = render('hide-this-page', examples.default)
+    const $button = $('.govuk-hide-this-page').find('.govuk-button')
+
+    expect($button.hasClass('govuk-button--warning')).toBeTruthy()
+    expect($button.text()).toContain('Hide this page')
+    expect($button.attr('href')).toBe('https://www.gov.uk')
+    expect($button.attr('data-new-tab-url')).toBe('https://www.google.com/search?q=weather')
+    expect($button.attr('data-fake-page-title')).toBe('How to prevent the spread of Coronavirus - GOV.UK')
+  })
+})
+
+describe('Custom options', () => {
+  it('renders with text', () => {
+    const $ = render('hide-this-page', examples.testing)
+    const $button = $('.govuk-hide-this-page').find('.govuk-button')
+
+    expect($button.text()).toContain('Hide this test')
+  })
+
+  it('renders with a current tab URL', () => {
+    const $ = render('hide-this-page', examples.testing)
+    const $button = $('.govuk-hide-this-page').find('.govuk-button')
+
+    expect($button.attr('href')).toBe('https://www.test.co.uk')
+  })
+
+  it('renders with a new tab URL', () => {
+    const $ = render('hide-this-page', examples.testing)
+    const $button = $('.govuk-hide-this-page').find('.govuk-button')
+
+    expect($button.attr('data-new-tab-url')).toBe('https://www.google.com/search?q=test')
+  })
+
+  it('renders with a custom id', () => {
+    const $ = render('hide-this-page', examples.testing)
+    const $component = $('.govuk-hide-this-page')
+
+    expect($component.attr('id')).toBe('test-id')
+  })
+
+  it('renders with a custom class', () => {
+    const $ = render('hide-this-page', examples.testing)
+    const $component = $('.govuk-hide-this-page')
+
+    expect($component.hasClass('test-class')).toBeTruthy()
+  })
+
+  it('renders with custom attributes', () => {
+    const $ = render('hide-this-page', examples.testing)
+    const $component = $('.govuk-hide-this-page')
+
+    expect($component.attr('test-attribute')).toBe('true')
+  })
+})
