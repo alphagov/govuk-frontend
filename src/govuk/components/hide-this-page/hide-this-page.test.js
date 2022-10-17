018a171bbd9dc43995c28b7ad5355ad04b80acab
Kimberly Grey <kimberly.grey@digital.cabinet-office.gov.uk>
2022-10-17 17:08:56 +0100

    Remove history rewriting and new tab functionality from HtP button

--- src/govuk/components/hide-this-page/hide-this-page.test.js
+++ src/govuk/components/hide-this-page/hide-this-page.test.js
@@ -23,24 +23,6 @@ describe('/components/hide-this-page', () => {
     expect(url).toBe(href)
   })
 
-  it('opens a page in a new tab when the button is clicked', async () => {
-    const expectedNewTabUrl = await page.evaluate((buttonClass) => document.querySelector(buttonClass).getAttribute('data-new-tab-url'), buttonClass)
-    const pageTarget = page.target()
-
-    await Promise.all([
-      page.waitForNavigation(),
-      page.click(buttonClass)
-    ])
-
-    // Because we're opening a new tab, we need to wait for that tab to open and load before we can test for it
-    // We do do this by waiting for a new target to appear in the browser object which was opened by our button and getting it's page object
-    const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget)
-    const newPage = await newTarget.page()
-    const newTabUrl = await newPage.url()
-
-    expect(newTabUrl).toBe(expectedNewTabUrl)
-  })
-
   it('activates the button functionality when the escape key is pressed 3 times', async () => {
     const href = await page.evaluate((buttonClass) => document.querySelector(buttonClass).href, buttonClass)
 
