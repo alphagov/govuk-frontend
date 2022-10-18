2e0bbd0832804790eb507a96536829e079abf2e0
owenatgov <owen.jones@digital.cabinet-office.gov.uk>
2022-10-18 15:27:38 +0100

    Attempt to fix esc key location changing

--- src/govuk/components/hide-this-page/hide-this-page.mjs
+++ src/govuk/components/hide-this-page/hide-this-page.mjs
@@ -6,30 +6,24 @@ function HideThisPage ($module) {
 }
 
 HideThisPage.prototype.exitPage = function (e) {
-  var isClickEvent = typeof e !== 'undefined'
-
-  if (isClickEvent) {
-    e.preventDefault()
-  }
-
-  var target = isClickEvent ? e.target : document.querySelector('.govuk-js-hide-this-page-button')
-  window.location.href = target.href
+  // Doesn't do anything right now
 }
 
 HideThisPage.prototype.init = function () {
-  // We put the loop here instead of in all.js because we want to have both listeners on the individual buttons for clicks
-  // and a single listener for the keyboard shortcut
+  // We put the loop here instead of in all.js because we want to have both
+  // listeners on the individual buttons for clicks and a single listener for
+  // the keyboard shortcut
   nodeListForEach(this.$module, function ($button) {
-    $button.querySelector('.govuk-js-hide-this-page-button').addEventListener('click', this.exitPage)
-  }.bind(this))
+    // We won't set an event listener on the link just yet, we'll treat it just like a link first
+    // $button.querySelector('.govuk-js-hide-this-page-button').addEventListener('click', this.exitPage)
+  }) // must remember to bind once we've got an event handler ready
 
   window.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' || e.keyCode === 27 || e.which === 27) {
       this.escCounter += 1
 
-      if (this.escCounter === 3) {
-        this.escCounter = 0
-        this.exitPage()
+      if (this.escCounter >= 3) {
+        document.querySelector('.govuk-js-hide-this-page-button').click()
       }
 
       setTimeout(function () {
