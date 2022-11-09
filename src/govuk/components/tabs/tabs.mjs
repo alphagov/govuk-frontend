7c86ad4d2be808ca96ade34acd700a4c6f7a7219
Colin Rotherham <work@colinr.com>
2022-11-09 14:12:22 +0000

    Fix compiler “Expected 1 arguments, but got 2”

--- src/govuk/components/tabs/tabs.mjs
+++ src/govuk/components/tabs/tabs.mjs
@@ -110,7 +110,7 @@ Tabs.prototype.teardown = function () {
   $tabList.removeAttribute('role')
 
   nodeListForEach($tabListItems, function ($item) {
-    $item.removeAttribute('role', 'presentation')
+    $item.removeAttribute('role')
   })
 
   nodeListForEach($tabs, function ($tab) {
