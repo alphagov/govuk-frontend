65312b78f0ca556996fe6ad85d79a1c8b01e3a84
Colin Rotherham <work@colinr.com>
2022-11-17 12:50:15 +0000

    Add helper function `packageNameToPath()`
    Used to locate local (or project root) node_modules from an npm workspace

--- app/middleware/vendor.js
+++ app/middleware/vendor.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { packageNameToPath } = require('../../lib/helper-functions')
 
 const router = express.Router()
 
@@ -6,15 +7,15 @@ const router = express.Router()
  * Add middleware to serve HTML5 Shiv
  * from node_modules
  */
-router.use('/html5-shiv/', express.static('node_modules/html5shiv/dist/'))
+router.use('/html5-shiv/', express.static(packageNameToPath('html5shiv', 'dist')))
 
 /**
  * Add middleware to serve legacy code
  * from node_modules
  */
-router.use('/govuk_template/', express.static('node_modules/govuk_template_jinja/assets/'))
-router.use('/govuk_frontend_toolkit/assets', express.static('node_modules/govuk_frontend_toolkit/images'))
-router.use('/govuk_frontend_toolkit/', express.static('node_modules/govuk_frontend_toolkit/javascripts/govuk/'))
-router.use('/jquery/', express.static('node_modules/jquery/dist'))
+router.use('/govuk_template/', express.static(packageNameToPath('govuk_template_jinja', 'assets')))
+router.use('/govuk_frontend_toolkit/assets', express.static(packageNameToPath('govuk_frontend_toolkit', 'images')))
+router.use('/govuk_frontend_toolkit/', express.static(packageNameToPath('govuk_frontend_toolkit', 'javascripts/govuk')))
+router.use('/jquery/', express.static(packageNameToPath('jquery', 'dist')))
 
 module.exports = router
