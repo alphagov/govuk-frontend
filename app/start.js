b6687a8dd329f1fefeb7fc0cbaef3aadce1b8726
Colin Rotherham <work@colinr.com>
2022-11-22 14:24:54 +0000

    Fix ESLint `promise/catch-or-return` expected catch() or return

--- app/start.js
+++ app/start.js
@@ -3,6 +3,8 @@ const configPaths = require('../config/paths')
 
 const PORT = process.env.PORT || configPaths.ports.app
 
-app().then(server => server.listen(PORT, () => {
-  console.log('Server started at http://localhost:' + PORT)
-}))
+app()
+  .then(server => server.listen(PORT, () =>
+    console.log('Server started at http://localhost:' + PORT)
+  ))
+  .catch(console.error)
