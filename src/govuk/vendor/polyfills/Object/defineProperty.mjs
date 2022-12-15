bcfe43ab14b0566a2aa9201b9deda8cc930d4052
Colin Rotherham <work@colinr.com>
2022-12-15 08:51:10 +0000

    Skip vendor type declaration linting
    Our vendor directory includes code copied from:
    
    1. https://polyfill.io web service
    2. https://www.npmjs.com/package/polyfill-library
    3. https://developer.mozilla.org
    
    Feature detection doesn’t necessarily follow completed browser API specifications so we’ll ignore it all as “known working”

--- src/govuk/vendor/polyfills/Object/defineProperty.mjs
+++ src/govuk/vendor/polyfills/Object/defineProperty.mjs
@@ -1,4 +1,5 @@
-(function(undefined) {
+// @ts-nocheck
+(function (undefined) {
 
 // Detection from https://github.com/Financial-Times/polyfill-service/blob/master/packages/polyfill-library/polyfills/Object/defineProperty/detect.js
 var detect = (
