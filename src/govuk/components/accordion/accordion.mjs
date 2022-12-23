28bc4d1252ea258f4ef9628b723a1c7758b642f0
Colin Rotherham <work@colinr.com>
2022-12-23 15:10:43 +0000

    Add missing `Event` polyfill to accordion component JavaScript

--- src/govuk/components/accordion/accordion.mjs
+++ src/govuk/components/accordion/accordion.mjs
@@ -1,9 +1,10 @@
 import { nodeListForEach, mergeConfigs, extractConfigByNamespace } from '../../common/index.mjs'
 import { normaliseDataset } from '../../common/normalise-dataset.mjs'
 import { I18n } from '../../i18n.mjs'
-import '../../vendor/polyfills/Function/prototype/bind.mjs'
 import '../../vendor/polyfills/Element/prototype/classList.mjs'
 import '../../vendor/polyfills/Element/prototype/closest.mjs'
+import '../../vendor/polyfills/Event.mjs' // addEventListener, event.target normalization and DOMContentLoaded
+import '../../vendor/polyfills/Function/prototype/bind.mjs'
 import '../../vendor/polyfills/String/prototype/trim.mjs'
 
 /**
