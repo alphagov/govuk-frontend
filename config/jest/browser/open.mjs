76e7d40fa220d9a6aeb7129120f0e48bebb1ef2f
Colin Rotherham <work@colinr.com>
2023-01-09 19:31:02 +0000

    Limit Puppeteer to one browser per worker
    Slightly increases (2-3 seconds) the overall macOS test run, but hugely reduces CPU load for Windows test runners

--- config/jest/browser/open.mjs
+++ config/jest/browser/open.mjs
@@ -8,16 +8,6 @@ import { download } from '../../../tasks/browser/download.mjs'
  * @param {import('jest').Config} jestConfig - Jest config
  */
 export default async function browserOpen (jestConfig) {
-  const { maxWorkers } = jestConfig
-
-  /**
-   * Increase Node.js max listeners warning threshold by Jest --maxWorkers
-   * Allows jest-puppeteer.config.js `browserPerWorker: true` to open multiple browsers
-   */
-  if (Number.isFinite(maxWorkers)) {
-    process.setMaxListeners(1 + maxWorkers)
-  }
-
   await download() // Download browser
   return setup(jestConfig) // Open browser, start server
 }
