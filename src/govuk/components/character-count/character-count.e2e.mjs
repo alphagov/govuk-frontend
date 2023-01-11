cd08eac611fee5c3d20f6943e77b358783c143b0
Romaric <romaric.pascal@digital.cabinet-office.gov.uk>
2023-01-11 17:50:01 +0000

    Add test that requires injecting JavaScript on the page

--- src/govuk/components/character-count/character-count.e2e.mjs
+++ src/govuk/components/character-count/character-count.e2e.mjs
@@ -1,8 +1,15 @@
-import { goToComponent } from '../../../../lib/webdriver-helpers.js'
+import { getExamples } from '../../../../lib/jest-helpers.js'
+import { goToComponent, renderAndInitialise } from '../../../../lib/webdriver-helpers.js'
 
 const debouncedWaitTime = 1500
 
 describe('Character count', () => {
+  let examples
+
+  beforeAll(async () => {
+    examples = await getExamples('character-count')
+  })
+
   describe('when JavaScript is available', () => {
     describe('on page load', () => {
       beforeAll(async () => {
@@ -23,14 +30,32 @@ describe('Character count', () => {
         // rather than the field itself so we need to keep using the class
         await $('.govuk-js-character-count').setValue('A')
 
-        const message = (await $('.govuk-character-count__status').getProperty('innerHTML')).trim()
-        expect(message).toEqual('You have 9 characters remaining')
+        const message = await $('.govuk-character-count__status').getProperty('innerHTML')
+        expect(message.trim()).toEqual('You have 9 characters remaining')
 
         // Wait for debounced update to happen
         await new Promise((resolve) => setTimeout(resolve, debouncedWaitTime))
 
-        const srMessage = (await $('.govuk-character-count__sr-status').getProperty('innerHTML')).trim()
-        expect(srMessage).toEqual('You have 9 characters remaining')
+        const srMessage = await $('.govuk-character-count__sr-status').getProperty('innerHTML')
+        expect(srMessage.trim()).toEqual('You have 9 characters remaining')
+      })
+    })
+
+    describe('JavaScript configuration', () => {
+      describe('at instantiation', () => {
+        it('configures the number of characters', async () => {
+          await renderAndInitialise(browser, 'character-count', {
+            nunjucksParams: examples['to configure in JavaScript'],
+            javascriptConfig: {
+              maxlength: 10
+            }
+          })
+
+          await $('.govuk-js-character-count').setValue('A'.repeat(11))
+
+          const message = await $('.govuk-character-count__status').getProperty('innerHTML')
+          expect(message.trim()).toEqual('You have 1 character too many')
+        })
       })
     })
   })
