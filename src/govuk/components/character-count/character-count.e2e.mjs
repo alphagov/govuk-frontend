5961c78bd7f7a8a5fa7ab2fdbde8dee12438a892
Romaric <romaric.pascal@digital.cabinet-office.gov.uk>
2023-01-11 17:37:28 +0000

    Port test that requires user input
    - had to keep using the CSS class for picking up the field, rather than an `aria/...` selector
      as the later was returning the `<label>` rather than the `<textarea>`
    - API for filling the field was not `type` like in Puppeteer but `setValue`. Note that there's an `addValue` to keep adding and a `keys` to just send keys (for ex. to test the space interaction on the buttons)

--- src/govuk/components/character-count/character-count.e2e.mjs
+++ src/govuk/components/character-count/character-count.e2e.mjs
@@ -1,5 +1,7 @@
 import { goToComponent } from '../../../../lib/webdriver-helpers.js'
 
+const debouncedWaitTime = 1500
+
 describe('Character count', () => {
   describe('when JavaScript is available', () => {
     describe('on page load', () => {
@@ -10,6 +12,25 @@ describe('Character count', () => {
       it('injects the visual counter', async () => {
         const message = await $('.govuk-character-count__status') !== null
         expect(message).toBeTruthy()
+      })
+    })
+
+    describe('counting characters', () => {
+      it('counts down to the character limit', async () => {
+        await goToComponent(browser, 'character-count')
+
+        // Using `aria/Can you provide more detail?` retrieves the label
+        // rather than the field itself so we need to keep using the class
+        await $('.govuk-js-character-count').setValue('A')
+
+        const message = (await $('.govuk-character-count__status').getProperty('innerHTML')).trim()
+        expect(message).toEqual('You have 9 characters remaining')
+
+        // Wait for debounced update to happen
+        await new Promise((resolve) => setTimeout(resolve, debouncedWaitTime))
+
+        const srMessage = (await $('.govuk-character-count__sr-status').getProperty('innerHTML')).trim()
+        expect(srMessage).toEqual('You have 9 characters remaining')
       })
     })
   })
