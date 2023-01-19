6fd00c401800a31ddc673bff817f72214ae4ec25
Colin Rotherham <work@colinr.com>
2023-01-19 11:25:52 +0000

    Fix PostCSS `:hover` combos for Review app “pseudo-classes” plugin
    For example a hard-coded `\:focus` radio input (via `postcss-pseudo-classes`) is now styled correctly in combination with a real `:hover` interaction

--- postcss.config.js
+++ postcss.config.js
@@ -38,6 +38,7 @@ module.exports = ({ env, file = '' }) => {
   // For example ':hover' and ':focus' classes to simulate form label states
   if (minimatch(dir, '**/app/assets/scss')) {
     plugins.push(pseudoclasses({
+      allCombinations: true,
       restrictTo: [
         ':link',
         ':visited',
