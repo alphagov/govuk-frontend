38d8d14dcbcfe9112556682c3417cb9081ff51de
Colin Rotherham <work@colinr.com>
2023-01-25 20:01:15 +0000

    Load `prismjs` and `iframe-resizer` from vendor
    As we do with jQuery, we don’t need 3rd party hosting to slow our browser tests down (works offline)

--- app/middleware/vendor.js
+++ app/middleware/vendor.js
@@ -17,6 +17,8 @@ router.use('/html5-shiv/', express.static(packageNameToPath('html5shiv', 'dist')
 router.use('/govuk_template/', express.static(packageNameToPath('govuk_template_jinja', 'assets')))
 router.use('/govuk_frontend_toolkit/assets', express.static(packageNameToPath('govuk_frontend_toolkit', 'images')))
 router.use('/govuk_frontend_toolkit/', express.static(packageNameToPath('govuk_frontend_toolkit', 'javascripts/govuk')))
+router.use('/iframe-resizer/', express.static(packageNameToPath('iframe-resizer', 'js')))
 router.use('/jquery/', express.static(packageNameToPath('jquery', 'dist')))
+router.use('/prismjs/', express.static(packageNameToPath('prismjs')))
 
 module.exports = router
