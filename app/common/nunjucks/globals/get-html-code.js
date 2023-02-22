2927d9d88528c708fd07a43a0b87b61d9fa502e5
Colin Rotherham <work@colinr.com>
2023-02-22 12:43:17 +0000

    Add Nunjucks globals for code output

---
+++ app/common/nunjucks/globals/get-html-code.js
@@ -0,0 +1,34 @@
+const { join } = require('path')
+
+const beautify = require('js-beautify')
+
+const { paths } = require('../../../../config/index.js')
+
+/**
+ * Component HTML code (formatted)
+ *
+ * @param {string} componentName - Component name
+ * @param {unknown} params - Component macro params
+ * @returns {string} Nunjucks code
+ */
+function getHTMLCode (componentName, params) {
+  const templatePath = join(paths.components, componentName, 'template.njk')
+
+  // Render to HTML
+  const html = this.env.render(templatePath, { params }).trim()
+
+  // Default beautify options
+  const options = beautify.html.defaultOptions()
+
+  return beautify.html(html, {
+    indent_size: 2,
+    // Ensure nested labels in headings are indented properly
+    inline: options.inline.filter((tag) => !['label'].includes(tag)),
+    // Remove blank lines
+    max_preserve_newlines: 0,
+    // Ensure attribute wrapping in header SVG is preserved
+    wrap_attributes: 'preserve'
+  })
+}
+
+module.exports = getHTMLCode
--- app/common/nunjucks/globals/index.js
+++ app/common/nunjucks/globals/index.js
@@ -1,8 +1,13 @@
+const markdown = require('marked')
+
 /**
  * Nunjucks globals
  */
-const markdown = require('marked')
+const getHTMLCode = require('./get-html-code.js')
+const getNunjucksCode = require('./get-nunjucks-code.js')
 
 module.exports = {
+  getHTMLCode,
+  getNunjucksCode,
   markdown
 }
