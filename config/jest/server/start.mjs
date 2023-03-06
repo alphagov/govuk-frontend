03ec64b6697fa5c6c43966a58168031eb4f6530a
Colin Rotherham <work@colinr.com>
2023-03-06 16:35:10 +0000

    Fix handling of `jest-dev-server` globals locally

--- config/jest/server/start.mjs
+++ config/jest/server/start.mjs
@@ -2,5 +2,7 @@ import { setup } from 'jest-dev-server'
 
 import config from '../../../jest-dev-server.config.js'
 
-// Start web server
-export default () => setup(config)
+// Start web server(s)
+export default async () => {
+  globalThis.servers = await setup(config)
+}
