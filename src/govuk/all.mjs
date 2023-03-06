26100218f57b3c5b4bcade09cc9f83ee51efc09c
Romaric <romaric.pascal@digital.cabinet-office.gov.uk>
2023-03-06 18:07:55 +0000

    Refactor the event handling in its own function

--- src/govuk/all.mjs
+++ src/govuk/all.mjs
@@ -26,102 +26,76 @@ function initAll (config) {
   // Defaults to the entire document if nothing is set.
   var $scope = config.scope instanceof HTMLElement ? config.scope : document
 
-  // Create a cancellable event that allows other scripts to cancel
-  // the initialisation of GOVUK Frontend (eg. if it's starting in a browser they don't want to run our components in)
-  // Those scripts can call `event.preventDefault()` to let us know
-  // they don't want the initialisation to carry on
-  var beforeEvent = new CustomEvent('govuk-frontend:before-init', {
-    bubbles: true,
-    cancelable: true,
-    detail: {
-      $scope,
-      config
-    }
-  })
-  $scope.dispatchEvent(beforeEvent)
-
-  // Break if users prevented default
-  if (beforeEvent.defaultPrevented) {
-    var preventedEvent = new CustomEvent('govuk-frontend:prevented-init', {
-      bubbles: true,
-      cancelable: false
+  withEvents($scope, 'init', { $scope, config }, function () {
+    var $accordions = $scope.querySelectorAll('[data-module="govuk-accordion"]')
+    nodeListForEach($accordions, function ($accordion) {
+      new Accordion($accordion, config.accordion).init()
     })
-    $scope.dispatchEvent(preventedEvent)
 
-    return
-  }
+    var $buttons = $scope.querySelectorAll('[data-module="govuk-button"]')
+    nodeListForEach($buttons, function ($button) {
+      new Button($button, config.button).init()
+    })
 
-  // Dispatch an event to notify other scripts that GOV.UK Frontend
-  // is actually initialising
-  var event = new CustomEvent('govuk-frontend:init', {
-    bubbles: true,
-    cancelable: false
+    var $characterCounts = $scope.querySelectorAll(
+      '[data-module="govuk-character-count"]'
+    )
+    nodeListForEach($characterCounts, function ($characterCount) {
+      new CharacterCount($characterCount, config.characterCount).init()
+    })
+
+    var $checkboxes = $scope.querySelectorAll(
+      '[data-module="govuk-checkboxes"]'
+    )
+    nodeListForEach($checkboxes, function ($checkbox) {
+      new Checkboxes($checkbox).init()
+    })
+
+    var $details = $scope.querySelectorAll('[data-module="govuk-details"]')
+    nodeListForEach($details, function ($detail) {
+      new Details($detail).init()
+    })
+
+    // Find first error summary module to enhance.
+    var $errorSummary = $scope.querySelector(
+      '[data-module="govuk-error-summary"]'
+    )
+    if ($errorSummary) {
+      new ErrorSummary($errorSummary, config.errorSummary).init()
+    }
+
+    // Find first header module to enhance.
+    var $header = $scope.querySelector('[data-module="govuk-header"]')
+    if ($header) {
+      new Header($header).init()
+    }
+
+    var $notificationBanners = $scope.querySelectorAll(
+      '[data-module="govuk-notification-banner"]'
+    )
+    nodeListForEach($notificationBanners, function ($notificationBanner) {
+      new NotificationBanner(
+        $notificationBanner,
+        config.notificationBanner
+      ).init()
+    })
+
+    var $radios = $scope.querySelectorAll('[data-module="govuk-radios"]')
+    nodeListForEach($radios, function ($radio) {
+      new Radios($radio).init()
+    })
+
+    // Find first skip link module to enhance.
+    var $skipLink = $scope.querySelector('[data-module="govuk-skip-link"]')
+    if ($skipLink) {
+      new SkipLink($skipLink).init()
+    }
+
+    var $tabs = $scope.querySelectorAll('[data-module="govuk-tabs"]')
+    nodeListForEach($tabs, function ($tabs) {
+      new Tabs($tabs).init()
+    })
   })
-  $scope.dispatchEvent(event)
-
-  var $accordions = $scope.querySelectorAll('[data-module="govuk-accordion"]')
-  nodeListForEach($accordions, function ($accordion) {
-    new Accordion($accordion, config.accordion).init()
-  })
-
-  var $buttons = $scope.querySelectorAll('[data-module="govuk-button"]')
-  nodeListForEach($buttons, function ($button) {
-    new Button($button, config.button).init()
-  })
-
-  var $characterCounts = $scope.querySelectorAll('[data-module="govuk-character-count"]')
-  nodeListForEach($characterCounts, function ($characterCount) {
-    new CharacterCount($characterCount, config.characterCount).init()
-  })
-
-  var $checkboxes = $scope.querySelectorAll('[data-module="govuk-checkboxes"]')
-  nodeListForEach($checkboxes, function ($checkbox) {
-    new Checkboxes($checkbox).init()
-  })
-
-  var $details = $scope.querySelectorAll('[data-module="govuk-details"]')
-  nodeListForEach($details, function ($detail) {
-    new Details($detail).init()
-  })
-
-  // Find first error summary module to enhance.
-  var $errorSummary = $scope.querySelector('[data-module="govuk-error-summary"]')
-  if ($errorSummary) {
-    new ErrorSummary($errorSummary, config.errorSummary).init()
-  }
-
-  // Find first header module to enhance.
-  var $header = $scope.querySelector('[data-module="govuk-header"]')
-  if ($header) {
-    new Header($header).init()
-  }
-
-  var $notificationBanners = $scope.querySelectorAll('[data-module="govuk-notification-banner"]')
-  nodeListForEach($notificationBanners, function ($notificationBanner) {
-    new NotificationBanner($notificationBanner, config.notificationBanner).init()
-  })
-
-  var $radios = $scope.querySelectorAll('[data-module="govuk-radios"]')
-  nodeListForEach($radios, function ($radio) {
-    new Radios($radio).init()
-  })
-
-  // Find first skip link module to enhance.
-  var $skipLink = $scope.querySelector('[data-module="govuk-skip-link"]')
-  if ($skipLink) {
-    new SkipLink($skipLink).init()
-  }
-
-  var $tabs = $scope.querySelectorAll('[data-module="govuk-tabs"]')
-  nodeListForEach($tabs, function ($tabs) {
-    new Tabs($tabs).init()
-  })
-
-  var afterEvent = new CustomEvent('govuk-frontend:after-init', {
-    bubbles: true,
-    cancelable: true
-  })
-  $scope.dispatchEvent(afterEvent)
 }
 
 export {
@@ -137,6 +111,65 @@ export {
   Radios,
   SkipLink,
   Tabs
+}
+
+/**
+ * Surrounds the given `callback` with events notifying:
+ *
+ * - its upcoming execution with a `before-<action-name>` event (cancellable)
+ * - its cancellation with a `prevented-<action-name>` event
+ * - the start of its execution with an `<action-name>` event
+ * - the end of its execution with an `after-<action-name>` event
+ *
+ * @param {EventTarget} $element
+ * @param {string} actionName
+ * @param {Object<string,any>} detail
+ * @param {Function} callback
+ * @returns {any} Whatever the callback returns
+ */
+function withEvents ($element, actionName, detail, callback) {
+  // Create a cancellable event that allows other scripts to cancel
+  // the initialisation of GOVUK Frontend (eg. if it's starting in a browser they don't want to run our components in)
+  // Those scripts can call `event.preventDefault()` to let us know
+  // they don't want the initialisation to carry on
+  var beforeEvent = new CustomEvent('govuk-frontend:before-' + actionName, {
+    bubbles: true,
+    cancelable: true,
+    detail
+  })
+  $element.dispatchEvent(beforeEvent)
+
+  // Break if users prevented default
+  if (beforeEvent.defaultPrevented) {
+    var preventedEvent = new CustomEvent(
+      'govuk-frontend:prevented-' + actionName,
+      {
+        bubbles: true,
+        cancelable: false
+      }
+    )
+    $element.dispatchEvent(preventedEvent)
+
+    return
+  }
+
+  // Dispatch an event to notify other scripts that GOV.UK Frontend
+  // is actually initialising
+  var event = new CustomEvent('govuk-frontend:' + actionName, {
+    bubbles: true,
+    cancelable: false
+  })
+  $element.dispatchEvent(event)
+
+  try {
+    return callback()
+  } finally {
+    var afterEvent = new CustomEvent('govuk-frontend:after-' + actionName, {
+      bubbles: true,
+      cancelable: true
+    })
+    $element.dispatchEvent(afterEvent)
+  }
 }
 
 /**
