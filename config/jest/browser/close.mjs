468496e8a958e2eef581a512d9e13d6024453b02
Colin Rotherham <work@colinr.com>
2023-03-07 20:10:17 +0000

    Fix handling of `jest-environment-puppeteer` hooks

--- config/jest/browser/close.mjs
+++ config/jest/browser/close.mjs
@@ -1,4 +1,12 @@
-import { teardown } from 'jest-environment-puppeteer'
+import teardown from 'jest-environment-puppeteer/teardown'
 
-// Close browser, stop server
-export default () => teardown()
+/**
+ * Close browser
+ *
+ * @param {import('jest').Config} jestConfig - Jest config
+ * @returns {Promise<void>}
+ */
+export default function browserClose (jestConfig) {
+  // Close browser, stop server (also in watch mode)
+  return teardown({ ...jestConfig, watch: false })
+}
--- config/jest/browser/open.mjs
+++ config/jest/browser/open.mjs
@@ -1,4 +1,4 @@
-import { setup } from 'jest-environment-puppeteer'
+import setup from 'jest-environment-puppeteer/setup'
 
 import { download } from '../../../tasks/browser/download.mjs'
 
@@ -6,6 +6,7 @@ import { download } from '../../../tasks/browser/download.mjs'
  * Open browser
  *
  * @param {import('jest').Config} jestConfig - Jest config
+ * @returns {Promise<void>}
  */
 export default async function browserOpen (jestConfig) {
   await download() // Download browser
