9a08a406359e8c761e23fd449b9c96ab097bff95
Colin Rotherham <work@colinr.com>
2023-04-12 22:08:48 +0100

    Allow Heroku to prune `devDependencies` after build
    Adds package `chokidar` to the Review app as it’s required for Nunjucks watch mode

--- jest-dev-server.config.js
+++ jest-dev-server.config.js
@@ -4,8 +4,7 @@ const { ports } = require('govuk-frontend-config')
  * @type {import('jest-dev-server').Config}
  */
 module.exports = {
-  // Start with `--ignore-scripts` to prevent "prestart" build
-  command: 'npm start --ignore-scripts --workspace app',
+  command: 'npm start --workspace app',
   port: ports.app,
 
   // Skip when already running
