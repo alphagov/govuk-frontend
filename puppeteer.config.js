a29df2f33ff31a0901e406034ce07c765c1a69ec
Colin Rotherham <work@colinr.com>
2023-04-19 16:07:32 +0100

    Require Puppeteer to download Chromium for ARM where supported

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -4,5 +4,14 @@ const { join } = require('path')
  * @type {import('puppeteer').Configuration}
  */
 module.exports = {
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer')
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  experiments: {
+    /**
+     * Use Chromium (for ARM) where supported to
+     * avoid 30-40% increase in total test time
+     *
+     * {@link https://pptr.dev/contributing#macos-arm-and-custom-executables}
+     */
+    macArmChromiumEnabled: true
+  }
 }
