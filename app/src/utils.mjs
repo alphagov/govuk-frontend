e70d7270efe4e865bd47c5758bedee4217b9a566
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2023-04-24 15:31:44 +0100

    Update formatting of errors from `express-validator`
    The library renamed the `param` field in the errors they create to `path`,
    so we need to update our formatter accordingly.
    
    > - Validation errors changed shape
    >    - Field validation errors param property has been renamed to path

--- app/src/utils.mjs
+++ app/src/utils.mjs
@@ -6,9 +6,9 @@ export function formatValidationErrors (errorsInstance) {
   const errors = errorsInstance.array()
   const formattedErrors = {}
   errors.forEach(error => {
-    formattedErrors[error.param] = {
-      id: error.param,
-      href: '#' + error.param,
+    formattedErrors[error.path] = {
+      id: error.path,
+      href: '#' + error.path,
       value: error.value,
       text: error.msg
     }
