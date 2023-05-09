5c8f49265b6358c92c50cc11f6a2b692303d0664
Colin Rotherham <work@colinr.com>
2023-05-09 16:23:41 +0100

    Remove Puppeteer Chromium for ARM experiment (now included)

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -4,14 +4,5 @@ const { join } = require('path')
  * @type {import('puppeteer').Configuration}
  */
 module.exports = {
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  experiments: {
-    /**
-     * Use Chromium (for ARM) where supported to
-     * avoid 30-40% increase in total test time
-     *
-     * {@link https://pptr.dev/contributing#macos-arm-and-custom-executables}
-     */
-    macArmChromiumEnabled: true
-  }
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer')
 }
