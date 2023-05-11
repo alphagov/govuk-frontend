98b2d233037b4191b9c3a25da9770bf6a19ef8f1
beeps (Kim Grey) <kimberly.grey@digital.cabinet-office.gov.uk>
2023-05-11 12:38:29 +0100

    Remove legacy review app middleware

--- packages/govuk-frontend-review/src/common/middleware/vendor.mjs
+++ packages/govuk-frontend-review/src/common/middleware/vendor.mjs
@@ -4,12 +4,9 @@ import { packageNameToPath } from 'govuk-frontend-lib/names'
 const router = express.Router()
 
 /**
- * Add middleware to serve legacy code
+ * Add middleware to serve dependencies
  * from node_modules
  */
-router.use('/govuk_template/', express.static(packageNameToPath('govuk_template_jinja', 'assets')))
-router.use('/govuk_frontend_toolkit/assets', express.static(packageNameToPath('govuk_frontend_toolkit', 'images')))
-router.use('/govuk_frontend_toolkit/', express.static(packageNameToPath('govuk_frontend_toolkit', 'javascripts/govuk')))
 router.use('/iframe-resizer/', express.static(packageNameToPath('iframe-resizer', 'js')))
 router.use('/jquery/', express.static(packageNameToPath('jquery', 'dist')))
 
