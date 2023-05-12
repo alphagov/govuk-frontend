24d29e7ad3772b6259825ac5d6b0fc10574fa6cb
Colin Rotherham <work@colinr.com>
2023-05-12 11:59:44 +0100

    Update package Gulp watch to compile styles on change

--- packages/govuk-frontend/tasks/watch.mjs
+++ packages/govuk-frontend/tasks/watch.mjs
@@ -4,12 +4,12 @@ import { npm, task } from 'govuk-frontend-tasks'
 import gulp from 'gulp'
 import slash from 'slash'
 
-import { scripts, templates } from './index.mjs'
+import { scripts, styles, templates } from './index.mjs'
 
 /**
  * Watch task
  * During development, this task will:
- * - lint when `.scss` files change
+ * - lint and run `gulp styles` when `.scss` files change
  * - lint and run `gulp scripts` when `.{cjs,js,mjs}` files change
  * - lint and run `gulp templates` when `.{md,njk}` files change
  *
@@ -17,13 +17,16 @@ import { scripts, templates } from './index.mjs'
  */
 export const watch = (options) => gulp.parallel(
   /**
-   * Stylesheets lint watcher
+   * Stylesheets build + lint watcher
    */
   task.name('lint:scss watch', () =>
     gulp.watch([
       `${slash(options.srcPath)}/govuk/**/*.scss`,
       `!${slash(options.srcPath)}/govuk/vendor/*`
-    ], npm.script('lint:scss:cli', [slash(join(options.workspace, '**/*.scss'))]))
+    ], gulp.parallel(
+      npm.script('lint:scss:cli', [slash(join(options.workspace, '**/*.scss'))]),
+      styles(options)
+    ))
   ),
 
   /**
