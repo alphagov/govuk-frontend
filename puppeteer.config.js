ce5609bd1b1c022574d65eaef171e705d1c0790c
Colin Rotherham <work@colinr.com>
2023-05-22 15:30:47 +0100

    Ensure Percy and Puppeteer always use project browser cache
    This moves percy.config.js into the review app so both Percy and Puppeteer are running from the same workspace during the screenshot tests

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -1,8 +1,10 @@
 const { join } = require('path')
 
+const { paths } = require('govuk-frontend-config')
+
 /**
  * @type {import('puppeteer').Configuration}
  */
 module.exports = {
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer')
+  cacheDirectory: join(paths.root, '.cache', 'puppeteer')
 }
