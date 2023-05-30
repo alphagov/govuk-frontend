d5cb02aac19d4b7d1ef5558b1882371a400f8b88
Colin Rotherham <work@colinr.com>
2023-05-30 10:05:04 +0100

    Remove unnecessary ESLint JSDoc rules
    Types are already checked by the TypeScript compiler and we no longer use `@jest-environment`

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,10 +43,6 @@ module.exports = {
         'jsdoc/require-param-description': 'off',
         'jsdoc/require-param': 'off',
 
-        // Require hyphens before param description
-        // Aligns with TSDoc style: https://tsdoc.org/pages/tags/param/
-        'jsdoc/require-hyphen-before-param-description': 'warn',
-
         // Check for valid formatting
         'jsdoc/check-line-alignment': [
           'warn',
@@ -54,21 +50,10 @@ module.exports = {
             wrapIndent: '  '
           }
         ],
-        'jsdoc/check-syntax': 'error',
 
-        // Add unknown @jest-environment tag name
-        'jsdoc/check-tag-names': [
-          'warn', {
-            definedTags: ['jest-environment']
-          }
-        ],
-
-        // Add missing .querySelectorAll() type
-        'jsdoc/no-undefined-types': [
-          'error', {
-            definedTypes: ['NodeListOf']
-          }
-        ],
+        // Require hyphens before param description
+        // Aligns with TSDoc style: https://tsdoc.org/pages/tags/param/
+        'jsdoc/require-hyphen-before-param-description': 'warn',
 
         // Maintain new line after description
         'jsdoc/tag-lines': [
