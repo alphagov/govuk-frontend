cbed1521fa40a7f787f1f3833d5c641053bc0b66
Colin Rotherham <work@colinr.com>
2023-06-28 14:16:13 +0100

    Simplify i18n attributes macro to handle single or plural text

--- src/govuk/macros/i18n.unit.test.mjs
+++ src/govuk/macros/i18n.unit.test.mjs
@@ -1,24 +1,36 @@
-import { callMacro } from 'govuk-frontend-helpers/nunjucks'
+import { renderMacro } from 'govuk-frontend-helpers/nunjucks'
 
 describe('i18n.njk', () => {
-  describe('govukPluralisedI18nAttributes', () => {
-    function callMacroUnderTest (...args) {
-      return callMacro('govukPluralisedI18nAttributes', 'govuk/macros/i18n.njk', ...args)
-    }
-
+  describe('govukI18nAttributes', () => {
     it('renders a single plural type', () => {
-      const attributes = callMacroUnderTest(['translation-key', { other: 'You have %{count} characters remaining.' }])
+      const attributes = renderMacro('govukI18nAttributes', 'govuk/macros/i18n.njk', {
+        key: 'translation-key',
+        messages: {
+          other: 'You have %{count} characters remaining.'
+        }
+      })
+
       // Note the starting space so we ensure it doesn't stick to possible other previous attributes
       expect(attributes).toEqual(' data-i18n.translation-key.other="You have %{count} characters remaining."')
     })
 
     it('renders multiple plural types', () => {
-      const attributes = callMacroUnderTest(['translation-key', { other: 'You have %{count} characters remaining.', one: 'One character remaining' }])
+      const attributes = renderMacro('govukI18nAttributes', 'govuk/macros/i18n.njk', {
+        key: 'translation-key',
+        messages: {
+          other: 'You have %{count} characters remaining.',
+          one: 'One character remaining'
+        }
+      })
+
       expect(attributes).toEqual(' data-i18n.translation-key.other="You have %{count} characters remaining." data-i18n.translation-key.one="One character remaining"')
     })
 
     it('outputs nothing if there are no translations', () => {
-      const attributes = callMacroUnderTest(['translation-key', {}])
+      const attributes = renderMacro('govukI18nAttributes', 'govuk/macros/i18n.njk', {
+        key: 'translation-key'
+      })
+
       expect(attributes).toEqual('')
     })
   })
