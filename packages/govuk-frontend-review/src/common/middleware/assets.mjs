754315c07a96c2b0a8f51a2143d2dbe4ec109c8d
Colin Rotherham <work@colinr.com>
2023-07-05 16:46:46 +0100

    Update review app to use pre-built `govuk-frontend` modules
    Rollup build is not necessary when `govuk-frontend.min.js` is already built

--- packages/govuk-frontend-review/src/common/middleware/assets.mjs
+++ packages/govuk-frontend-review/src/common/middleware/assets.mjs
@@ -11,7 +11,7 @@ const router = express.Router()
  */
 
 router.use('/assets', express.static(packageTypeToPath('govuk-frontend', { modulePath: 'assets', moduleRoot: paths.app })))
-router.use('/javascripts', express.static(join(paths.app, 'dist/javascripts')))
+router.use('/javascripts', express.static(packageTypeToPath('govuk-frontend', { modulePath: '/', moduleRoot: paths.app })))
 router.use('/stylesheets', express.static(join(paths.app, 'dist/stylesheets')))
 
 export default router
