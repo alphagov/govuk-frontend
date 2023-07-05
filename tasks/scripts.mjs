013b6f963952a1b6ca7aff8e858c75e017ac9f59
Colin Rotherham <work@colinr.com>
2023-07-05 16:32:55 +0100

    Build GitHub release JavaScript also into package
    This runs the Rollup build for our GitHub release JavaScript `govuk-frontend-{{ version }}.min.js` into package too but without the version number

--- tasks/scripts.mjs
+++ tasks/scripts.mjs
@@ -1,5 +1,6 @@
 import { join, resolve } from 'path'
 
+import { pkg } from 'govuk-frontend-config'
 import { configs, scripts, npm, task } from 'govuk-frontend-tasks'
 import gulp from 'gulp'
 
@@ -19,6 +20,25 @@ export const compile = (options) => gulp.series(
       srcPath: join(options.srcPath, 'govuk'),
       destPath: join(options.destPath, 'govuk'),
       configPath: join(options.basePath, 'rollup.publish.config.mjs')
+    })
+  ),
+
+  /**
+   * Compile GOV.UK Frontend JavaScript (minified) for main entry point only
+   */
+  task.name("compile:js 'minified'", () =>
+    scripts.compile('**/all.mjs', {
+      ...options,
+
+      srcPath: join(options.srcPath, 'govuk'),
+      destPath: join(options.destPath, 'govuk'),
+      configPath: join(options.basePath, 'rollup.release.config.mjs'),
+
+      // Rename using package name and `*.min.js` extension due to
+      // web server ES module `*.mjs` Content-Type header support
+      filePath ({ dir, name }) {
+        return join(dir, `${name.replace(/^all/, pkg.name)}.min.js`)
+      }
     })
   ),
 
