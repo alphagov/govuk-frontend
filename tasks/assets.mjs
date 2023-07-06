53417711b9cbc3e3df3ab480e8a9ba650e3883a0
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2023-07-06 11:45:32 +0100

    Watch for changes to assets
    Watch the `govuk/assets` folder and re-run the assets task when there are changes.
    
    This means that changes to the assets folder are applied to the dist folder (and therefore are reflected in the review app) without having to manually run the `build:package` task.

---
+++ tasks/assets.mjs
@@ -0,0 +1,18 @@
+import { join } from 'path'
+
+import { files, task } from 'govuk-frontend-tasks'
+import gulp from 'gulp'
+
+/**
+ * Copy GOV.UK Frontend assets (for watch)
+ *
+ * @type {import('govuk-frontend-tasks').TaskFunction}
+ */
+export const assets = (options) => gulp.series(
+  task.name('copy:assets', () =>
+    files.copy('**/*', {
+      srcPath: join(options.srcPath, 'govuk/assets'),
+      destPath: join(options.destPath, 'govuk/assets')
+    })
+  )
+)
