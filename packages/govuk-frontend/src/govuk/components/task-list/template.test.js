5d5ddb0f69898e05ba961222892c692171553aab
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2023-07-10 17:41:25 +0100

    Add tests for custom classes

--- packages/govuk-frontend/src/govuk/components/task-list/template.test.js
+++ packages/govuk-frontend/src/govuk/components/task-list/template.test.js
@@ -15,6 +15,34 @@ describe('Task List', () => {
     expect($component.get(0).tagName).toEqual('ul')
   })
 
+  it('allows for custom classes on the root of the component', () => {
+    const $ = render('task-list', examples['custom classes'])
+
+    const $component = $('.govuk-task-list')
+    expect($component.hasClass('custom-class-on-component')).toBeTruthy()
+  })
+
+  it('allows for custom classes on each task', () => {
+    const $ = render('task-list', examples['custom classes'])
+
+    const $listItem = $('.govuk-task-list__item')
+    expect($listItem.hasClass('custom-class-on-task')).toBeTruthy()
+  })
+
+  it('allows for custom classes on each status', () => {
+    const $ = render('task-list', examples['custom classes'])
+
+    const $status = $('.govuk-task-list__status')
+    expect($status.hasClass('custom-class-on-status')).toBeTruthy()
+  })
+
+  it('allows for custom classes on tags', () => {
+    const $ = render('task-list', examples['custom classes'])
+
+    const $tag = $('.govuk-task-list__status .govuk-tag')
+    expect($tag.hasClass('custom-class-on-tag')).toBeTruthy()
+  })
+
   describe('when a task has an href set', () => {
     let $component
 
@@ -34,6 +62,14 @@ describe('Task List', () => {
 
       expect($statusWithId.text()).toContain('Completed')
     })
+
+    it('applies title classes to the link', () => {
+      const $ = render('task-list', examples['custom classes'])
+
+      const $itemWithLink = $('.govuk-task-list__item:first-child')
+      const $itemWithLinkTitle = $itemWithLink.find('.govuk-task-list__link')
+      expect($itemWithLinkTitle.hasClass('custom-class-on-linked-title')).toBeTruthy()
+    })
   })
 
   describe('when a task does not have an href set', () => {
@@ -43,6 +79,14 @@ describe('Task List', () => {
       const $itemWithNoLink = $('.govuk-task-list__item:last-child')
       const $itemWithNoLinkTitle = $itemWithNoLink.find('div')
       expect($itemWithNoLinkTitle.text()).toContain('Payment')
+    })
+
+    it('applies title classes to the title wrapper div', () => {
+      const $ = render('task-list', examples['custom classes'])
+
+      const $itemWithNoLink = $('.govuk-task-list__item:last-child')
+      const $itemWithNoLinkTitle = $itemWithNoLink.find('.govuk-task-list__task-name-and-hint div')
+      expect($itemWithNoLinkTitle.hasClass('custom-class-on-unlinked-title')).toBeTruthy()
     })
   })
 
