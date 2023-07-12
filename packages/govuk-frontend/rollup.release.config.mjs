d2c4e2e504c7c052c8deb48aae57303ca8c07a01
Colin Rotherham <work@colinr.com>
2023-07-12 17:04:29 +0100

    Apply Babel transforms (without polyfills) on Rollup builds
    Transforms will be applied based on `.browserslistrc` supported browsers, with helper functions bundled if needed

--- packages/govuk-frontend/rollup.release.config.mjs
+++ packages/govuk-frontend/rollup.release.config.mjs
@@ -1,3 +1,4 @@
+import { babel } from '@rollup/plugin-babel'
 import replace from '@rollup/plugin-replace'
 import terser from '@rollup/plugin-terser'
 import * as GOVUKFrontend from 'govuk-frontend/src/govuk/all.mjs'
@@ -53,6 +54,9 @@ export default defineConfig(({ i: input }) => ({
 
       // Add GOV.UK Frontend release version
       development: pkg.version
+    }),
+    babel({
+      babelHelpers: 'bundled'
     })
   ]
 }))
