506cd46f9cfb5c2a6b2f351f9af0a97b4173d012
Brett Kyle <brett.kyle@digital.cabinet-office.gov.uk>
2023-07-26 12:01:40 +0100

    Remove init() method from initAll function and webpack example
    Because we're not storing the object we create using `new`, we're getting a `no-new` failure from eslint.
    
    We'll temporarily disable this warning, but plan to have a component registry in the future at which point we can store these objects there.

--- docs/examples/webpack/src/javascripts/app.mjs
+++ docs/examples/webpack/src/javascripts/app.mjs
@@ -3,5 +3,6 @@ import { Button } from 'govuk-frontend'
 const $buttons = document.querySelectorAll('[data-module="govuk-button"]')
 
 $buttons.forEach(($button) => {
-  new Button($button).init()
+  /* eslint-disable-next-line no-new */
+  new Button($button)
 })
