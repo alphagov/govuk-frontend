a826c1f696a5888e985967b3d812a8b1b42ca9a4
Brett Kyle <brett.kyle@digital.cabinet-office.gov.uk>
2023-08-23 14:51:40 +0100

    Refactor module stats on review app

--- packages/govuk-frontend-review/src/common/nunjucks/filters/index.mjs
+++ packages/govuk-frontend-review/src/common/nunjucks/filters/index.mjs
@@ -2,7 +2,6 @@
  * Nunjucks filters
  */
 export { componentNameToMacroName } from '@govuk-frontend/lib/names'
-export { formatNumber } from './format-number.mjs'
 export { highlight } from './highlight.mjs'
 export { slugify } from './slugify.mjs'
 export { unslugify } from './unslugify.mjs'
