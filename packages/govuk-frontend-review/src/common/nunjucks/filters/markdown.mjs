1adec5531aaa18cb649fce46b0659c7ca700d5a7
Colin Rotherham <work@colinr.com>
2023-09-11 15:03:56 +0100

    Add Markdown "smart" typography and URL linking

--- packages/govuk-frontend-review/src/common/nunjucks/filters/markdown.mjs
+++ packages/govuk-frontend-review/src/common/nunjucks/filters/markdown.mjs
@@ -1,5 +1,12 @@
-import { marked } from 'marked'
+import { Marked } from 'marked'
+import markedLinkifyIt from 'marked-linkify-it'
+import { markedSmartypants } from 'marked-smartypants'
 import filters from 'nunjucks/src/filters.js'
+
+// Custom Marked instance with extensions
+const marked = new Marked()
+  .use(markedLinkifyIt()) // Enable automatic URL linking
+  .use(markedSmartypants()) // Enable "smart" typographic punctuation
 
 /**
  * Render Markdown
