8b9c71e5624c79dbf9490be0e7261e6a594c733a
Colin Rotherham <work@colinr.com>
2023-09-18 13:52:02 +0100

    Configure ESLint to allow ES2020 `??` nullish coalescing
    Babel transpiles ES2020 `??` nullish coalescing for our Browserslist supported browsers

--- packages/govuk-frontend/.eslintrc.js
+++ packages/govuk-frontend/.eslintrc.js
@@ -39,6 +39,9 @@ module.exports = {
         // Babel transpiles ES2020 class fields
         'es-x/no-class-fields': 'off',
 
+        // Babel transpiles ES2020 `??` nullish coalescing
+        'es-x/no-nullish-coalescing-operators': 'off',
+
         // ES modules include ES2017 'Object.entries()' coverage
         // https://browsersl.ist/#q=supports+es6-module+and+not+supports+object-entries
         'es-x/no-object-entries': 'off',
