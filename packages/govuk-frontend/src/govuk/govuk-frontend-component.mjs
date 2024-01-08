fd47805bf8b50883fd81834dc6f92363f7bcbf81
Colin Rotherham <work@colinr.com>
2024-01-08 17:46:06 +0000

    Throw `InitError` when components are already initialised

--- packages/govuk-frontend/src/govuk/govuk-frontend-component.mjs
+++ packages/govuk-frontend/src/govuk/govuk-frontend-component.mjs
@@ -1,5 +1,5 @@
-import { isSupported } from './common/index.mjs'
-import { SupportError } from './errors/index.mjs'
+import { isInitialised, isSupported } from './common/index.mjs'
+import { InitError, SupportError } from './errors/index.mjs'
 
 /**
  * Base Component class
@@ -14,9 +14,36 @@ export class GOVUKFrontendComponent {
    * Constructs a new component, validating that GOV.UK Frontend is supported
    *
    * @internal
+   * @param {Element | null} [$module] - HTML element to use for component
    */
-  constructor() {
+  constructor($module) {
     this.checkSupport()
+    this.checkInitialised($module)
+
+    const moduleName = /** @type {ChildClassConstructor} */ (this.constructor)
+      .moduleName
+
+    if (typeof moduleName === 'string') {
+      moduleName && $module?.setAttribute(`data-${moduleName}-init`, '')
+    } else {
+      throw new InitError(moduleName)
+    }
+  }
+
+  /**
+   * Validates whether component is already initialised
+   *
+   * @private
+   * @param {Element | null} [$module] - HTML element to be checked
+   * @throws {InitError} when component is already initialised
+   */
+  checkInitialised($module) {
+    const moduleName = /** @type {ChildClassConstructor} */ (this.constructor)
+      .moduleName
+
+    if ($module && moduleName && isInitialised($module, moduleName)) {
+      throw new InitError(moduleName)
+    }
   }
 
   /**
@@ -31,3 +58,12 @@ export class GOVUKFrontendComponent {
     }
   }
 }
+
+/**
+ * @typedef ChildClass
+ * @property {string} [moduleName] - The module name that'll be looked for in the DOM when initialising the component
+ */
+
+/**
+ * @typedef {typeof GOVUKFrontendComponent & ChildClass} ChildClassConstructor
+ */
