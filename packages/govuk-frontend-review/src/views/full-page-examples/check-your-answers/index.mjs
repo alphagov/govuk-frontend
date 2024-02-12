2c5502ad186fa4626853e4327b6d0ae0a3fe4d09
Colin Rotherham <work@colinr.com>
2024-02-12 10:48:23 +0000

    Add confirm page to “Check your answers”

---
+++ packages/govuk-frontend-review/src/views/full-page-examples/check-your-answers/index.mjs
@@ -0,0 +1,23 @@
+import express from 'express'
+import { matchedData, validationResult } from 'express-validator'
+
+import { formatValidationErrors } from '../../../utils.mjs'
+
+const router = express.Router()
+
+router.post('/check-your-answers', (req, res) => {
+  const viewPath = './full-page-examples/check-your-answers'
+  const errors = formatValidationErrors(validationResult(req))
+
+  if (!errors) {
+    return res.render(`${viewPath}/confirm`)
+  }
+
+  res.render(`${viewPath}/index`, {
+    errors,
+    errorSummary: Object.values(errors),
+    values: matchedData(req, { onlyValidData: false }) // In production this should sanitized.
+  })
+})
+
+export default router
