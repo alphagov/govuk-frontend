2f130a6a4436d4f9f245b2cf031eef951a89ec7d
Owen Jones <owen.jones@digital.cabinet-office.gov.uk>
2024-04-04 12:00:48 +0100

    Apply postcss to both govuk-frontend and review app

--- packages/govuk-frontend-review/postcss.config.mjs
+++ packages/govuk-frontend-review/postcss.config.mjs
@@ -1,6 +1,7 @@
 import autoprefixer from 'autoprefixer'
 import cssnano from 'cssnano'
 import cssnanoPresetDefault from 'cssnano-preset-default'
+import postcssCustomProperties from 'postcss-custom-properties'
 import pseudoclasses from 'postcss-pseudo-classes'
 
 /**
@@ -12,7 +13,8 @@ export default {
   plugins: [
     // Add vendor prefixes
     autoprefixer({ env: 'stylesheets' }),
-
+    // Custom properties for old browsers
+    postcssCustomProperties(),
     // Add review app auto-generated 'companion' classes for each pseudo-class
     // For example ':hover' and ':focus' classes to simulate form label states
     pseudoclasses({
