040f4e2d93385deddc5795ca382e08afff1b749a
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2024-04-08 17:07:52 +0100

    Scaffold iOS project to load the review app in a WKWebView

---
+++ packages/govuk-frontend-review-native-wrapper/GOV.UK Frontend Webview Embedding/GOV.UK Frontend Webview Embedding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GOV.UK Frontend Webview Embedding
+//
+//  Created by Romaric Pascal on 08/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ packages/govuk-frontend-review-native-wrapper/GOV.UK Frontend Webview Embedding/GOV.UK Frontend Webview Embedding/GOV_UK_Frontend_Webview_EmbeddingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOV_UK_Frontend_Webview_EmbeddingApp.swift
+//  GOV.UK Frontend Webview Embedding
+//
+//  Created by Romaric Pascal on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct GOV_UK_Frontend_Webview_EmbeddingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
