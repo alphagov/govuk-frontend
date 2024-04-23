5ec4d577976ad4a32cfab059f5a5ba022f57b6f0
beeps <hi+github@berly.kim>
2024-04-23 13:31:55 +0100

    Disable Level AAA contrast check

--- src/govuk/components/service-header/accessibility.puppeteer.test.mjs
+++ src/govuk/components/service-header/accessibility.puppeteer.test.mjs
@@ -2,13 +2,24 @@ import { axe, render } from '@govuk-frontend/helpers/puppeteer'
 import { getExamples } from '@govuk-frontend/lib/components'
 
 describe('/components/service-header', () => {
+  let axeRules
+
+  beforeAll(() => {
+    axeRules = {
+      /**
+       * Ignore 'Element has insufficient color contrast' for WCAG Level AAA
+       */
+      'color-contrast-enhanced': { enabled: false }
+    }
+  })
+
   describe('component examples', () => {
     it('passes accessibility tests', async () => {
       const examples = await getExamples('service-header')
 
       for (const exampleName in examples) {
         await render(page, 'service-header', examples[exampleName])
-        await expect(axe(page)).resolves.toHaveNoViolations()
+        await expect(axe(page, axeRules)).resolves.toHaveNoViolations()
       }
     }, 120000)
   })
