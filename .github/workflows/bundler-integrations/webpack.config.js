e3b3ca7e1e6692a732d0e9f8b6c132821b3b7196
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2024-05-13 13:14:35 +0100

    Fix tree-shaking of inner modules for the Webpack integration
    We were not quite following the right step to optimise the build as it would be in an actual production set up
    while still retaining the possibility to inspect the content

--- .github/workflows/bundler-integrations/webpack.config.js
+++ .github/workflows/bundler-integrations/webpack.config.js
@@ -1,11 +1,28 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
 /** @type {import('webpack').Configuration} */
 module.exports = {
+  // Enable production mode for Webpack, as tree-shaking is a combination of
+  // - `usedExports` including, but not exporting code `export`s that are not used
+  // - `TerserPlugin` clearing unused code, effectively clearing the unused exports
+  //
+  // More details: https://webpack.js.org/guides/tree-shaking/
+  // (especially the end of 'Add a Utility', and 'Minify the Output')
+  mode: 'production',
   optimization: {
-    // Configure WebPack to drop exports that are not used from the code
-    // so we can disable Terser and still check what's being included
-    usedExports: true,
-    // Prevent minification so we can see what's going on in the built file
-    minimize: false
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          mangle: false,
+          // Reproduce Webpack's defaults
+          // https://webpack.js.org/configuration/optimization/#optimizationminimizer
+          compress: {
+            passes: 2
+          }
+        }
+      })
+    ]
   },
   target: 'web',
   entry: {
