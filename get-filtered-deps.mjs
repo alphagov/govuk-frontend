b6dcca594ad696c50b7c456cf8016d49fb5c8677
Owen Jones <owen.jones@digital.cabinet-office.gov.uk>
2024-06-05 10:13:50 +0100

    Establish looping and data collection

--- get-filtered-deps.mjs
+++ get-filtered-deps.mjs
@@ -1,11 +1,21 @@
 import { readFileSync } from 'fs'
 
 // eslint-disable-next-line import/no-unresolved
-import { Octokit } from 'octokit'
+import { Octokit, RequestError } from 'octokit'
 import { satisfies } from 'semver'
 
 const notAServiceWords = ['prototype', 'beta', 'alpha']
-const filteredDeps = []
+const initialDepsFilter = []
+const pageLimit = 10
+let repoOwner
+let repoName
+let currentVersion = false
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_AUTH_TOKEN
+})
+
+getDeps()
 
 async function getDeps() {
   // This is a really gross way to get the json files but we're running into
@@ -27,121 +37,130 @@ async function getDeps() {
       ownerList.includes(repo.owner) &&
       repo.name !== 'alphagov/govuk-frontend'
     ) {
-      filteredDeps.push(repo)
+      initialDepsFilter.push(repo)
     }
   }
 
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_AUTH_TOKEN
-  })
+  const filterTest = initialDepsFilter.slice(0, 10)
+  const filteredDeps = []
 
-  const repoOwner = 'alphagov'
-  const repoName = 'whitehall'
-  let lockfileName = 'package-lock.json'
-  let bisectResults
-  let page = 1
-  let lastCommitOfPreviousRange
-  try {
-    while (!bisectResults) {
-      console.log('Page', page)
-      let commitRange = await octokit.rest.repos.listCommits({
+  for (const dependent of filterTest) {
+    let bisectResults = false
+    let page = 1
+    let lastCommitOfPreviousRange
+
+    repoOwner = dependent.owner
+    repoName = dependent.repo_name
+
+    try {
+      console.log(`Analysing ${repoOwner}/${repoName}`)
+      console.log('Checking for a package.json...')
+      const packageCheck = await octokit.rest.repos.getContent({
         owner: repoOwner,
         repo: repoName,
-        path: lockfileName,
-        per_page: 64,
-        page
+        path: 'package.json',
+        headers: {
+          accept: 'application/vnd.github.raw+json'
+        }
       })
 
-      if (commitRange.data.length === 0) {
-        lockfileName = 'yarn.lock'
-        commitRange = await octokit.rest.repos.listCommits({
+      const packageHasGovukFrontend =
+        packageCheck.data.indexOf('"govuk-frontend"')
+
+      if (packageHasGovukFrontend === -1) {
+        throw new IndirectDependencyError()
+      }
+
+      while (!bisectResults && page <= pageLimit) {
+        console.log(`Page ${page} of commit list`)
+        const commitRange = await octokit.rest.repos.listCommits({
           owner: repoOwner,
           repo: repoName,
-          path: lockfileName,
+          path: 'package.json',
           per_page: 64,
           page
         })
+
+        if (commitRange.data.length === 0) {
+          console.log('no data :(')
+          break
+        }
+
+        console.log(`${commitRange.data.length} commits`)
+
+        if (lastCommitOfPreviousRange) {
+          bisectResults = await bisectDiffRange(
+            [lastCommitOfPreviousRange, commitRange.data[0]],
+            containsCrownUpdate
+          )
+        }
+
+        bisectResults =
+          bisectResults ||
+          (await bisectDiffRange(commitRange.data, containsCrownUpdate))
+        lastCommitOfPreviousRange =
+          commitRange.data[commitRange.data.length - 1]
+        page++
       }
 
-      if (commitRange.data.length === 0) {
-        console.log('no data :(')
-        return
-      }
-
-      if (lastCommitOfPreviousRange) {
-        bisectResults = await bisectDiffRange(
-          [lastCommitOfPreviousRange, commitRange.data[0]],
-          containsCrownUpdate
+      if (!bisectResults) {
+        console.log(
+          "We weren't able to finish bisecting for some reason. Attempting to finish analysis manually..."
         )
+
+        if (!currentVersion) {
+          currentVersion = packageCheck.data.substring(
+            packageHasGovukFrontend + 19,
+            packageCheck.data.indexOf('",', packageHasGovukFrontend)
+          )
+        }
       }
 
-      bisectResults =
-        bisectResults ||
-        (await bisectDiffRange(commitRange.data, containsCrownUpdate))
-      console.log(bisectResults)
-      lastCommitOfPreviousRange = commitRange.data[commitRange.data.length - 1]
-      page++
-    }
-  } catch (e) {
-    if (e instanceof IndirectDependencyError) {
-      console.log('Indirect dependency')
-      return
-    }
+      filteredDeps.push({
+        name: dependent.name,
+        currentVersion,
+        crownUpdateVersion: bisectResults?.commit?.author?.date
+      })
 
-    throw e
-  }
+      currentVersion = false
 
-  async function containsCrownUpdate(range) {
-    const depDiff = await octokit.rest.dependencyGraph.diffRange({
-      owner: repoOwner,
-      repo: repoName,
-      basehead: `${range[range.length - 1].sha}...${range[0].sha}`
-    })
+      console.log(`Analysis finished of ${dependent.name}`)
+    } catch (e) {
+      if (
+        e instanceof RequestError &&
+        e.response.url.endsWith('/package.json')
+      ) {
+        console.log(
+          `${repoOwner}/${repoName} doesn't have a package.json and therefore can't be using govuk-frontend directly. Skipping...`
+        )
+      } else if (e instanceof IndirectDependencyError) {
+        console.log(
+          `${repoOwner}/${repoName} is using govuk-frontend as an indirect dependancy. Skipping...`
+        )
+      } else if (e instanceof NotUsingCrownError) {
+        console.log(`${repoOwner}/${repoName} isn't using the new crown.`)
+        filteredDeps.push({
+          name: dependent.name,
+          currentVersion,
+          crownUpdateVersion: false
+        })
 
-    const frontendVersionChanges = depDiff.data.filter(
-      (item) => item.name === 'govuk-frontend'
-    )
-
-    // `govuk-frontend` may appear in lockfiles, but not be listed in `package.json`
-    // when brought as a dependency of a dependency. We can skip those by throwing
-    // (easier than returning)
-    if (
-      !frontendVersionChanges.find((item) => item.manifest === 'package.json')
-    ) {
-      throw new IndirectDependencyError()
-    }
-
-    const latestVersion = frontendVersionChanges.find(
-      (item) => item.change_type === 'added' && item.manifest === lockfileName
-    )?.version
-    const oldestVersion = frontendVersionChanges.find(
-      (item) => item.change_type === 'removed' && item.manifest === lockfileName
-    )?.version
-
-    if (!latestVersion) {
-      console.log('Missing added entry')
-      return false
-    }
-
-    if (!oldestVersion) {
-      console.log('Missing removed entry')
-      return false
-    }
-
-    if (
-      satisfies(latestVersion, '>=5.1.0') &&
-      satisfies(oldestVersion, '<5.1.0')
-    ) {
-      console.log('Crown update detected')
-      return true
+        currentVersion = false
+      } else {
+        throw e
+      }
     }
   }
+
+  console.log(filteredDeps)
 }
 
-getDeps()
-
-async function bisectDiffRange(range, func) {
-  console.log('we bisectin...', range.length)
+async function bisectDiffRange(
+  range,
+  func,
+  logMessage = 'starting new bisect...'
+) {
+  console.log(logMessage, range.length)
   if (await func(range)) {
     // Handle odd arrays where towards the end of the bisection
     // we'd get an array of only 1 element. That element will
@@ -164,11 +183,119 @@ async function bisectDiffRange(range, func) {
     const inBetweenRange = range.slice(rangeHalfLength - 1, rangeHalfLength + 1)
 
     return (
-      (await bisectDiffRange(firstHalf, func)) ||
-      (await bisectDiffRange(secondHalf, func)) ||
-      (await bisectDiffRange(inBetweenRange, func))
+      (await bisectDiffRange(firstHalf, func, 'bisecting first half')) ||
+      (await bisectDiffRange(secondHalf, func, 'bisecting second half')) ||
+      (await bisectDiffRange(inBetweenRange, func, 'bisecting between halves'))
     )
   }
 }
 
+async function containsCrownUpdate(range) {
+  const depDiff = await octokit.rest.dependencyGraph.diffRange({
+    owner: repoOwner,
+    repo: repoName,
+    basehead: `${range[range.length - 1].sha}...${range[0].sha}`
+  })
+
+  const frontendVersionChanges = depDiff.data.filter(
+    (item) => item.name === 'govuk-frontend'
+  )
+
+  if (frontendVersionChanges.length === 0) {
+    return false
+  }
+
+  // We try to define the latest and oldest versions twice using package-lock,
+  // yarn.lock and package.json before giving up
+
+  let latestVersion = getFrontendVersionsFromDiffRange(
+    frontendVersionChanges,
+    'added',
+    'package-lock.json'
+  )
+  let oldestVersion = getFrontendVersionsFromDiffRange(
+    frontendVersionChanges,
+    'removed',
+    'package-lock.json'
+  )
+
+  console.log(latestVersion, oldestVersion)
+
+  if (!latestVersion && !oldestVersion) {
+    latestVersion = getFrontendVersionsFromDiffRange(
+      frontendVersionChanges,
+      'added',
+      'yarn.lock'
+    )
+    oldestVersion = getFrontendVersionsFromDiffRange(
+      frontendVersionChanges,
+      'removed',
+      'yarn.lock'
+    )
+    console.log(latestVersion, oldestVersion)
+  }
+
+  if (!latestVersion && !oldestVersion) {
+    latestVersion = getFrontendVersionsFromDiffRange(
+      frontendVersionChanges,
+      'added',
+      'package.json'
+    )?.replace('^', '')
+    oldestVersion = getFrontendVersionsFromDiffRange(
+      frontendVersionChanges,
+      'removed',
+      'package.json'
+    )?.replace('^', '')
+    console.log(latestVersion, oldestVersion)
+  }
+
+  if (!latestVersion) {
+    console.log('Missing added entry')
+    return false
+  }
+
+  if (!currentVersion) {
+    currentVersion = latestVersion
+    console.log('Current version set', currentVersion)
+
+    if (
+      (satisfies(currentVersion, '<5.1.0') &&
+        satisfies(currentVersion, '>4.8.0')) ||
+      (satisfies(currentVersion, '<4.8.0') &&
+        satisfies(currentVersion, '>3.15.0')) ||
+      satisfies(currentVersion, '<3.15.0')
+    ) {
+      throw new NotUsingCrownError()
+    }
+  }
+
+  if (!oldestVersion) {
+    console.log('Missing removed entry')
+    return false
+  }
+
+  if (
+    (satisfies(latestVersion, '>=5.1.0') &&
+      satisfies(oldestVersion, '<5.1.0')) ||
+    (satisfies(latestVersion, '>=4.8.0') &&
+      satisfies(oldestVersion, '<4.8.0')) ||
+    (satisfies(latestVersion, '>=3.15.0') &&
+      satisfies(oldestVersion, '<3.15.0'))
+  ) {
+    console.log('Crown update detected')
+    return true
+  }
+}
+
+function getFrontendVersionsFromDiffRange(
+  versionChanges,
+  changeType,
+  lockfileName
+) {
+  return versionChanges.find(
+    (item) => item.change_type === changeType && item.manifest === lockfileName
+  )?.version
+}
+
 class IndirectDependencyError extends Error {}
+class NotUsingCrownError extends Error {}
