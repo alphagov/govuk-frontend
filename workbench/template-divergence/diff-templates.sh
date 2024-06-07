14143de5539582e9db1660e8f76ad7214d6f4e5c
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2024-06-07 17:25:26 +0100

    Add script to diff templates in 4.8.0 with 5.4.0

---
+++ workbench/template-divergence/diff-templates.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ex
+
+mkdir -p dist
+
+for f in ./node_modules/govuk-frontend/govuk/components/*; do
+
+  if [[ -d "${f}" ]]; then
+
+    COMPONENT_NAME=`basename ${f}`
+
+    # trailing `|| true` as `diff` exits with 1 when finding a difference
+    # which would make the script exit due to `set -e` at the top
+    diff ../../packages/govuk-frontend/src/govuk/components/${COMPONENT_NAME}/template.njk ./node_modules/govuk-frontend/govuk/components/${COMPONENT_NAME}/template.njk > dist/${COMPONENT_NAME}.diff || true
+  fi
+done
