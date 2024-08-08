7026715bdc9d534963316309ce6b636afd111775
beeps <hi+github@berly.kim>
2024-08-08 14:17:45 +0100

    Change wrapping element, make navigation required
    - Change wrapping element so that it’s a `section` if a service name is defined, or a `div` otherwise
    - Make navigation items required for the component
    - Update examples to account for navigation requirement

--- packages/govuk-frontend/src/govuk/components/service-navigation/service-navigation.puppeteer.test.js
+++ packages/govuk-frontend/src/govuk/components/service-navigation/service-navigation.puppeteer.test.js
@@ -18,204 +18,201 @@ describe('/components/service-navigation', () => {
     await page.emulate(iPhone)
   })
 
-  describe('/components/service-navigation/with-navigation', () => {
-    describe('when JavaScript is unavailable or fails', () => {
+  describe('when JavaScript is unavailable or fails', () => {
+    beforeAll(async () => {
+      await page.setJavaScriptEnabled(false)
+      await render(page, 'service-navigation', examples.default)
+    })
+
+    afterAll(async () => {
+      await page.setJavaScriptEnabled(true)
+    })
+
+    it('renders navigation expanded by default', async () => {
+      const navigationIsVisible = await page.waitForSelector(
+        navigationSelector,
+        { visible: true, timeout: 10000 }
+      )
+
+      expect(navigationIsVisible).toBeTruthy()
+    })
+
+    it('renders the toggle button hidden', async () => {
+      const buttonHiddenAttribute = await page.$eval(
+        toggleButtonSelector,
+        (el) => el.hasAttribute('hidden')
+      )
+
+      expect(buttonHiddenAttribute).toBeTruthy()
+    })
+  })
+
+  describe('when JavaScript is available', () => {
+    describe('if no navigation is present', () => {
+      it('exits gracefully with no errors', async () => {
+        // Errors logged to the console will cause this test to fail
+        return expect(
+          render(page, 'service-navigation', examples.default)
+        ).resolves.not.toThrow()
+      })
+    })
+
+    describe('errors at instantiation', () => {
+      it('can throw a SupportError if appropriate', async () => {
+        await expect(
+          render(page, 'service-navigation', examples.default, {
+            beforeInitialisation() {
+              document.body.classList.remove('govuk-frontend-supported')
+            }
+          })
+        ).rejects.toMatchObject({
+          cause: {
+            name: 'SupportError',
+            message:
+              'GOV.UK Frontend initialised without `<body class="govuk-frontend-supported">` from template `<script>` snippet'
+          }
+        })
+      })
+
+      it('throws when $module is not set', async () => {
+        await expect(
+          render(page, 'service-navigation', examples.default, {
+            beforeInitialisation($module) {
+              // Remove the root of the components as a way
+              // for the constructor to receive the wrong type for `$module`
+              $module.remove()
+            }
+          })
+        ).rejects.toMatchObject({
+          cause: {
+            name: 'ElementError',
+            message: 'Service Navigation: Root element (`$module`) not found'
+          }
+        })
+      })
+
+      it("throws when the toggle's aria-control attribute is missing", async () => {
+        await expect(
+          render(page, 'service-navigation', examples.default, {
+            beforeInitialisation($module, { selector }) {
+              $module.querySelector(selector).removeAttribute('aria-controls')
+            },
+            context: {
+              selector: toggleButtonSelector
+            }
+          })
+        ).rejects.toMatchObject({
+          cause: {
+            name: 'ElementError',
+            message:
+              'Service Navigation: Navigation button (`<button class="govuk-js-service-navigation-toggle">`) attribute (`aria-controls`) not found'
+          }
+        })
+      })
+
+      it('throws when the menu is missing, but a toggle is present', async () => {
+        await expect(
+          render(page, 'service-navigation', examples.default, {
+            beforeInitialisation($module, { selector }) {
+              // Remove the `<ul>` referenced by $menuButton's `aria-controls`
+              $module.querySelector(selector).remove()
+            },
+            context: {
+              selector: navigationSelector
+            }
+          })
+        ).rejects.toMatchObject({
+          cause: {
+            name: 'ElementError',
+            message:
+              'Service Navigation: Navigation (`<ul id="navigation">`) not found'
+          }
+        })
+      })
+    })
+
+    describe('on page load', () => {
       beforeAll(async () => {
-        await page.setJavaScriptEnabled(false)
-        await render(page, 'service-navigation', examples['with navigation'])
+        await render(page, 'service-navigation', examples.default)
       })
 
-      afterAll(async () => {
-        await page.setJavaScriptEnabled(true)
-      })
-
-      it('renders navigation expanded by default', async () => {
-        const navigationIsVisible = await page.waitForSelector(
-          navigationSelector,
-          { visible: true, timeout: 10000 }
+      it('renders navigation hidden by default', async () => {
+        const navigationIsHidden = await page.$eval(navigationSelector, (el) =>
+          el.hasAttribute('hidden')
         )
 
-        expect(navigationIsVisible).toBeTruthy()
+        expect(navigationIsHidden).toBeTruthy()
       })
 
-      it('renders the toggle button hidden', async () => {
+      it('renders the toggle button visibly', async () => {
         const buttonHiddenAttribute = await page.$eval(
           toggleButtonSelector,
           (el) => el.hasAttribute('hidden')
         )
 
-        expect(buttonHiddenAttribute).toBeTruthy()
+        expect(buttonHiddenAttribute).toBeFalsy()
+      })
+
+      it('renders the toggle button with `aria-expanded` set to false', async () => {
+        const toggleExpandedAttribute = await page.$eval(
+          toggleButtonSelector,
+          (el) => el.getAttribute('aria-expanded')
+        )
+
+        expect(toggleExpandedAttribute).toBe('false')
       })
     })
 
-    describe('when JavaScript is available', () => {
-      describe('if no navigation is present', () => {
-        it('exits gracefully with no errors', async () => {
-          // Errors logged to the console will cause this test to fail
-          return expect(
-            render(page, 'service-navigation', examples.default)
-          ).resolves.not.toThrow()
-        })
+    describe('when toggle button is clicked once', () => {
+      beforeAll(async () => {
+        await render(page, 'service-navigation', examples.default)
+        await page.waitForSelector(toggleButtonSelector)
+        await page.click(toggleButtonSelector)
       })
 
-      describe('errors at instantiation', () => {
-        it('can throw a SupportError if appropriate', async () => {
-          await expect(
-            render(page, 'service-navigation', examples.default, {
-              beforeInitialisation() {
-                document.body.classList.remove('govuk-frontend-supported')
-              }
-            })
-          ).rejects.toMatchObject({
-            cause: {
-              name: 'SupportError',
-              message:
-                'GOV.UK Frontend initialised without `<body class="govuk-frontend-supported">` from template `<script>` snippet'
-            }
-          })
-        })
+      it('shows the navigation', async () => {
+        const navigationHiddenAttribute = await page.$eval(
+          navigationSelector,
+          (el) => el.hasAttribute('hidden')
+        )
 
-        it('throws when $module is not set', async () => {
-          await expect(
-            render(page, 'service-navigation', examples.default, {
-              beforeInitialisation($module) {
-                // Remove the root of the components as a way
-                // for the constructor to receive the wrong type for `$module`
-                $module.remove()
-              }
-            })
-          ).rejects.toMatchObject({
-            cause: {
-              name: 'ElementError',
-              message: 'Service Navigation: Root element (`$module`) not found'
-            }
-          })
-        })
-
-        it("throws when the toggle's aria-control attribute is missing", async () => {
-          await expect(
-            render(page, 'service-navigation', examples['with navigation'], {
-              beforeInitialisation($module, { selector }) {
-                $module.querySelector(selector).removeAttribute('aria-controls')
-              },
-              context: {
-                selector: toggleButtonSelector
-              }
-            })
-          ).rejects.toMatchObject({
-            cause: {
-              name: 'ElementError',
-              message:
-                'Service Navigation: Navigation button (`<button class="govuk-js-service-navigation-toggle">`) attribute (`aria-controls`) not found'
-            }
-          })
-        })
-
-        it('throws when the menu is missing, but a toggle is present', async () => {
-          await expect(
-            render(page, 'service-navigation', examples['with navigation'], {
-              beforeInitialisation($module, { selector }) {
-                // Remove the `<ul>` referenced by $menuButton's `aria-controls`
-                $module.querySelector(selector).remove()
-              },
-              context: {
-                selector: navigationSelector
-              }
-            })
-          ).rejects.toMatchObject({
-            cause: {
-              name: 'ElementError',
-              message:
-                'Service Navigation: Navigation (`<ul id="navigation">`) not found'
-            }
-          })
-        })
+        expect(navigationHiddenAttribute).toBeFalsy()
       })
 
-      describe('on page load', () => {
-        beforeAll(async () => {
-          await render(page, 'service-navigation', examples['with navigation'])
-        })
+      it('sets `aria-expanded` to true', async () => {
+        const toggleExpandedAttribute = await page.$eval(
+          toggleButtonSelector,
+          (el) => el.getAttribute('aria-expanded')
+        )
 
-        it('renders navigation hidden by default', async () => {
-          const navigationIsHidden = await page.$eval(
-            navigationSelector,
-            (el) => el.hasAttribute('hidden')
-          )
+        expect(toggleExpandedAttribute).toBe('true')
+      })
+    })
 
-          expect(navigationIsHidden).toBeTruthy()
-        })
-
-        it('renders the toggle button visibly', async () => {
-          const buttonHiddenAttribute = await page.$eval(
-            toggleButtonSelector,
-            (el) => el.hasAttribute('hidden')
-          )
-
-          expect(buttonHiddenAttribute).toBeFalsy()
-        })
-
-        it('renders the toggle button with `aria-expanded` set to false', async () => {
-          const toggleExpandedAttribute = await page.$eval(
-            toggleButtonSelector,
-            (el) => el.getAttribute('aria-expanded')
-          )
-
-          expect(toggleExpandedAttribute).toBe('false')
-        })
+    describe('when toggle button is clicked twice', () => {
+      beforeAll(async () => {
+        await render(page, 'service-navigation', examples.default)
+        await page.waitForSelector(toggleButtonSelector)
+        await page.click(toggleButtonSelector)
+        await page.click(toggleButtonSelector)
       })
 
-      describe('when toggle button is clicked once', () => {
-        beforeAll(async () => {
-          await render(page, 'service-navigation', examples['with navigation'])
-          await page.waitForSelector(toggleButtonSelector)
-          await page.click(toggleButtonSelector)
-        })
+      it('hides the navigation', async () => {
+        const navigationHiddenAttribute = await page.$eval(
+          navigationSelector,
+          (el) => el.hasAttribute('hidden')
+        )
 
-        it('shows the navigation', async () => {
-          const navigationHiddenAttribute = await page.$eval(
-            navigationSelector,
-            (el) => el.hasAttribute('hidden')
-          )
-
-          expect(navigationHiddenAttribute).toBeFalsy()
-        })
-
-        it('sets `aria-expanded` to true', async () => {
-          const toggleExpandedAttribute = await page.$eval(
-            toggleButtonSelector,
-            (el) => el.getAttribute('aria-expanded')
-          )
-
-          expect(toggleExpandedAttribute).toBe('true')
-        })
+        expect(navigationHiddenAttribute).toBeTruthy()
       })
 
-      describe('when toggle button is clicked twice', () => {
-        beforeAll(async () => {
-          await render(page, 'service-navigation', examples['with navigation'])
-          await page.waitForSelector(toggleButtonSelector)
-          await page.click(toggleButtonSelector)
-          await page.click(toggleButtonSelector)
-        })
+      it('sets `aria-expanded` to false', async () => {
+        const toggleExpandedAttribute = await page.$eval(
+          toggleButtonSelector,
+          (el) => el.getAttribute('aria-expanded')
+        )
 
-        it('hides the navigation', async () => {
-          const navigationHiddenAttribute = await page.$eval(
-            navigationSelector,
-            (el) => el.hasAttribute('hidden')
-          )
-
-          expect(navigationHiddenAttribute).toBeTruthy()
-        })
-
-        it('sets `aria-expanded` to false', async () => {
-          const toggleExpandedAttribute = await page.$eval(
-            toggleButtonSelector,
-            (el) => el.getAttribute('aria-expanded')
-          )
-
-          expect(toggleExpandedAttribute).toBe('false')
-        })
+        expect(toggleExpandedAttribute).toBe('false')
       })
     })
   })
