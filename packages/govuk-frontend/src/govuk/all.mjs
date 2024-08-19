9eb4e2990801f0175cf14b4d8dd847d477be661f
Patrick Cartlidge <patrickcartlidge@gmail.com>
2024-08-19 11:27:24 +0100

    Add `isSupported` to `all.mjs`
    - adds `isSupported` as an export for import from `govuk-frontend`
    - removes `internal` from JSDoc to mark function as public
    - adds `exports isSupported function` test
    - updates `exports Components` test

--- packages/govuk-frontend/src/govuk/all.mjs
+++ packages/govuk-frontend/src/govuk/all.mjs
@@ -13,6 +13,7 @@ export { ServiceNavigation } from './components/service-navigation/service-navig
 export { SkipLink } from './components/skip-link/skip-link.mjs'
 export { Tabs } from './components/tabs/tabs.mjs'
 export { initAll, createAll } from './init.mjs'
+export { isSupported } from './common/index.mjs'
 
 /**
  * @typedef {import('./init.mjs').Config} Config
