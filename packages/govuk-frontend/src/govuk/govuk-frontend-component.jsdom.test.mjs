ee7513df43a43d177179cceb53034eff65d438a4
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2024-09-16 16:46:03 +0100

    Encapsulate check for components support in method of `GOVUKFrontendComponent`
    This will allow subclasses to override when components are supported, possibly combining it with when GOV.UK Frontend itself is supported

---
+++ packages/govuk-frontend/src/govuk/govuk-frontend-component.jsdom.test.mjs
@@ -0,0 +1,44 @@
+import { SupportError } from './errors/index.mjs'
+import { GOVUKFrontendComponent } from './govuk-frontend-component.mjs'
+
+describe('GOVUKFrontendComponent', () => {
+  describe('isSupported()', () => {
+    beforeEach(() => {
+      // Jest does not tidy the JSDOM document between tests
+      // so we need to take care of that ourselves
+      document.documentElement.innerHTML = ''
+    })
+
+    describe('default implementation', () => {
+      class ServiceComponent extends GOVUKFrontendComponent {
+        static moduleName = 'app-service-component'
+      }
+
+      it('Makes initialisation throw if GOV.UK Frontend is not supported', () => {
+        expect(() => new ServiceComponent(document.body)).toThrow(SupportError)
+      })
+
+      it('Allows initialisation if GOV.UK Frontend is supported', () => {
+        document.body.classList.add('govuk-frontend-supported')
+
+        expect(() => new ServiceComponent(document.body)).not.toThrow()
+      })
+    })
+
+    describe('when overriden', () => {
+      it('Allows child classes to define their own condition for support', () => {
+        class ServiceComponent extends GOVUKFrontendComponent {
+          static moduleName = 'app-service-component'
+
+          isSupported() {
+            return true
+          }
+        }
+
+        expect(() => new ServiceComponent(document.body)).not.toThrow(
+          SupportError
+        )
+      })
+    })
+  })
+})
