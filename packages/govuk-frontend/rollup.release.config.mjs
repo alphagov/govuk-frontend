2a63f5c795a8a7065c8f372fe6b1e59ba1e6071e
Patrick Cartlidge <patrickcartlidge@gmail.com>
2024-10-02 16:37:19 +0100

    Refactor `GOVUKFrontendComponent` `root` typing
    - Moves the type check of `root` into `GOVUKFrontendComponent` base
    class which components in the Design System extend
    - Moves assignment of `root` into the `GOVUKFrontendComponent` base
    class which components in the Design System extend
    - Adds `elementType` which specifies `type` of `root` to be checked and
    can be overloaded by child classes
    - Add `getGOVUKFrontendExportsNames` to `rollup.release.config.mjs`
    which ensures that we don't run into an error in which `window.*` is
    undefined during build

--- packages/govuk-frontend/rollup.release.config.mjs
+++ packages/govuk-frontend/rollup.release.config.mjs
@@ -2,69 +2,86 @@ import config from '@govuk-frontend/config'
 import { babel } from '@rollup/plugin-babel'
 import replace from '@rollup/plugin-replace'
 import terser from '@rollup/plugin-terser'
-import * as GOVUKFrontend from 'govuk-frontend/src/govuk/all.mjs'
 import { defineConfig } from 'rollup'
+
+async function getGOVUKFrontendExportsNames() {
+  try {
+    global.HTMLElement = /** @type {any} */ (function () {})
+    global.HTMLAnchorElement = /** @type {any} */ (function () {})
+    return Object.keys(await import('govuk-frontend/src/govuk/all.mjs'))
+  } finally {
+    delete global.HTMLElement
+    delete global.HTMLAnchorElement
+  }
+}
 
 /**
  * Rollup config for GitHub release
  *
  * ECMAScript (ES) module bundles for browser <script type="module">
  * or using `import` for modern browsers and Node.js scripts
+ *
+ * @param {import('rollup').RollupOptions} input
+ * @returns {Promise<import('rollup').RollupOptions|import('rollup').RollupOptions[]>} rollup config
  */
-export default defineConfig(({ i: input }) => ({
-  input,
+export default async (input) => {
+  const GOVUKFrontendComponents = await getGOVUKFrontendExportsNames()
 
-  /**
-   * Output options
-   */
-  output: {
-    compact: true,
-    format: 'es',
-
-    // Bundled modules
-    preserveModules: false,
+  return defineConfig(({ i: input }) => ({
+    input,
 
     /**
-     * Output plugins
+     * Output options
+     */
+    output: {
+      compact: true,
+      format: 'es',
+
+      // Bundled modules
+      preserveModules: false,
+
+      /**
+       * Output plugins
+       */
+      plugins: [
+        terser({
+          // Allow Terser to remove @preserve comments
+          format: { comments: false },
+
+          mangle: {
+            keep_classnames: true,
+            keep_fnames: true,
+            // Ensure all top-level exports skip mangling, for example
+            // non-function string constants like `export { version }`
+            reserved: GOVUKFrontendComponents
+          },
+
+          // Include sources content from source maps to inspect
+          // GOV.UK Frontend and other dependencies' source code
+          sourceMap: {
+            includeSources: true
+          },
+
+          // Compatibility workarounds
+          safari10: true
+        })
+      ]
+    },
+
+    /**
+     * Input plugins
      */
     plugins: [
-      terser({
-        // Allow Terser to remove @preserve comments
-        format: { comments: false },
+      replace({
+        include: '**/common/govuk-frontend-version.mjs',
+        preventAssignment: true,
 
-        mangle: {
-          keep_classnames: true,
-          keep_fnames: true,
-          // Ensure all top-level exports skip mangling, for example
-          // non-function string constants like `export { version }`
-          reserved: Object.keys(GOVUKFrontend)
-        },
-
-        // Include sources content from source maps to inspect
-        // GOV.UK Frontend and other dependencies' source code
-        sourceMap: {
-          includeSources: true
-        },
-
-        // Compatibility workarounds
-        safari10: true
+        // Add GOV.UK Frontend release version
+        development: config.version
+      }),
+      babel({
+        babelHelpers: 'bundled'
       })
     ]
-  },
-
-  /**
-   * Input plugins
-   */
-  plugins: [
-    replace({
-      include: '**/common/govuk-frontend-version.mjs',
-      preventAssignment: true,
-
-      // Add GOV.UK Frontend release version
-      development: config.version
-    }),
-    babel({
-      babelHelpers: 'bundled'
-    })
-  ]
-}))
+  }))(input)
+}
