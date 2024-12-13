045212487f18fd6f6effd03bbf127d63a7653075
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2024-12-13 18:32:56 +0000

    Move creation of the Show/Hide toggle to AccordionSection

--- packages/govuk-frontend/src/govuk/components/accordion/accordion-section.mjs
+++ packages/govuk-frontend/src/govuk/components/accordion/accordion-section.mjs
@@ -1,3 +1,4 @@
+import { createElement } from '../../common/create-element.mjs'
 import { Component } from '../../component.mjs'
 import { ElementError } from '../../errors/index.mjs'
 
@@ -24,5 +25,47 @@ export class AccordionSection extends Component {
         identifier: `Section headers (\`<div class="govuk-accordion__section-header">\`)`
       })
     }
+
+    this.$toggleIcon = createElement('span', {
+      class: 'govuk-accordion-nav__chevron'
+    })
+
+    this.$toggleText = createElement('span', {
+      class: 'govuk-accordion__section-toggle-text'
+    })
+
+    this.$toggle = createShowHideToggle(this.$toggleIcon, this.$toggleText)
   }
+}
+
+/**
+ * Creates a `<span>` rendering the 'Show'/'Hide' toggle
+ *
+ * @param {Node} $toggleIcon - The element for the toggle's icon
+ * @param {Node} $toggleText - The element for the toggle's text
+ * @returns {HTMLSpanElement} - The `<span>` with the visual representation of the 'Show/Hide' toggle
+ */
+function createShowHideToggle($toggleIcon, $toggleText) {
+  // Create an inner container to limit the width of the focus state
+  const $showHideToggleFocus = createElement(
+    'span',
+    {
+      class: 'govuk-accordion__section-toggle-focus'
+    },
+    [$toggleIcon, $toggleText]
+  )
+
+  const $showHideToggle = createElement(
+    'span',
+    {
+      class: 'govuk-accordion__section-toggle',
+      // Tell Google not to index the 'show' text as part of the heading. Must be
+      // set on the element before it's added to the DOM.
+      // See https://developers.google.com/search/docs/advanced/robots/robots_meta_tag#data-nosnippet-attr
+      'data-nosnippet': ''
+    },
+    [$showHideToggleFocus]
+  )
+
+  return $showHideToggle
 }
--- packages/govuk-frontend/src/govuk/components/accordion/accordion.mjs
+++ packages/govuk-frontend/src/govuk/components/accordion/accordion.mjs
@@ -231,47 +231,10 @@ export class Accordion extends ConfigurableComponent {
       $button.appendChild(this.getButtonPunctuationEl())
     }
 
-    $button.appendChild(this.createShowHideToggle())
+    $button.appendChild(section.$toggle)
 
     $heading.removeChild($span)
     $heading.appendChild($button)
-  }
-
-  /**
-   * Creates a `<span>` rendering the 'Show'/'Hide' toggle
-   *
-   * @returns {HTMLSpanElement} - The `<span>` with the visual representation of the 'Show/Hide' toggle
-   */
-  createShowHideToggle() {
-    // Create an inner container to limit the width of the focus state
-    const $showHideToggleFocus = createElement(
-      'span',
-      {
-        class: 'govuk-accordion__section-toggle-focus'
-      },
-      [
-        createElement('span', {
-          class: iconClass
-        }),
-        createElement('span', {
-          class: sectionToggleTextClass
-        })
-      ]
-    )
-
-    const $showHideToggle = createElement(
-      'span',
-      {
-        class: 'govuk-accordion__section-toggle',
-        // Tell Google not to index the 'show' text as part of the heading. Must be
-        // set on the element before it's added to the DOM.
-        // See https://developers.google.com/search/docs/advanced/robots/robots_meta_tag#data-nosnippet-attr
-        'data-nosnippet': ''
-      },
-      [$showHideToggleFocus]
-    )
-
-    return $showHideToggle
   }
 
   /**
