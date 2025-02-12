b6007f649a432f4257d542b8b1ead51a79a7402f
Patrick Cartlidge <patrickcartlidge@gmail.com>
2025-02-12 14:55:38 +0000

    No `--dragging` if File Upload is disabled

--- packages/govuk-frontend/src/govuk/components/file-upload/file-upload.mjs
+++ packages/govuk-frontend/src/govuk/components/file-upload/file-upload.mjs
@@ -221,6 +221,8 @@ export class FileUpload extends ConfigurableComponent {
    * @param {DragEvent} event - The `dragenter` event
    */
   updateDropzoneVisibility(event) {
+    if (this.$button.disabled) return
+
     // DOM interfaces only type `event.target` as `EventTarget`
     // so we first need to make sure it's a `Node`
     if (event.target instanceof Node) {
