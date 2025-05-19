954550474261a4abb31173012455aa41bd711a24
Colin Rotherham <work@colinr.com>
2025-05-19 18:27:51 +0100

    Add tests for components, overrides, utilities

---
+++ packages/govuk-frontend/src/govuk/overrides/overrides.unit.test.mjs
@@ -0,0 +1,29 @@
+import { join } from 'path'
+
+import { paths } from '@govuk-frontend/config'
+import { compileSassFile } from '@govuk-frontend/helpers/tests'
+import { getListing } from '@govuk-frontend/lib/files'
+
+describe('Overrides', () => {
+  let sassFiles
+
+  beforeAll(async () => {
+    sassFiles = await getListing('**/src/govuk/overrides/**/*.scss', {
+      cwd: paths.package,
+      ignore: ['**/_all.scss', '**/_index.scss']
+    })
+  })
+
+  it('renders CSS for all overrides', () => {
+    const sassTasks = sassFiles.map((sassFilePath) => {
+      const file = join(paths.package, sassFilePath)
+
+      return expect(compileSassFile(file)).resolves.toMatchObject({
+        css: expect.any(String),
+        loadedUrls: expect.arrayContaining([expect.any(URL)])
+      })
+    })
+
+    return Promise.all(sassTasks)
+  })
+})
