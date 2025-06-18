2773949059e69faad170da82d2400c02cbbe3b20
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2025-06-18 08:33:36 +0100

    Update button template tests to use jsdom

--- packages/govuk-frontend/src/govuk/components/button/template.jsdom.test.js
+++ packages/govuk-frontend/src/govuk/components/button/template.jsdom.test.js
@@ -1,5 +1,4 @@
-const { render } = require('@govuk-frontend/helpers/nunjucks')
-const { getExamples } = require('@govuk-frontend/lib/components')
+const { getExamples, render } = require('@govuk-frontend/lib/components')
 
 describe('Button', () => {
   let examples
@@ -10,200 +9,211 @@ describe('Button', () => {
 
   describe('default example', () => {
     it('renders the default example', () => {
-      const $ = render('button', examples.default)
+      document.body.innerHTML = render('button', examples.default)
 
-      const $component = $('.govuk-button')
-      expect($component.get(0).tagName).toBe('button')
-      expect($component.text()).toContain('Save and continue')
+      const $component = document.querySelector('.govuk-button')
+      expect($component.tagName).toBe('BUTTON')
+      expect($component).toHaveTextContent('Save and continue')
     })
   })
 
   describe('custom options', () => {
     it('renders with attributes', () => {
-      const $ = render('button', examples.attributes)
+      document.body.innerHTML = render('button', examples.attributes)
 
-      const $component = $('.govuk-button')
-      expect($component.attr('aria-controls')).toBe('test-target-element')
-      expect($component.attr('data-tracking-dimension')).toBe('123')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('aria-controls', 'test-target-element')
+      expect($component).toHaveAttribute('data-tracking-dimension', '123')
     })
 
     it('renders with classes', () => {
-      const $ = render('button', examples.classes)
+      document.body.innerHTML = render('button', examples.classes)
 
-      const $component = $('.govuk-button')
-      expect($component.hasClass('app-button--custom-modifier')).toBeTruthy()
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveClass('app-button--custom-modifier')
     })
 
     it('renders with disabled', () => {
-      const $ = render('button', examples.disabled)
+      document.body.innerHTML = render('button', examples.disabled)
 
-      const $component = $('.govuk-button')
-      expect($component.attr('aria-disabled')).toBe('true')
-      expect($component.attr('disabled')).toBe('disabled')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('aria-disabled', 'true')
+      expect($component).toBeDisabled()
     })
 
     it('renders with name', () => {
-      const $ = render('button', examples.name)
+      document.body.innerHTML = render('button', examples.name)
 
-      const $component = $('.govuk-button')
-      expect($component.attr('name')).toBe('start-now')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('name', 'start-now')
     })
 
     it('renders with id', () => {
-      const $ = render('button', examples.id)
+      document.body.innerHTML = render('button', examples.id)
 
-      const $component = $('.govuk-button')
-      expect($component.attr('id')).toBe('submit')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('id', 'submit')
     })
 
     it('renders with value', () => {
-      const $ = render('button', examples.value)
+      document.body.innerHTML = render('button', examples.value)
 
-      const $component = $('.govuk-button')
-      expect($component.attr('value')).toBe('start')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveValue('start')
     })
 
     it('renders with type', () => {
-      const $ = render('button', examples.type)
+      document.body.innerHTML = render('button', examples.type)
 
-      const $component = $('.govuk-button')
-      expect($component.attr('type')).toBe('button')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('type', 'button')
     })
 
     it('renders with html', () => {
-      const $ = render('button', examples.html)
+      document.body.innerHTML = render('button', examples.html)
 
-      const $component = $('.govuk-button')
-      expect($component.html()).toContain('Start <em>now</em>')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toContainHTML('Start <em>now</em>')
     })
 
     describe('preventDoubleClick', () => {
       it('does not render the attribute if not set', () => {
-        const $ = render('button', examples['no data-prevent-double-click'])
+        document.body.innerHTML = render(
+          'button',
+          examples['no data-prevent-double-click']
+        )
 
-        const $component = $('.govuk-button')
-        expect($component.attr('data-prevent-double-click')).toBeUndefined()
+        const $component = document.querySelector('.govuk-button')
+        expect($component).not.toHaveAttribute('data-prevent-double-click')
       })
 
       it('renders with preventDoubleClick attribute set to true', () => {
-        const $ = render('button', examples['prevent double click'])
+        document.body.innerHTML = render(
+          'button',
+          examples['prevent double click']
+        )
 
-        const $component = $('.govuk-button')
-        expect($component.attr('data-prevent-double-click')).toBe('true')
+        const $component = document.querySelector('.govuk-button')
+        expect($component).toHaveAttribute('data-prevent-double-click', 'true')
       })
 
       it('renders with preventDoubleClick attribute set to false', () => {
-        const $ = render('button', examples["don't prevent double click"])
+        document.body.innerHTML = render(
+          'button',
+          examples["don't prevent double click"]
+        )
 
-        const $component = $('.govuk-button')
-        expect($component.attr('data-prevent-double-click')).toBe('false')
+        const $component = document.querySelector('.govuk-button')
+        expect($component).toHaveAttribute('data-prevent-double-click', 'false')
       })
     })
   })
 
   describe('link', () => {
     it('renders with anchor, href and an accessible role of button', () => {
-      const $ = render('button', examples['explicit link'])
+      document.body.innerHTML = render('button', examples['explicit link'])
 
-      const $component = $('.govuk-button')
-      expect($component.get(0).tagName).toBe('a')
-      expect($component.attr('href')).toBe('/')
-      expect($component.attr('role')).toBe('button')
-      expect($component.text()).toContain('Continue')
+      const $component = document.querySelector('.govuk-button')
+      expect($component.tagName).toBe('A')
+      expect($component).toHaveAttribute('href', '/')
+      expect($component).toHaveRole('button')
+      expect($component).toHaveTextContent('Continue')
     })
 
     it('renders with hash href if no href passed', () => {
-      const $ = render('button', examples['no href'])
+      document.body.innerHTML = render('button', examples['no href'])
 
-      const $component = $('.govuk-button')
-      expect($component.attr('href')).toBe('#')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('href', '#')
     })
 
     it('renders with attributes', () => {
-      const $ = render('button', examples['link attributes'])
+      document.body.innerHTML = render('button', examples['link attributes'])
 
-      const $component = $('.govuk-button')
-      expect($component.attr('aria-controls')).toBe('test-target-element')
-      expect($component.attr('data-tracking-dimension')).toBe('123')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('aria-controls', 'test-target-element')
+      expect($component).toHaveAttribute('data-tracking-dimension', '123')
     })
 
     it('renders with classes', () => {
-      const $ = render('button', examples['link classes'])
+      document.body.innerHTML = render('button', examples['link classes'])
 
-      const $component = $('.govuk-button')
-      expect($component.hasClass('app-button--custom-modifier')).toBeTruthy()
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveClass('app-button--custom-modifier')
     })
   })
 
   describe('with explicit input button set by "element"', () => {
     it('renders with anchor, href and an accessible role of button', () => {
-      const $ = render('button', examples.input)
+      document.body.innerHTML = render('button', examples.input)
 
-      const $component = $('.govuk-button')
-      expect($component.get(0).tagName).toBe('input')
-      expect($component.attr('type')).toBe('submit')
+      const $component = document.querySelector('.govuk-button')
+      expect($component.tagName).toBe('INPUT')
+      expect($component).toHaveAttribute('type', 'submit')
     })
 
     it('renders with attributes', () => {
-      const $ = render('button', examples['input attributes'])
+      document.body.innerHTML = render('button', examples['input attributes'])
 
-      const $component = $('.govuk-button')
-      expect($component.attr('aria-controls')).toBe('test-target-element')
-      expect($component.attr('data-tracking-dimension')).toBe('123')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('aria-controls', 'test-target-element')
+      expect($component).toHaveAttribute('data-tracking-dimension', '123')
     })
 
     it('renders with classes', () => {
-      const $ = render('button', examples['input classes'])
+      document.body.innerHTML = render('button', examples['input classes'])
 
-      const $component = $('.govuk-button')
-      expect($component.hasClass('app-button--custom-modifier')).toBeTruthy()
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveClass('app-button--custom-modifier')
     })
 
     it('renders with disabled', () => {
-      const $ = render('button', examples['input disabled'])
+      document.body.innerHTML = render('button', examples['input disabled'])
 
-      const $component = $('.govuk-button')
-      expect($component.attr('aria-disabled')).toBe('true')
-      expect($component.attr('disabled')).toBe('disabled')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('aria-disabled', 'true')
+      expect($component).toBeDisabled()
     })
 
     it('renders with name', () => {
-      const $ = render('button', examples.input)
+      document.body.innerHTML = render('button', examples.input)
 
-      const $component = $('.govuk-button')
-      expect($component.attr('name')).toBe('start-now')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('name', 'start-now')
     })
 
     it('renders with type', () => {
-      const $ = render('button', examples['input type'])
+      document.body.innerHTML = render('button', examples['input type'])
 
-      const $component = $('.govuk-button')
-      expect($component.attr('type')).toBe('button')
+      const $component = document.querySelector('.govuk-button')
+      expect($component).toHaveAttribute('type', 'button')
     })
   })
 
   describe('implicitly as no "element" param is set', () => {
     it('renders a link if you pass an href', () => {
-      const $ = render('button', examples.link)
+      document.body.innerHTML = render('button', examples.link)
 
-      const $component = $('.govuk-button')
-      expect($component.get(0).tagName).toBe('a')
+      const $component = document.querySelector('.govuk-button')
+      expect($component.tagName).toBe('A')
     })
 
     it("renders a button if you don't pass anything", () => {
-      const $ = render('button', examples['no type'])
+      document.body.innerHTML = render('button', examples['no type'])
 
-      const $component = $('.govuk-button')
-      expect($component.get(0).tagName).toBe('button')
+      const $component = document.querySelector('.govuk-button')
+      expect($component.tagName).toBe('BUTTON')
     })
   })
 
   describe('Start button', () => {
     it('renders a svg', () => {
-      const $ = render('button', examples['start link'])
+      document.body.innerHTML = render('button', examples['start link'])
 
-      const $component = $('.govuk-button .govuk-button__start-icon')
-      expect($component.get(0).tagName).toBe('svg')
+      const $component = document.querySelector(
+        '.govuk-button .govuk-button__start-icon'
+      )
+      expect($component.tagName).toBe('svg')
     })
   })
 })
