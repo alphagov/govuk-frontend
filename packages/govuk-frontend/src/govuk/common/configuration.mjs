40feb523f0b99638463485156de50aaaf97c6bee
Colin Rotherham <work@colinr.com>
2025-08-09 20:41:04 +0100

    Document new helper as internal only

--- packages/govuk-frontend/src/govuk/common/configuration.mjs
+++ packages/govuk-frontend/src/govuk/common/configuration.mjs
@@ -199,6 +199,7 @@ export function normaliseDataset(Component, dataset) {
 /**
  * Normalise options passed to `initAll` or `createAll`
  *
+ * @internal
  * @template {CompatibleClass} ComponentClass
  * @param {Config | CreateAllOptions<ComponentClass> | OnErrorCallback<ComponentClass> | Element | Document | null} [scopeOrOptions] - Scope of the document to search within, initialisation options or error callback function
  * @returns {CreateAllOptions<ComponentClass>} Normalised options
