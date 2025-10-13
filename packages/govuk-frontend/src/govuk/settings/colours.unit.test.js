bb68d229f01effd2e1ca6f8cc0d4fb9a4b43a36c
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2025-10-13 16:05:55 +0100

    Add brand colours as their own palette
    Add a separate palette for the brand colours, including all tints and shades.
    The palette is marked as private to encourage access through `govuk-colour`
    and not to modify it.
    
    Tests check that the right variants are present for each colour,
    so we don't mistakenly remove a variant.
    
    Co-authored-by: domoscargin <domoscargin@users.noreply.github.com>
    Co-authored-by: Owen Jones <owen.jones@digital.cabinet-office.gov.uk>

--- packages/govuk-frontend/src/govuk/settings/colours.unit.test.js
+++ packages/govuk-frontend/src/govuk/settings/colours.unit.test.js
@@ -1,4 +1,66 @@
 const { compileSassString } = require('@govuk-frontend/helpers/tests')
+const { outdent } = require('outdent')
+
+const allVariants = [
+  'primary',
+  'tint-25',
+  'tint-50',
+  'tint-80',
+  'tint-95',
+  'shade-25',
+  'shade-50'
+]
+
+describe('Colour palette', () => {
+  it.each([
+    ['blue', allVariants],
+    ['green', allVariants],
+    ['teal', [...allVariants, 'accent']],
+    ['purple', allVariants],
+    ['magenta', allVariants],
+    ['red', allVariants],
+    ['orange', allVariants],
+    ['yellow', allVariants],
+    ['brown', ['primary', 'tint-25', 'tint-50', 'tint-95']],
+    ['black', ['primary', 'tint-25', 'tint-50', 'tint-80', 'tint-95']]
+  ])('Provides expected variants for `%s`', async (colour, variants) => {
+    const sass = `
+      @import "settings/colours-palette";
+
+      $variants: map-keys(map-get($_govuk-palette, "#{${colour}}"));
+
+      :root {
+        variants: $variants;
+      }
+    `
+
+    await expect(compileSassString(sass)).resolves.toMatchObject({
+      css: outdent`
+        :root {
+          variants: ${variants.map((v) => `"${v}"`).join(', ')};
+        }
+      `
+    })
+  })
+
+  it.each(['white'])('Provides a colour for `%s`', async (colour) => {
+    const sass = `
+      @import "settings/colours-palette";
+
+      :root {
+        type: type-of(map-get($_govuk-palette, "#{${colour}}"));
+      }
+    `
+
+    await expect(compileSassString(sass)).resolves.toMatchObject({
+      css: outdent`
+        :root {
+          type: color;
+        }
+      `
+    })
+  })
+})
 
 describe('Organisation colours', () => {
   it('should define contrast-safe colours that meet contrast requirements', async () => {
