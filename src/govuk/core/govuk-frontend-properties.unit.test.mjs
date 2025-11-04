d9c2c0ff7edde796368b0f1776377abb30375f35
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2025-11-04 16:06:32 +0000

    Fix custom properties being output for each import
    They should only be output once

--- src/govuk/core/govuk-frontend-properties.unit.test.mjs
+++ src/govuk/core/govuk-frontend-properties.unit.test.mjs
@@ -12,6 +12,21 @@ describe('GOV.UK Frontend custom properties', () => {
     await expect(css).toContain('--govuk-frontend-version: "development";')
   })
 
+  it('does not output custom properties more than once when included multiple times', async () => {
+    const sass = `
+      @import "base";
+      @import "core/govuk-frontend-properties";
+      @import "core/govuk-frontend-properties";
+    `
+
+    const { css } = await compileSassString(sass)
+    // Use the `--govuk-frontend-version` property rather than `:root`
+    // as `:root` may appear multiple times due to media queries in the future
+    const rootOccurrences = css.matchAll(/--govuk-frontend-version/g)
+
+    expect(Array.from(rootOccurrences)).toHaveLength(1)
+  })
+
   describe('breakpoints', () => {
     it('outputs one custom property for each breakpoint', async () => {
       const sass = `
