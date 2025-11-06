844e1b057d78a02783c575ca3daf3df7883b5a59
beeps <hi+github@berly.kim>
2025-11-06 14:12:37 +0000

    Remove header JavaScript and functional tests

--- src/govuk/init.jsdom.test.mjs
+++ src/govuk/init.jsdom.test.mjs
@@ -17,7 +17,6 @@ jest.mock(`./components/checkboxes/checkboxes.mjs`)
 jest.mock(`./components/error-summary/error-summary.mjs`)
 jest.mock(`./components/exit-this-page/exit-this-page.mjs`)
 jest.mock(`./components/file-upload/file-upload.mjs`)
-jest.mock(`./components/header/header.mjs`)
 jest.mock(`./components/notification-banner/notification-banner.mjs`)
 jest.mock(`./components/password-input/password-input.mjs`)
 jest.mock(`./components/radios/radios.mjs`)
@@ -28,7 +27,6 @@ jest.mock(`./components/tabs/tabs.mjs`)
 describe('initAll', () => {
   const components = [
     'checkboxes',
-    'header',
     'radios',
     'service-navigation',
     'skip-link',
