uk-gov-mirror/hmcts.govuk-frontend

Restore Sass variables for applied colours
Sets the output of `govuk-applied-colour` function
to the corresponding variables that were existing before
to smoothen the upgrade path.

packages/govuk-frontend/src/govuk/settings/colours.unit.test.js
@@ -113,6 +113,41 @@ describe('Applied colours', () => {
         ' success, border, input-border, hover, link, link-visited, link-hover, link-active)'
     )
   })
+
+  describe('legacy variables', () => {
+    describe.each([
+      'brand',
+      'text',
+      'template-background',
+      'body-background',
+      'print-text',
+      'secondary-text',
+      'focus',
+      'focus-text',
+      'error',
+      'success',
+      'border',
+      'input-border',
+      'link',
+      'link-visited',
+      'link-hover',
+      'link-active',
+    ])("$govuk-%s-colour", (appliedColourName) => {
+      it('sets a Sass variable with the applied colour value', async () => {
+        const sass = `
+          @import "settings/colours-applied";
+
+          :root {
+            result: $govuk-${appliedColourName}-colour == govuk-applied-colour(${appliedColourName});
+          }
+        `
+
+        const { css } = await compileSassString(sass)
+
+        expect(css).toContain(`result: true;`)
+      })
+    })
+  })
 })
 
 describe('Organisation colours', () => {

alphagov/govuk-frontend

packages/govuk-frontend/src/govuk/settings/colours.unit.test.js
@@ -139,6 +139,41 @@ describe('Functional colours', () => {
         ' success, border, input-border, hover, link, link-visited, link-hover, link-active)'
     )
   })
+
+  describe('legacy variables', () => {
+    describe.each([
+      'brand',
+      'text',
+      'template-background',
+      'body-background',
+      'print-text',
+      'secondary-text',
+      'focus',
+      'focus-text',
+      'error',
+      'success',
+      'border',
+      'input-border',
+      'link',
+      'link-visited',
+      'link-hover',
+      'link-active',
+    ])("$govuk-%s-colour", (functionalColourName) => {
+      it('sets a Sass variable with the functional colour value', async () => {
+        const sass = `
+          @import "settings/colours-functional";
+
+          :root {
+            result: $govuk-${functionalColourName}-colour == govuk-functional-colour(${functionalColourName});
+          }
+        `
+
+        const { css } = await compileSassString(sass)
+
+        expect(css).toContain(`result: true;`)
+      })
+    })
+  })
 })
 
 describe('Organisation colours', () => {