6e11118cb1d355f172112d6ace32178cb77cbe27
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-01-12 18:05:29 +0000

    Test for usage of CSS variables for part of declaration values
    Use an ad-hoc PostCSS plugin to check that CSS variables
    are only used as part of a `box-shadow` declaration, and not
    inside shorthand properties like `border` or `outline`.
    
    CSS variables used for the whole value are OK, but if used
    as part of a shorthand like `border` or `outline` and the custom property
    is missing or invalid, the browser will also discard the other parts
    of the shorthand (like the `border-width`), increasing the risk of broken
    layout.

--- src/govuk/components/components.unit.test.js
+++ src/govuk/components/components.unit.test.js
@@ -15,12 +15,51 @@ describe('Components', () => {
   })
 
   describe('Sass render', () => {
-    it('renders CSS for all components', () => {
-      const file = join(paths.package, 'src/govuk/components/_index.scss')
+    describe('all components', () => {
+      let sassCompilationResult
 
-      return expect(compileSassFile(file)).resolves.toMatchObject({
-        css: expect.any(String),
-        loadedUrls: expect.arrayContaining([expect.any(URL)])
+      beforeAll(async () => {
+        const source = join(paths.package, 'src/govuk/components/_index.scss')
+        sassCompilationResult = await compileSassFile(source)
+      })
+
+      it('renders CSS for all components', () => {
+        return expect(sassCompilationResult).toMatchObject({
+          css: expect.any(String),
+          loadedUrls: expect.arrayContaining([expect.any(URL)])
+        })
+      })
+
+      it.only('does not use custom properties in shorthand properties', async () => {
+        const { css } = sassCompilationResult
+
+        // Use a list of allowed properties rather than disallowed ones
+        // to catch new uses and explicitly allow them, rather than
+        // risk problematic future uses implicitly be accepted
+        const allowedProperties = ['box-shadow']
+
+        await require('postcss')({
+          postcssPlugin: 'govuk-frontend-var-in-shorthands',
+          Declaration(declaration) {
+            // Find out if the value includes a CSS variable that is not the entire value
+            // Test first for `var(` to avoid running regex unnecessarily
+            if (
+              declaration.value.includes('var(') &&
+              !declaration.value.match(/^var\(.*?\)$/)
+            ) {
+              // If it does, error unless we allow custom properties for that property
+              if (!allowedProperties.includes(declaration.prop)) {
+                throw new Error(
+                  `\`${declaration.prop}: ${declaration.value}\` includes a custom property as part of its value`
+                )
+              }
+            }
+          }
+        }).process(css, {
+          // Avoid PostCSS complaining about lacking a `from` option to locate a Browserslist file
+          // as we don't care about Browser support here, just parsing syntax
+          from: undefined
+        })
       })
     })
 
