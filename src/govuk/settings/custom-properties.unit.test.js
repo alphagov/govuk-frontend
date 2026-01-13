dd2b98cdb9cfd23d5876e0c732b4acee5142f57e
Owen Jones <owen.jones@digital.cabinet-office.gov.uk>
2026-01-13 11:56:55 +0000

    Add `govuk-output-custom-properties` setting
    Update packages/govuk-frontend/src/govuk/settings/_custom-properties.scss
    
    Co-authored-by: Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>

---
+++ src/govuk/settings/custom-properties.unit.test.js
@@ -0,0 +1,44 @@
+const { compileSassString } = require('@govuk-frontend/helpers/tests')
+
+describe('$govuk-output-custom-properties', () => {
+  it('outputs core custom properties if true', async () => {
+    const sass = `
+      $govuk-output-custom-properties: true;
+      @import "base";
+      @import "core/govuk-frontend-properties";
+    `
+
+    const { css } = await compileSassString(sass)
+
+    await expect(css).toContain('--govuk-')
+    await expect(css).toContain('--_govuk-')
+  })
+
+  it('does not output core custom properties if false', async () => {
+    const sass = `
+      $govuk-output-custom-properties: false;
+      @import "base";
+      @import "core/govuk-frontend-properties";
+    `
+
+    const { css } = await compileSassString(sass)
+
+    await expect(css).not.toContain('--govuk-breakpoint-')
+    await expect(css).not.toContain('--_govuk-')
+  })
+
+  it.each(['true', 'false'])(
+    'outputs --govuk-frontend-version if setting is `%s`',
+    async (state) => {
+      const sass = `
+      $govuk-output-custom-properties: ${state};
+      @import "base";
+      @import "core/govuk-frontend-properties";
+    `
+
+      let { css } = await compileSassString(sass)
+
+      await expect(css).toContain('--govuk-frontend-version')
+    }
+  )
+})
