5281f7a6332eae71ceefda52cd507c9d7a8b4d51
Owen Jones <owen.jones@digital.cabinet-office.gov.uk>
2026-01-13 16:31:36 +0000

    Add component testing for unknown custom properties
    Update packages/govuk-frontend/src/govuk/components/components.unit.test.js
    
    Co-Authored-By: Oliver Byford <121939+36degrees@users.noreply.github.com>

--- src/govuk/components/components.unit.test.js
+++ src/govuk/components/components.unit.test.js
@@ -3,17 +3,9 @@ const { join } = require('path')
 const { paths } = require('@govuk-frontend/config')
 const { compileSassFile } = require('@govuk-frontend/helpers/tests')
 const { getListing } = require('@govuk-frontend/lib/files')
+const stylelint = require('stylelint')
 
 describe('Components', () => {
-  let sassFiles
-
-  beforeAll(async () => {
-    sassFiles = await getListing('**/src/govuk/components/**/*.scss', {
-      cwd: paths.package,
-      ignore: ['**/_all.scss', '**/_index.scss']
-    })
-  })
-
   describe('Sass render', () => {
     describe('all components', () => {
       let sassCompilationResult
@@ -61,19 +53,78 @@ describe('Components', () => {
           from: undefined
         })
       })
+
+      it('does not reference any undefined custom properties', async () => {
+        const linter = await stylelint.lint({
+          config: { rules: { 'no-unknown-custom-properties': true } },
+          code: sassCompilationResult.css
+        })
+
+        // Output stylelint warnings to make debugging easier
+        if (linter.results[0].warnings.length) {
+          console.log(
+            'Warnings were present when testing all components for unknown custom properties:'
+          )
+          console.log(linter.results[0].warnings)
+        }
+
+        return expect(linter.results[0].warnings).toHaveLength(0)
+      })
     })
 
-    it('renders CSS for each component', () => {
-      const sassTasks = sassFiles.map((sassFilePath) => {
-        const file = join(paths.package, sassFilePath)
+    describe('Individual components', () => {
+      let compiledSassFiles
 
-        return expect(compileSassFile(file)).resolves.toMatchObject({
-          css: expect.any(String),
-          loadedUrls: expect.arrayContaining([expect.any(URL)])
-        })
+      beforeAll(async () => {
+        const sassFiles = await getListing(
+          '**/src/govuk/components/**/*.scss',
+          {
+            cwd: paths.package,
+            ignore: ['**/_all.scss', '**/_index.scss']
+          }
+        )
+
+        compiledSassFiles = await Promise.all(
+          sassFiles.map(async (sassFilePath) => {
+            const file = join(paths.package, sassFilePath)
+
+            return {
+              file,
+              compiledSass: await compileSassFile(file)
+            }
+          })
+        )
       })
 
-      return Promise.all(sassTasks)
+      it('renders CSS for each component', async () => {
+        return compiledSassFiles.map((compiledSassFile) =>
+          expect(compiledSassFile.compiledSass).toMatchObject({
+            css: expect.any(String),
+            loadedUrls: expect.arrayContaining([expect.any(URL)])
+          })
+        )
+      })
+
+      it('does not render any empty custom properties per component', async () => {
+        const sassTasks = compiledSassFiles.map(async (compiledSassFile) => {
+          const linter = await stylelint.lint({
+            config: { rules: { 'no-unknown-custom-properties': true } },
+            code: compiledSassFile.compiledSass.css
+          })
+
+          // Output stylelint warnings to make debugging easier
+          if (linter.results[0].warnings.length) {
+            console.log(
+              `Warnings were present when testing ${compiledSassFile.file} for unknown custom properties:`
+            )
+            console.log(linter.results[0].warnings)
+          }
+
+          return expect(linter.results[0].warnings).toHaveLength(0)
+        })
+
+        return Promise.all(sassTasks)
+      })
     })
   })
 })
