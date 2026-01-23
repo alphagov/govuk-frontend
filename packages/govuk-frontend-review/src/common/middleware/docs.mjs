4e99e3fedf3d149d3b4f9c439eb3369a28fddd2a
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-01-23 18:28:35 +0000

    Remove redirect to Frontend docs when accessing Sass docs
    There's already a link to explicitely access the Frontend docs
    and having that redirect prevents from previewing the Sass docs
    changes from a PR

--- packages/govuk-frontend-review/src/common/middleware/docs.mjs
+++ packages/govuk-frontend-review/src/common/middleware/docs.mjs
@@ -13,22 +13,6 @@ router.get('/', (req, res) => {
 })
 
 /**
- * Sass docs latest release (when deployed)
- */
-router.use('/sass', ({ app }, res, next) => {
-  const { isDevelopment } =
-    /** @type {import('../../app.mjs').FeatureFlags} */ (app.get('flags'))
-
-  if (!isDevelopment) {
-    return res.redirect(
-      'https://frontend.design-system.service.gov.uk/sass-api-reference/'
-    )
-  }
-
-  next()
-})
-
-/**
  * Add middleware
  */
 router.use('/sass', express.static(join(paths.app, 'dist/docs/sassdoc')))
