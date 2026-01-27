61615431420709febc16eae76fea7d24af00ff71
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-01-27 14:34:07 +0000

    Move `settings` custom properties test into the `custom-properties` layer
    The code validate by those tests is inside the `custom-properties` layer
    (ie. the reaction to the Sass variable value) so the tests are brought
    closer to the code they validate.

--- packages/govuk-frontend/src/govuk/custom-properties/breakpoints.unit.test.mjs
+++ packages/govuk-frontend/src/govuk/custom-properties/breakpoints.unit.test.mjs
@@ -32,4 +32,28 @@ describe('custom-properties/breakpoints', () => {
 
     expect(Array.from(occurrences)).toHaveLength(1)
   })
+
+  describe('$govuk-output-custom-properties', () => {
+    it('outputs the properties properties if `true`', async () => {
+      const sass = `
+        $govuk-output-custom-properties: true;
+        @import "custom-properties/breakpoints";
+      `
+
+      const { css } = await compileSassString(sass)
+
+      await expect(css).toContain('--govuk-breakpoint-mobile')
+    })
+
+    it('does not output core custom properties if `false`', async () => {
+      const sass = `
+        $govuk-output-custom-properties: false;
+        @import "custom-properties/breakpoints";
+      `
+
+      const { css } = await compileSassString(sass)
+
+      await expect(css).not.toContain('--govuk-breakpoint-mobile')
+    })
+  })
 })
