c3870e4abf79972ab072e82835023dfbb2f1a10c
Brett Kyle <22524634+domoscargin@users.noreply.github.com>
2026-02-02 15:10:22 +0000

    Special case label 'for' test
    html-validate's 'valid-for' rule requires specific elements (like <input>).
    
    We're using a <div> for our dummy elements, so we need to use an <input> specifically for this test.
    
    We should investigate a better, more integrated approach to dummy content in the future

--- src/govuk/components/label/template.test.js
+++ src/govuk/components/label/template.test.js
@@ -58,7 +58,7 @@ describe('Label', () => {
       const $ = render('label', examples.for)
 
       const labelForAttr = $('.govuk-label').attr('for')
-      expect(labelForAttr).toBe('test-target-element')
+      expect(labelForAttr).toBe('test-target-input')
     })
 
     it('can be nested inside an H1 using isPageHeading', () => {
