f9fb1e869907e4d3f8ee55a602fad445f2f46130
Brett Kyle <22524634+domoscargin@users.noreply.github.com>
2026-02-11 15:59:40 +0000

    Remove slash-div from silenced deprecations

--- shared/config/sass.js
+++ shared/config/sass.js
@@ -2,7 +2,7 @@
  * @type {import('sass-embedded').Options<"async">}
  */
 const deprecationOptions = {
-  silenceDeprecations: ['global-builtin', 'import', 'mixed-decls', 'slash-div']
+  silenceDeprecations: ['global-builtin', 'import', 'mixed-decls']
 }
 
 module.exports = {
