5c054e56730703e49c01fec846379d12c81379de
Brett Kyle <22524634+domoscargin@users.noreply.github.com>
2026-02-26 14:37:38 +0000

    Add check for unevaluated Sass functions
    We're reusing a test that we already have that checks for functions that begin govuk or _govuk. We do have a couple of non-prefixed functions that I've added into the test manually.
    
    I did do a search through our sass files to find @function definitions and create a list, but that seems a bit extreme.

--- tests/sass-tests/sass-file-compilation.integration.test.mjs
+++ tests/sass-tests/sass-file-compilation.integration.test.mjs
@@ -7,6 +7,9 @@ import slash from 'slash'
 
 import { sassConfig } from './sass.config.js'
 
+/**
+ * Setup
+ */
 const govukFrontendPath = packageNameToPath('govuk-frontend')
 
 // Grab a list of all Sass files and sort them alphabetically, for consistent output
@@ -28,6 +31,9 @@ async function compileSassFile(sassFilePath) {
     .catch((error) => ({ css: '', error }))
 }
 
+/**
+ * Test suite
+ */
 describe('Sass file compilation', () => {
   beforeAll(async () => {
     await Promise.all(
@@ -42,4 +48,27 @@ describe('Sass file compilation', () => {
 
     expect(error).toBeNull()
   })
+
+  // See packages/govuk-frontend/src/govuk/index.unit.test.mjs for details of
+  // this test. We've copied it here and tweaked it to include functions that
+  // aren't prefixed with govuk or _govuk.
+  it.each(sassFiles)(
+    '%s does not contain any unexpected govuk- function calls',
+    async (sassFilePath) => {
+      // Most of our functions are prefixed with _govuk or govuk,
+      // but there are some exceptions, which we'll call out here.
+      const nonGOVUKFunctions = ['_should-warn', '_warning-text']
+
+      const { css } = compiledFiles.get(sassFilePath)
+      const functionNames = ['_?govuk-[\\w-]+', ...nonGOVUKFunctions].join('|')
+
+      const matches = css.matchAll(
+        new RegExp(`(?:${functionNames})\\(.*?\\)`, 'g')
+      )
+
+      // `matchAll` does not return an actual `Array` so we need
+      // a little conversion before we can check its length
+      expect(Array.from(matches)).toHaveLength(0)
+    }
+  )
 })
