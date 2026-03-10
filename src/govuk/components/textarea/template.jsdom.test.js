8d8b8f065cdcfdd86cb20c62a7d9a45c6df29d96
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2026-03-10 15:48:56 +0000

    Update textarea template tests to use jsdom

--- src/govuk/components/textarea/template.jsdom.test.js
+++ src/govuk/components/textarea/template.jsdom.test.js
@@ -1,6 +1,4 @@
-const { render } = require('@govuk-frontend/helpers/nunjucks')
-const { htmlWithClassName } = require('@govuk-frontend/helpers/tests')
-const { getExamples } = require('@govuk-frontend/lib/components')
+const { getExamples, render } = require('@govuk-frontend/lib/components')
 
 const WORD_BOUNDARY = '\\b'
 const WHITESPACE = '\\s'
@@ -14,263 +12,314 @@ describe('Textarea', () => {
 
   describe('default example', () => {
     it('autopopulates default id from name', () => {
-      const $ = render('textarea', examples.default)
+      document.body.innerHTML = render('textarea', examples.default)
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('id')).toBe($component.attr('name'))
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveAttribute('id', $component.getAttribute('name'))
     })
 
     it('renders with name', () => {
-      const $ = render('textarea', examples.default)
+      document.body.innerHTML = render('textarea', examples.default)
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('name')).toBe('more-detail')
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveAttribute('name', 'more-detail')
     })
 
     it('renders with default number of rows', () => {
-      const $ = render('textarea', examples.default)
+      document.body.innerHTML = render('textarea', examples.default)
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('rows')).toBe('5')
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveAttribute('rows', '5')
     })
 
     it('renders with a form group wrapper', () => {
-      const $ = render('textarea', examples.default)
+      document.body.innerHTML = render('textarea', examples.default)
 
-      const $formGroup = $('.govuk-form-group')
-      expect($formGroup.length).toBeTruthy()
+      const $formGroup = document.querySelector('.govuk-form-group')
+      expect($formGroup).toBeInTheDocument()
     })
   })
 
   describe('custom options', () => {
     it('renders with id', () => {
-      const $ = render('textarea', examples.id)
+      document.body.innerHTML = render('textarea', examples.id)
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('id')).not.toBe($component.attr('name'))
-      expect($component.attr('id')).toBe('textarea-id')
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveAttribute('id', 'textarea-id')
     })
 
     it('renders with classes', () => {
-      const $ = render('textarea', examples.classes)
+      document.body.innerHTML = render('textarea', examples.classes)
 
-      const $component = $('.govuk-textarea')
-      expect($component.hasClass('app-textarea--custom-modifier')).toBeTruthy()
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveClass('app-textarea--custom-modifier')
     })
 
     it('renders with value', () => {
-      const $ = render('textarea', examples['with default value'])
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with default value']
+      )
 
-      const $component = $('.govuk-textarea')
-      expect($component.text()).toBe('221B Baker Street\nLondon\nNW1 6XE\n')
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveValue('221B Baker Street\nLondon\nNW1 6XE\n')
     })
 
     it('renders with attributes', () => {
-      const $ = render('textarea', examples.attributes)
+      document.body.innerHTML = render('textarea', examples.attributes)
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('data-attribute')).toBe('my data value')
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveAttribute('data-attribute', 'my data value')
     })
 
     it('renders with aria-describedby', () => {
-      const $ = render('textarea', examples['with describedBy'])
+      document.body.innerHTML = render('textarea', examples['with describedBy'])
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('aria-describedby')).toMatch('test-target-element')
+      const $component = document.querySelector('.govuk-textarea')
+
+      // Because aria-describedby references test-target-element which has no
+      // content, expect on the attribute instead of toHaveAccessibleDescription
+      expect($component).toHaveAttribute(
+        'aria-describedby',
+        'test-target-element'
+      )
     })
 
     it('renders with rows', () => {
-      const $ = render('textarea', examples['with custom rows'])
+      document.body.innerHTML = render('textarea', examples['with custom rows'])
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('rows')).toBe('8')
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveAttribute('rows', '8')
     })
 
     it('renders with a form group wrapper that has extra classes', () => {
-      const $ = render('textarea', examples['with optional form-group classes'])
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with optional form-group classes']
+      )
 
-      const $formGroup = $('.govuk-form-group')
-      expect($formGroup.hasClass('extra-class')).toBeTruthy()
+      const $formGroup = document.querySelector('.govuk-form-group')
+      expect($formGroup).toHaveClass('extra-class')
     })
   })
 
   describe('when it has the spellcheck attribute', () => {
     it('renders with spellcheck attribute set to true', () => {
-      const $ = render('textarea', examples['with spellcheck enabled'])
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with spellcheck enabled']
+      )
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('spellcheck')).toBe('true')
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveAttribute('spellcheck', 'true')
     })
 
     it('renders with spellcheck attribute set to false', () => {
-      const $ = render('textarea', examples['with spellcheck disabled'])
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with spellcheck disabled']
+      )
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('spellcheck')).toBe('false')
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveAttribute('spellcheck', 'false')
     })
 
     it('renders without spellcheck attribute by default', () => {
-      const $ = render('textarea', examples.default)
+      document.body.innerHTML = render('textarea', examples.default)
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('spellcheck')).toBeUndefined()
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).not.toHaveAttribute('spellcheck')
     })
   })
 
   describe('when it includes a hint', () => {
     it('renders with hint', () => {
-      const $ = render('textarea', examples['with hint'])
+      document.body.innerHTML = render('textarea', examples['with hint'])
 
-      expect(htmlWithClassName($, '.govuk-hint')).toMatchSnapshot()
+      expect(document.querySelector('.govuk-hint').outerHTML).toMatchSnapshot()
     })
 
     it('associates the textarea as "described by" the hint', () => {
-      const $ = render('textarea', examples['with hint'])
+      document.body.innerHTML = render('textarea', examples['with hint'])
 
-      const $textarea = $('.govuk-textarea')
-      const hintId = $('.govuk-hint').attr('id')
+      const $textarea = document.querySelector('.govuk-textarea')
 
-      const describedBy = new RegExp(
-        `${WORD_BOUNDARY}${hintId}${WORD_BOUNDARY}`
+      expect($textarea).toHaveAccessibleDescription(
+        "Don't include personal or financial information, eg your National " +
+          'Insurance number or credit card details.'
       )
-
-      expect($textarea.attr('aria-describedby')).toMatch(describedBy)
     })
 
     it('associates the textarea as "described by" the hint and parent fieldset', () => {
-      const $ = render('textarea', examples['with hint and described by'])
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with hint and described by']
+      )
 
-      const $textarea = $('.govuk-textarea')
-      const hintId = $('.govuk-hint').attr('id')
+      const $textarea = document.querySelector('.govuk-textarea')
+      const hintId = document.querySelector('.govuk-hint').id
 
       const describedBy = new RegExp(
         `${WORD_BOUNDARY}test-target-element${WHITESPACE}${hintId}${WORD_BOUNDARY}`
       )
 
-      expect($textarea.attr('aria-describedby')).toMatch(describedBy)
+      // Because aria-describedby references test-target-element which has no
+      // content, expect on the attribute instead of toHaveAccessibleDescription
+      expect($textarea).toHaveAttribute(
+        'aria-describedby',
+        expect.stringMatching(describedBy)
+      )
     })
   })
 
   describe('when it includes an error message', () => {
     it('renders with error message', () => {
-      const $ = render('textarea', examples['with error message'])
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with error message']
+      )
 
-      expect(htmlWithClassName($, '.govuk-error-message')).toMatchSnapshot()
+      expect(
+        document.querySelector('.govuk-error-message').outerHTML
+      ).toMatchSnapshot()
     })
 
     it('associates the textarea as "described by" the error message', () => {
-      const $ = render('textarea', examples['with error message'])
-
-      const $component = $('.govuk-textarea')
-      const errorMessageId = $('.govuk-error-message').attr('id')
-
-      const describedBy = new RegExp(
-        `${WORD_BOUNDARY}${errorMessageId}${WORD_BOUNDARY}`
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with error message']
       )
 
-      expect($component.attr('aria-describedby')).toMatch(describedBy)
+      const $component = document.querySelector('.govuk-textarea')
+
+      expect($component).toHaveAccessibleDescription(
+        'Error: You must provide an explanation'
+      )
     })
 
     it('associates the textarea as "described by" the error message and parent fieldset', () => {
-      const $ = render(
+      document.body.innerHTML = render(
         'textarea',
         examples['with error message and described by']
       )
 
-      const $component = $('.govuk-textarea')
-      const errorMessageId = $('.govuk-error-message').attr('id')
+      const $component = document.querySelector('.govuk-textarea')
+      const errorMessageId = document.querySelector('.govuk-error-message').id
 
       const describedBy = new RegExp(
         `${WORD_BOUNDARY}test-target-element${WHITESPACE}${errorMessageId}${WORD_BOUNDARY}`
       )
 
-      expect($component.attr('aria-describedby')).toMatch(describedBy)
+      // Because aria-describedby references test-target-element which has no
+      // content, expect on the attribute instead of toHaveAccessibleDescription
+      expect($component).toHaveAttribute(
+        'aria-describedby',
+        expect.stringMatching(describedBy)
+      )
     })
 
     it('adds the error class to the textarea', () => {
-      const $ = render('textarea', examples['with error message'])
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with error message']
+      )
 
-      const $component = $('.govuk-textarea')
-      expect($component.hasClass('govuk-textarea--error')).toBeTruthy()
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveClass('govuk-textarea--error')
     })
 
     it('renders with a form group wrapper that has an error state', () => {
-      const $ = render('textarea', examples['with error message'])
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with error message']
+      )
 
-      const $formGroup = $('.govuk-form-group')
-      expect($formGroup.hasClass('govuk-form-group--error')).toBeTruthy()
+      const $formGroup = document.querySelector('.govuk-form-group')
+      expect($formGroup).toHaveClass('govuk-form-group--error')
     })
   })
 
   describe('when it includes both a hint and an error message', () => {
     it('associates the textarea as described by both the hint and the error message', () => {
-      const $ = render('textarea', examples['with hint and error message'])
-
-      const $component = $('.govuk-textarea')
-      const errorMessageId = $('.govuk-error-message').attr('id')
-      const hintId = $('.govuk-hint').attr('id')
-
-      const describedByCombined = new RegExp(
-        `${WORD_BOUNDARY}${hintId}${WHITESPACE}${errorMessageId}${WORD_BOUNDARY}`
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with hint and error message']
       )
 
-      expect($component.attr('aria-describedby')).toMatch(describedByCombined)
+      const $component = document.querySelector('.govuk-textarea')
+
+      expect($component).toHaveAccessibleDescription(
+        'Hint Error: Error message'
+      )
     })
 
     it('associates the textarea as described by the hint, error message and parent fieldset', () => {
-      const $ = render(
+      document.body.innerHTML = render(
         'textarea',
         examples['with hint, error message and described by']
       )
 
-      const $component = $('.govuk-textarea')
-      const errorMessageId = $('.govuk-error-message').attr('id')
-      const hintId = $('.govuk-hint').attr('id')
+      const $component = document.querySelector('.govuk-textarea')
+      const errorMessageId = document.querySelector('.govuk-error-message').id
+      const hintId = document.querySelector('.govuk-hint').id
 
       const describedByCombined = new RegExp(
         `${WORD_BOUNDARY}test-target-element${WHITESPACE}${hintId}${WHITESPACE}${errorMessageId}${WORD_BOUNDARY}`
       )
 
-      expect($component.attr('aria-describedby')).toMatch(describedByCombined)
+      // Because aria-describedby references test-target-element which has no
+      // content, expect on the attribute instead of toHaveAccessibleDescription
+      expect($component).toHaveAttribute(
+        'aria-describedby',
+        expect.stringMatching(describedByCombined)
+      )
     })
   })
 
   describe('with dependant components', () => {
     it('have correct nesting order', () => {
-      const $ = render('textarea', examples.default)
+      document.body.innerHTML = render('textarea', examples.default)
 
-      const $component = $('.govuk-form-group > .govuk-textarea')
-      expect($component.length).toBeTruthy()
+      const $component = document.querySelector(
+        '.govuk-form-group > .govuk-textarea'
+      )
+      expect($component).toBeInTheDocument()
     })
 
     it('renders with label', () => {
-      const $ = render('textarea', examples.default)
+      document.body.innerHTML = render('textarea', examples.default)
 
-      expect(htmlWithClassName($, '.govuk-label')).toMatchSnapshot()
+      expect(document.querySelector('.govuk-label').outerHTML).toMatchSnapshot()
     })
 
     it('renders label with "for" attribute reffering the textarea "id"', () => {
-      const $ = render('textarea', examples.default)
+      document.body.innerHTML = render('textarea', examples.default)
 
-      const $label = $('.govuk-label')
-      expect($label.attr('for')).toBe('more-detail')
+      const $label = document.querySelector('.govuk-label')
+      expect($label).toHaveAttribute('for', 'more-detail')
     })
 
     it('renders label as page heading', () => {
-      const $ = render('textarea', examples['with label as page heading'])
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with label as page heading']
+      )
 
-      const $label = $('.govuk-label')
-      expect($('.govuk-label-wrapper')).toBeTruthy()
-      expect($label.attr('for')).toBe('textarea-with-page-heading')
+      const $label = document.querySelector('.govuk-label')
+      expect(document.querySelector('.govuk-label-wrapper')).toBeInTheDocument()
+      expect($label).toHaveAttribute('for', 'textarea-with-page-heading')
     })
   })
 
   describe('when it includes an autocomplete attribute', () => {
     it('renders the autocomplete attribute', () => {
-      const $ = render('textarea', examples['with autocomplete attribute'])
+      document.body.innerHTML = render(
+        'textarea',
+        examples['with autocomplete attribute']
+      )
 
-      const $component = $('.govuk-textarea')
-      expect($component.attr('autocomplete')).toBe('street-address')
+      const $component = document.querySelector('.govuk-textarea')
+      expect($component).toHaveAttribute('autocomplete', 'street-address')
     })
   })
 })
