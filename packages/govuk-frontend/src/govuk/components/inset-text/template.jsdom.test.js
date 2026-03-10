9182c54479b9ebe71ea31dc707ebb204afe29902
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2026-03-10 16:16:43 +0000

    Update inset text template tests to use jsdom

--- packages/govuk-frontend/src/govuk/components/inset-text/template.jsdom.test.js
+++ packages/govuk-frontend/src/govuk/components/inset-text/template.jsdom.test.js
@@ -1,7 +1,4 @@
-const { render } = require('@govuk-frontend/helpers/nunjucks')
-const { getExamples } = require('@govuk-frontend/lib/components')
-const { indent } = require('nunjucks/src/filters')
-const { outdent } = require('outdent')
+const { getExamples, render } = require('@govuk-frontend/lib/components')
 
 describe('Inset text', () => {
   let examples
@@ -12,69 +9,65 @@ describe('Inset text', () => {
 
   describe('by default', () => {
     it('renders with classes', () => {
-      const $ = render('inset-text', examples.classes)
+      document.body.innerHTML = render('inset-text', examples.classes)
 
-      const $component = $('.govuk-inset-text')
-      expect(
-        $component.hasClass('app-inset-text--custom-modifier')
-      ).toBeTruthy()
+      const $component = document.querySelector('.govuk-inset-text')
+      expect($component).toHaveClass('app-inset-text--custom-modifier')
     })
 
     it('renders with id', () => {
-      const $ = render('inset-text', examples.id)
+      document.body.innerHTML = render('inset-text', examples.id)
 
-      const $component = $('.govuk-inset-text')
-      expect($component.attr('id')).toBe('my-inset-text')
+      const $component = document.querySelector('.govuk-inset-text')
+      expect($component.id).toBe('my-inset-text')
     })
 
     it('renders nested components using `call`', () => {
-      const $ = render('inset-text', {
+      document.body.innerHTML = render('inset-text', {
         callBlock: '<div class="app-nested-component"></div>'
       })
 
-      expect($('.govuk-inset-text .app-nested-component').length).toBeTruthy()
+      expect(
+        document.querySelector('.govuk-inset-text .app-nested-component')
+      ).toBeInTheDocument()
     })
 
     it('allows text to be passed whilst escaping HTML entities', () => {
-      const $ = render('inset-text', examples['html as text'])
+      document.body.innerHTML = render('inset-text', examples['html as text'])
 
-      const content = $('.govuk-inset-text').html().trim()
-      expect(content).toBe(
-        'It can take &lt;b&gt;up to 8 weeks&lt;/b&gt; to register a lasting power of attorney if there are no mistakes in the application.'
+      const $component = document.querySelector('.govuk-inset-text')
+      expect($component).toHaveTextContent(
+        'It can take <b>up to 8 weeks</b> to register a lasting power of ' +
+          'attorney if there are no mistakes in the application.'
       )
     })
 
     it('allows HTML to be passed un-escaped', () => {
-      const $ = render('inset-text', examples['with html'])
+      document.body.innerHTML = render('inset-text', examples['with html'])
 
-      const mainContent = $('.govuk-inset-text .govuk-body:first-child')
-        .text()
-        .trim()
-
-      const warningContent = $('.govuk-inset-text .govuk-warning-text__text')
-        .text()
-        .trim()
-
-      expect(mainContent).toBe(
-        'It can take up to 8 weeks to register a lasting power of attorney if there are no mistakes in the application.'
+      const mainContent = document.querySelector(
+        '.govuk-inset-text .govuk-body:first-child'
       )
 
-      expect(warningContent).toEqual(
-        indent(
-          outdent`
-            Warning
-            You can be fined up to £5,000 if you don’t register.
-          `,
-          6
-        )
+      const warningContent = document.querySelector(
+        '.govuk-inset-text .govuk-warning-text__text'
+      )
+
+      expect(mainContent).toHaveTextContent(
+        'It can take up to 8 weeks to register a lasting power of attorney if ' +
+          'there are no mistakes in the application.'
+      )
+
+      expect(warningContent).toHaveTextContent(
+        'Warning You can be fined up to £5,000 if you don’t register.'
       )
     })
 
     it('renders with attributes', () => {
-      const $ = render('inset-text', examples.attributes)
+      document.body.innerHTML = render('inset-text', examples.attributes)
 
-      const $component = $('.govuk-inset-text')
-      expect($component.attr('data-attribute')).toBe('my data value')
+      const $component = document.querySelector('.govuk-inset-text')
+      expect($component).toHaveAttribute('data-attribute', 'my data value')
     })
   })
 })
