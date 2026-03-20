33572ee9b14722dd820c299f756ee49b6611d8b3
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-03-20 22:55:49 +0000

    Add deprecation warnings when setting asset url functions as string
    While it still works with `@import`, when `@use`ing GOV.UK Frontend, users
    need to pass a Sass function acquired with `meta.get-function`.

--- src/govuk/tools/image-url.unit.test.js
+++ src/govuk/tools/image-url.unit.test.js
@@ -1,7 +1,23 @@
 const { deprecationOptions } = require('@govuk-frontend/config/sass')
 const { compileSassString } = require('@govuk-frontend/helpers/tests')
 const { outdent } = require('outdent')
-const { NodePackageImporter } = require('sass-embedded')
+const { NodePackageImporter, sassNull } = require('sass-embedded')
+
+// Create a mock warn function that we can use to override the native @warn
+// function, that we can make assertions about post-render.
+const mockWarnFunction = jest.fn().mockReturnValue(sassNull)
+
+// Until we only support Sass modules, users can pass the name of a native Sass function
+// so we need to remove `global-builtin` from the fatalDeprecations in some tests
+const fatalDeprecations = deprecationOptions.fatalDeprecations.filter(
+  (deprecationId) => deprecationId !== 'global-builtin'
+)
+
+const sassConfig = {
+  logger: {
+    warn: mockWarnFunction
+  }
+}
 
 describe('@function image-url', () => {
   it('by default concatenates the image path and the filename', async () => {
@@ -26,12 +42,13 @@ describe('@function image-url', () => {
   })
 
   describe('$govuk-image-url-function', () => {
-    describe('as a string (@import only)', () => {
+    describe('as a string', () => {
       it('executes a native Sass function', async () => {
         const sass = `
-          @import "tools/image-url";
-
-          $govuk-image-url-function: 'to-upper-case';
+          @use "settings" with (
+            $govuk-image-url-function: 'to-upper-case'
+          );
+          @use "tools/image-url" as *;
 
           .foo {
             background-image: govuk-image-url("baz.png");
@@ -55,6 +72,53 @@ describe('@function image-url', () => {
         })
       })
 
+      it('uses the default for a non-native function', async () => {
+        const sass = `
+          @use "settings" with (
+            $govuk-images-path: '/path/to/images/',
+            $govuk-image-url-function: non-native-function
+          );
+          @use "tools/image-url" as *;
+
+          .foo {
+            background-image: govuk-image-url("baz.png");
+          }
+        `
+
+        await expect(compileSassString(sass)).resolves.toMatchObject({
+          css: outdent`
+            .foo {
+              background-image: url("/path/to/images/baz.png");
+            }
+          `
+        })
+      })
+
+      it('emits deprecation message', async () => {
+        const sass = `
+          @use "settings" with (
+            $govuk-image-url-function: 'a-function'
+          );
+          @use "tools/image-url" as *;
+
+          .foo {
+            background-image: govuk-image-url("baz.png");
+          }
+        `
+
+        await compileSassString(sass, { ...sassConfig, fatalDeprecations })
+
+        expect(mockWarnFunction).toHaveBeenCalledWith(
+          'Passing a string to $govuk-image-url-function is deprecated. ' +
+            'Pass a function using meta.get-function(...) instead. ' +
+            'To silence this warning, update $govuk-suppressed-warnings ' +
+            'with key: "image-url-string"',
+          expect.anything()
+        )
+      })
+    })
+
+    describe('with `@import`', () => {
       it('executes a custom function', async () => {
         const sass = `
           $govuk-image-url-function: 'images-url';
@@ -97,6 +161,28 @@ describe('@function image-url', () => {
           }
         `
         })
+      })
+
+      it('emits deprecation message when using `@import`', async () => {
+        const sass = `
+          $govuk-image-url-function: 'to-upper-case';
+          @import "tools/image-url";
+
+          @font-face {
+            font-family: "whatever";
+            src: govuk-image-url("whatever.woff2");
+          }
+        `
+
+        await compileSassString(sass, { ...sassConfig, fatalDeprecations })
+
+        expect(mockWarnFunction).toHaveBeenCalledWith(
+          'Passing a string to $govuk-image-url-function is deprecated. ' +
+            'Pass a function using meta.get-function(...) instead. ' +
+            'To silence this warning, update $govuk-suppressed-warnings ' +
+            'with key: "image-url-string"',
+          expect.anything()
+        )
       })
     })
 
