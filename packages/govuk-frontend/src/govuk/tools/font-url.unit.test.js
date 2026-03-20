d75f716fccdbcff2a0678ec38fdab93766d952af
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-03-20 22:55:49 +0000

    Add deprecation warnings when setting asset url functions as string
    While it still works with `@import`, when `@use`ing GOV.UK Frontend, users
    need to pass a Sass function acquired with `meta.get-function`.

--- packages/govuk-frontend/src/govuk/tools/font-url.unit.test.js
+++ packages/govuk-frontend/src/govuk/tools/font-url.unit.test.js
@@ -2,6 +2,23 @@ const { deprecationOptions } = require('@govuk-frontend/config/sass')
 const { compileSassString } = require('@govuk-frontend/helpers/tests')
 const { outdent } = require('outdent')
 const { NodePackageImporter } = require('sass-embedded')
+const { sassNull } = require('sass-embedded')
+
+// Until we only support Sass modules, users can pass the name of a native Sass function
+// so we'll remove `global-builtin` from the fatalDeprecations temprarily
+const fatalDeprecations = deprecationOptions.fatalDeprecations.filter(
+  (deprecationId) => deprecationId !== 'global-builtin'
+)
+
+// Create a mock warn function that we can use to override the native @warn
+// function, that we can make assertions about post-render.
+const mockWarnFunction = jest.fn().mockReturnValue(sassNull)
+
+const sassConfig = {
+  logger: {
+    warn: mockWarnFunction
+  }
+}
 
 describe('@function font-url', () => {
   it('by default concatenates the font path and the filename', async () => {
@@ -28,25 +45,19 @@ describe('@function font-url', () => {
   })
 
   describe('$govuk-font-url-function', () => {
-    describe('as a string (@import only)', () => {
+    describe('as a string', () => {
       it('executes a native Sass function', async () => {
         const sass = `
-          @import "tools/font-url";
-
-          $govuk-fonts-path: '/path/to/fonts/';
-          $govuk-font-url-function: 'to-upper-case';
+          @use "settings" with (
+            $govuk-font-url-function: 'to-upper-case'
+          );
+          @use "tools/font-url" as *;
 
           @font-face {
             font-family: "whatever";
             src: govuk-font-url("whatever.woff2");
           }
         `
-
-        // Until we only support Sass modules, users can pass the name of a native Sass function
-        // so we'll remove `global-builtin` from the fatalDeprecations temprarily
-        const fatalDeprecations = deprecationOptions.fatalDeprecations.filter(
-          (deprecationId) => deprecationId !== 'global-builtin'
-        )
 
         await expect(
           compileSassString(sass, { fatalDeprecations })
@@ -60,37 +71,13 @@ describe('@function font-url', () => {
         })
       })
 
-      it('can be overridden to use a custom function', async () => {
+      it('uses the default for a non-native function', async () => {
         const sass = `
-          $govuk-font-url-function: 'fonts-url';
-
-          @import "pkg:@govuk-frontend/helpers/assets-urls";
-          @import "tools/font-url";
-
-          @font-face {
-            font-family: "whatever";
-            src: govuk-font-url("whatever.woff2");
-          }
-        `
-
-        await expect(
-          compileSassString(sass, { importers: [new NodePackageImporter()] })
-        ).resolves.toMatchObject({
-          css: outdent`
-            @font-face {
-              font-family: "whatever";
-              src: url("example.woff");
-            }
-          `
-        })
-      })
-
-      it('uses the default if the function does not exist', async () => {
-        const sass = `
-          @import "tools/font-url";
-
-          $govuk-fonts-path: '/path/to/fonts/';
-          $govuk-font-url-function: unknown-function;
+          @use "settings" with (
+            $govuk-fonts-path: '/path/to/fonts/',
+            $govuk-font-url-function: non-native-function
+          );
+          @use "tools/font-url" as *;
 
           @font-face {
             font-family: "whatever";
@@ -105,6 +92,102 @@ describe('@function font-url', () => {
               src: url("/path/to/fonts/whatever.woff2");
             }
           `
+        })
+      })
+
+      it('emits deprecation message', async () => {
+        const sass = `
+          @use "settings" with (
+            $govuk-font-url-function: 'a-function'
+          );
+          @use "tools/font-url" as *;
+
+          @font-face {
+            font-family: "whatever";
+            src: govuk-font-url("whatever.woff2");
+          }
+        `
+
+        await compileSassString(sass, { ...sassConfig })
+
+        expect(mockWarnFunction).toHaveBeenCalledWith(
+          'Passing a string to $govuk-font-url-function is deprecated. ' +
+            'Pass a function using meta.get-function(...) instead. ' +
+            'To silence this warning, update $govuk-suppressed-warnings ' +
+            'with key: "font-url-string"',
+          expect.anything()
+        )
+      })
+
+      describe('with `@import`', () => {
+        it('can be overridden to use a custom function', async () => {
+          const sass = `
+            $govuk-font-url-function: 'fonts-url';
+
+            @import "pkg:@govuk-frontend/helpers/assets-urls";
+            @import "tools/font-url";
+
+            @font-face {
+              font-family: "whatever";
+              src: govuk-font-url("whatever.woff2");
+            }
+          `
+
+          await expect(
+            compileSassString(sass, { importers: [new NodePackageImporter()] })
+          ).resolves.toMatchObject({
+            css: outdent`
+              @font-face {
+                font-family: "whatever";
+                src: url("example.woff");
+              }
+            `
+          })
+        })
+
+        it('uses the default if the function does not exist', async () => {
+          const sass = `
+            @import "tools/font-url";
+
+            $govuk-fonts-path: '/path/to/fonts/';
+            $govuk-font-url-function: unknown-function;
+
+            @font-face {
+              font-family: "whatever";
+              src: govuk-font-url("whatever.woff2");
+            }
+          `
+
+          await expect(compileSassString(sass)).resolves.toMatchObject({
+            css: outdent`
+              @font-face {
+                font-family: "whatever";
+                src: url("/path/to/fonts/whatever.woff2");
+              }
+            `
+          })
+        })
+
+        it('emits deprecation message', async () => {
+          const sass = `
+            $govuk-font-url-function: 'to-upper-case';
+            @import "tools/font-url";
+
+            @font-face {
+              font-family: "whatever";
+              src: govuk-font-url("whatever.woff2");
+            }
+          `
+
+          await compileSassString(sass, { ...sassConfig, fatalDeprecations })
+
+          expect(mockWarnFunction).toHaveBeenCalledWith(
+            'Passing a string to $govuk-font-url-function is deprecated. ' +
+              'Pass a function using meta.get-function(...) instead. ' +
+              'To silence this warning, update $govuk-suppressed-warnings ' +
+              'with key: "font-url-string"',
+            expect.anything()
+          )
         })
       })
     })
