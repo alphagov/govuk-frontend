b4873e49f9904579b81c0667aefc15af066b2e9d
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-04-20 15:54:42 +0100

    Add tests for compilation of individual files
    Ensure that import only files have a `govuk-exports` call and that there's no stray `govuk-` function calls.
    
    Co-authored-by: domoscargin <domoscargin@users.noreply.github.com>

--- packages/govuk-frontend/src/govuk/overrides/overrides.unit.test.js
+++ packages/govuk-frontend/src/govuk/overrides/overrides.unit.test.js
@@ -1,5 +1,15 @@
-const { compileSassString } = require('@govuk-frontend/helpers/tests')
+const { readFile } = require('node:fs/promises')
+const { join } = require('node:path')
+
+const { paths } = require('@govuk-frontend/config')
+const {
+  compileSassString,
+  compileSassFile,
+  getSassPathsFromLayer
+} = require('@govuk-frontend/helpers/tests')
 const stylelint = require('stylelint')
+
+const partials = getSassPathsFromLayer('overrides')
 
 describe('The overrides layer', () => {
   describe.each([
@@ -33,6 +43,46 @@ describe('The overrides layer', () => {
       }
 
       return expect(linter.results[0].warnings).toHaveLength(0)
+    })
+  })
+
+  describe.each(partials)('$name', ({ partialPath, name }) => {
+    let css
+    beforeAll(async () => {
+      const file = join(paths.package, partialPath)
+
+      css = (await compileSassFile(file)).css
+    })
+
+    // Sass will error when trying to access an undefined variable or mixin
+    // but will not on a function, so we need to check the output for calls
+    // to GOV.UK Frontend's API that are not namespaced with `base.`
+    it('does not contain any unexpected govuk- function calls', async () => {
+      const matches = css.matchAll(/_?govuk-[\w-]+\(.*?\)/g)
+
+      // `matchAll` does not return an actual `Array` so we need
+      // a little conversion before we can check its length
+      expect(Array.from(matches)).toHaveLength(0)
+    })
+
+    it('does not output the custom properties', () => {
+      const occurrences = css.matchAll(/--govuk-breakpoint-mobile/g)
+
+      expect(Array.from(occurrences)).toHaveLength(0)
+    })
+
+    it('has a corresponding import-only file', async () => {
+      const importOnlyPath = join(
+        paths.package,
+        partialPath.replace('.scss', '.import.scss')
+      )
+      const { moduleName } = /_(?<moduleName>.*)\.scss/.exec(name).groups
+
+      const fileContent = await readFile(importOnlyPath, { encoding: 'utf-8' })
+
+      expect(fileContent).toContain(
+        `@include govuk-exports("govuk/overrides/${moduleName}")`
+      )
     })
   })
 })
