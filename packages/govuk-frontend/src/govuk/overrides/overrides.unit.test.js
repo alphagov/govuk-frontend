469da24fbf286553a7be5a85da12682fb0480c5a
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-04-20 15:25:51 +0100

    Add tests for both `@import` and `@use`
    Tests for output of custom properties will be added in a future commit as the behaviour currently differs between `@import` and `@use` for now.
    
    Co-authored-by: domoscargin <domoscargin@users.noreply.github.com>

--- packages/govuk-frontend/src/govuk/overrides/overrides.unit.test.js
+++ packages/govuk-frontend/src/govuk/overrides/overrides.unit.test.js
@@ -2,28 +2,37 @@ const { compileSassString } = require('@govuk-frontend/helpers/tests')
 const stylelint = require('stylelint')
 
 describe('The overrides layer', () => {
-  it('does not reference any undefined custom properties', async () => {
-    // Requires base as this is where the custom properties come from
-    const sass = `
+  describe.each([
+    [
+      'import',
+      `
       @import "base";
       @import "overrides";
     `
+    ],
+    ['use', `@use "overrides";`]
+  ])('with `@%s`', (type, sass) => {
+    let css
 
-    const { css } = await compileSassString(sass)
-
-    const linter = await stylelint.lint({
-      config: { rules: { 'no-unknown-custom-properties': true } },
-      code: css
+    beforeAll(async () => {
+      css = (await compileSassString(sass)).css
     })
 
-    // Output stylelint warnings to make debugging easier
-    if (linter.results[0].warnings.length) {
-      console.log(
-        'Warnings were present when testing the overrides layer for unknown custom properties:'
-      )
-      console.log(linter.results[0].warnings)
-    }
+    it('does not reference any undefined custom properties', async () => {
+      const linter = await stylelint.lint({
+        config: { rules: { 'no-unknown-custom-properties': true } },
+        code: css
+      })
 
-    return expect(linter.results[0].warnings).toHaveLength(0)
+      // Output stylelint warnings to make debugging easier
+      if (linter.results[0].warnings.length) {
+        console.log(
+          'Warnings were present when testing the utilities layer for unknown custom properties:'
+        )
+        console.log(linter.results[0].warnings)
+      }
+
+      return expect(linter.results[0].warnings).toHaveLength(0)
+    })
   })
 })
