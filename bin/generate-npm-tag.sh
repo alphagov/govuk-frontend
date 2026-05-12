d58c9f6045ea1acfa8cfd49307fa38fe03268e43
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-05-12 13:14:29 +0100

    Rename variables in `generate-npm-tag`
    We tend to talk about `PACKAGE_VERSION` in the release scripts, and npm's tag for the newest release is `latest` rather than `highest`.

--- bin/generate-npm-tag.sh
+++ bin/generate-npm-tag.sh
@@ -2,27 +2,27 @@
 set -e
 
 # Use first argument as version or extract tag version from ./packages/govuk-frontend/package.json
-CURRENT_VERSION=${1:-$(npm run get-version --silent --workspace govuk-frontend)}
+PACKAGE_VERSION=${1:-$(npm run get-version --silent --workspace govuk-frontend)}
 
-# Use second argument as highest published version or get highest tag published on Github
-HIGHEST_PUBLISHED_VERSION=${2:-$(git tag --list 2>/dev/null | sort -V | tail -n1 2>/dev/null | sed 's/v//g')}
+# Use second argument as latest published version or get latest tag published on Github
+LATEST_PUBLISHED_VERSION=${2:-$(git tag --list 2>/dev/null | sort -V | tail -n1 2>/dev/null | sed 's/v//g')}
 
 version() { echo "$@" | awk -F. '{ printf("%d%03d%03d\n", $1,$2,$3); }'; }
 
-if [ "$CURRENT_VERSION" = "$HIGHEST_PUBLISHED_VERSION" ]; then
+if [ "$PACKAGE_VERSION" = "$LATEST_PUBLISHED_VERSION" ]; then
   echo "⚠️ Git tag $TAG already exists. Check you have run \`npm version\` correctly."
   exit 1
-elif echo "$CURRENT_VERSION" | grep -q "internal"; then
+elif echo "$PACKAGE_VERSION" | grep -q "internal"; then
   NPM_TAG="internal"
-elif echo "$CURRENT_VERSION" | grep -q "beta"; then
+elif echo "$PACKAGE_VERSION" | grep -q "beta"; then
   NPM_TAG="next"
-elif echo "$CURRENT_VERSION" | grep -q -E '^\d+\.\d+\.\d+-\D+(\.\d+)?$'; then
+elif echo "$PACKAGE_VERSION" | grep -q -E '^\d+\.\d+\.\d+-\D+(\.\d+)?$'; then
   echo "⚠️ Pre-releases with an identifier other than 'beta' or 'internal' are not allowed, therefore we will not generate an npm tag. Please check your current version."
   exit 1
-elif [ $(version "$CURRENT_VERSION") -ge $(version "$HIGHEST_PUBLISHED_VERSION") ]; then
+elif [ $(version "$PACKAGE_VERSION") -ge $(version "$LATEST_PUBLISHED_VERSION") ]; then
   NPM_TAG="latest"
 else
-  major_version_num=$(echo "$CURRENT_VERSION" | cut -d. -f1)
+  major_version_num=$(echo "$PACKAGE_VERSION" | cut -d. -f1)
   NPM_TAG="latest-$major_version_num"
 fi
 
