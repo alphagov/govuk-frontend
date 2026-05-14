ee7f5eba657b2c56af7d29da8578890e2ea99816
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-05-14 16:16:50 +0100

    Add missing tests for custom-properties layers
    Check that compilation works OK with both `@import` and `@use`, and ensure no `govuk-` functions are left when compiling partials.

--- packages/govuk-frontend/src/govuk/custom-properties/custom-properties.unit.test.mjs
+++ packages/govuk-frontend/src/govuk/custom-properties/custom-properties.unit.test.mjs
@@ -1,15 +1,55 @@
-import { compileSassString } from '@govuk-frontend/helpers/tests'
+import { join } from 'node:path'
+
+import { paths } from '@govuk-frontend/config'
+import {
+  compileSassFile,
+  compileSassString,
+  getSassPathsFromLayer
+} from '@govuk-frontend/helpers/tests'
+
+const partials = getSassPathsFromLayer('core')
 
 describe('GOV.UK Frontend custom properties', () => {
-  it('imports each set of custom properties', async () => {
-    const sass = `
+  describe.each([
+    [
+      'import',
+      `
+      @import "base";
       @import "custom-properties";
     `
+    ],
+    ['use', `@use "custom-properties";`]
+  ])('with `@%s`', (type, sass) => {
+    let css
 
-    const { css } = await compileSassString(sass)
+    beforeAll(async () => {
+      css = (await compileSassString(sass)).css
+    })
 
-    await expect(css).toContain('--govuk-frontend-version')
-    await expect(css).toContain('--govuk-breakpoint-mobile')
-    await expect(css).toContain('--govuk-brand-colour')
+    it('outputs each set of custom properties', async () => {
+      await expect(css).toContain('--govuk-frontend-version')
+      await expect(css).toContain('--govuk-breakpoint-mobile')
+      await expect(css).toContain('--govuk-brand-colour')
+    })
+  })
+
+  describe.each(partials)('$name', ({ partialPath, name }) => {
+    let css
+    beforeAll(async () => {
+      const file = join(paths.package, partialPath)
+
+      css = (await compileSassFile(file)).css
+    })
+
+    // Sass will error when trying to access an undefined variable or mixin
+    // but will not on a function, so we need to check the output for calls
+    // to GOV.UK Frontend's API that are not namespaced with `base.`
+    it('does not contain any unexpected govuk- function calls', async () => {
+      const matches = css.matchAll(/_?govuk-[\w-]+\(.*?\)/g)
+
+      // `matchAll` does not return an actual `Array` so we need
+      // a little conversion before we can check its length
+      expect(Array.from(matches)).toHaveLength(0)
+    })
   })
 })
