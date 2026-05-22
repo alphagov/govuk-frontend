59d7690416e97dab0ff0316054c91eba912131ae
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-05-22 17:41:38 +0100

    Add tests simulating including a library that `@import`s GOV.UK Frontend
    We need to explicitely import `index.import` because Sass does not load the import only file if using `pkg:` URLs: https://github.com/sass/dart-sass/issues/2771

---
+++ tests/sass-tests/libaries.integration.test.mjs
@@ -0,0 +1,83 @@
+import { compileSassStringLikeUsers } from './helpers/sass.js'
+
+describe('libraries', () => {
+  describe('if the library `@import`s GOV.UK Frontend', () => {
+    describe('without configuration', () => {
+      let css
+
+      beforeAll(async () => {
+        // Sass does not resolve import-only files when using `pkg:` URLs
+        // with `@import` so we need to explicitely load `index.import`
+        // See: https://github.com/sass/sass/issues/4224
+        const sass = `
+          @import 'pkg:govuk-frontend/index.import';
+          @import 'libraries/with-import/index';
+        `
+
+        css = await compileSassStringLikeUsers(sass)
+      })
+
+      it('outputs the CSS of GOV.UK Frontend only once', async () => {
+        expect(css.match(/govuk-visually-hidden/)).toHaveLength(1)
+      })
+
+      it('applied the default GOV.UK Frontend configuration to the library', async () => {
+        expect(css).toContain(
+          '--with-import-colour: var(--govuk-brand-colour, #1d70b8)'
+        )
+        expect(css).toContain(
+          '--with-import-icon: url("/assets/images/home-icon.svg")'
+        )
+      })
+    })
+
+    describe('with configuration', () => {
+      let css
+
+      beforeAll(async () => {
+        const sass = `
+          $govuk-functional-colours: (brand: hotpink);
+          $govuk-image-url-function: 'images-url';
+
+          @import 'pkg:@govuk-frontend/helpers/assets-urls';
+          @import 'pkg:govuk-frontend/index.import';
+          @import 'libraries/with-import/index';
+        `
+
+        css = await compileSassStringLikeUsers(sass)
+      })
+
+      it('applies configuration to both GOV.UK Frontend and the library', async () => {
+        expect(css).toContain('--govuk-brand-colour: hotpink')
+        expect(css).toContain(
+          '--with-import-colour: var(--govuk-brand-colour, hotpink)'
+        )
+        expect(css).toContain('--with-import-icon: url("example.svg")')
+      })
+    })
+
+    describe('without explicitly `@import`ing GOV.UK Frontend', () => {
+      let css
+
+      beforeAll(async () => {
+        const sass = `
+          $govuk-functional-colours: (brand: hotpink);
+          $govuk-image-url-function: 'images-url';
+
+          @import 'pkg:@govuk-frontend/helpers/assets-urls';
+          @import 'libraries/with-import/index';
+        `
+
+        css = await compileSassStringLikeUsers(sass)
+      })
+
+      it('applies configuration to both GOV.UK Frontend and the library', async () => {
+        expect(css).toContain('--govuk-brand-colour: hotpink')
+        expect(css).toContain(
+          '--with-import-colour: var(--govuk-brand-colour, hotpink)'
+        )
+        expect(css).toContain('--with-import-icon: url("example.svg")')
+      })
+    })
+  })
+})
