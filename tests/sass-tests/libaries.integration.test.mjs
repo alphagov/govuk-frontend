f4c096ff89aa07eb4ccc424373bc3b51021d6c70
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2026-05-26 14:17:16 +0100

    Add tests simulating including a library that `@forward`s GOV.UK Frontend
    Verify that when configuring the library, the configuration also gets applied to a subsequent library that `@use`s GOV.UK Frontend.

--- tests/sass-tests/libaries.integration.test.mjs
+++ tests/sass-tests/libaries.integration.test.mjs
@@ -134,4 +134,68 @@ describe('libraries', () => {
       })
     })
   })
+
+  describe('if a library `@forward`s GOV.UK Frontend and another `@use`s it', () => {
+    describe('without configuration', () => {
+      let css
+
+      beforeAll(async () => {
+        const sass = `
+          @use 'libraries/with-forward';
+          @use 'libraries/with-use';
+        `
+
+        css = await compileSassStringLikeUsers(sass)
+      })
+
+      it('outputs the CSS of GOV.UK Frontend only once', async () => {
+        expect(css.match(/govuk-visually-hidden/)).toHaveLength(1)
+      })
+
+      it('applied the default GOV.UK Frontend configuration to both libaries', async () => {
+        expect(css).toContain(
+          '--with-use-colour: var(--govuk-brand-colour, #1d70b8)'
+        )
+        expect(css).toContain(
+          '--with-use-icon: url("/assets/images/home-icon.svg")'
+        )
+        expect(css).toContain(
+          '--with-forward-colour: var(--govuk-brand-colour, #1d70b8)'
+        )
+        expect(css).toContain(
+          '--with-forward-icon: url("/assets/images/home-icon.svg")'
+        )
+      })
+    })
+
+    describe('with configuration', () => {
+      let css
+
+      beforeAll(async () => {
+        const sass = `
+          @use "sass:meta";
+          @use 'pkg:@govuk-frontend/helpers/assets-urls';
+          @use 'libraries/with-forward' with (
+            $govuk-functional-colours: (brand: hotpink),
+            $govuk-image-url-function: meta.get-function('images-url', $module: 'assets-urls')
+          );
+          @use 'libraries/with-use';
+        `
+
+        css = await compileSassStringLikeUsers(sass)
+      })
+
+      it('applies configuration to both GOV.UK Frontend and the libraries', async () => {
+        expect(css).toContain('--govuk-brand-colour: hotpink')
+        expect(css).toContain(
+          '--with-use-colour: var(--govuk-brand-colour, hotpink)'
+        )
+        expect(css).toContain('--with-use-icon: url("example.svg")')
+        expect(css).toContain(
+          '--with-forward-colour: var(--govuk-brand-colour, hotpink)'
+        )
+        expect(css).toContain('--with-forward-icon: url("example.svg")')
+      })
+    })
+  })
 })
